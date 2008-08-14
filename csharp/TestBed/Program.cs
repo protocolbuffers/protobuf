62b3d31fb7f85df243f7559199171ca3375ce89e
Jon Skeet <skeet@pobox.com>
2008-08-14 20:36:24 +0100

    Fixed a few TODOs

---
+++ csharp/TestBed/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace TestBed {
+
+  // Avoid using the .NET 3.5 System.Action delegate
+  delegate void Action();
+
+  class Program {
+
+    private static readonly TimeSpan TimeLimit = TimeSpan.FromMinutes(1);
+    private const int IterationsPerChunk = 50;
+
+    static void Main(string[] args) {
+
+      // Deserialize once to warm up the JIT and give us data use later
+      byte[] data = File.ReadAllBytes(args[0]);
+      Northwind.Database fast = Northwind.Database.ParseFrom(data);
+      SlowNorthwind.Database slow = SlowNorthwind.Database.ParseFrom(data);
+
+      Benchmark("Fast deserialize", () => Northwind.Database.ParseFrom(data));
+      Benchmark("Fast serialize", () => fast.ToByteArray());
+      Benchmark("Slow deserialize", () => SlowNorthwind.Database.ParseFrom(data));
+      Benchmark("Slow serialize", () => slow.ToByteArray());
+      //Console.ReadLine();
+    }
+
+    private static void Benchmark(string description, Action actionUnderTest) {
+      int totalIterations = 0;
+      Stopwatch sw = Stopwatch.StartNew();
+      while (sw.Elapsed < TimeLimit) {
+        for (int i = 0; i < IterationsPerChunk; i++) {
+          actionUnderTest();
+        }
+        totalIterations += IterationsPerChunk;
+      }
+      sw.Stop();
+      Console.WriteLine("{0}: {1} iterations in {2}ms; {3:f2}ms per iteration",
+          description, totalIterations, sw.ElapsedMilliseconds,
+          (double)sw.ElapsedMilliseconds / totalIterations);
+    }
+  }
+}
---
+++ csharp/TestBed/bin/Release/TestBed.vshost.exe.manifest
@@ -0,0 +1,11 @@
+﻿<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+  <assemblyIdentity version="1.0.0.0" name="MyApplication.app"/>
+  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v2">
+    <security>
+      <requestedPrivileges xmlns="urn:schemas-microsoft-com:asm.v3">
+        <requestedExecutionLevel level="asInvoker" uiAccess="false"/>
+      </requestedPrivileges>
+    </security>
+  </trustInfo>
+</assembly>
