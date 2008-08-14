3ae573c17475021046f3d3b2b5f01de91c80aa1d
Jon Skeet <skeet@pobox.com>
2008-08-14 20:35:31 +0100

    Fleshed out service interfaces, and wrote the simpler service tests. Mocking tests still to be done.

--- csharp/ProtocolBuffers.Test/ServiceTest.cs
+++ csharp/ProtocolBuffers.Test/ServiceTest.cs
@@ -1,10 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Google.ProtocolBuffers.Descriptors;
+using Google.ProtocolBuffers.TestProtos;
 using NUnit.Framework;
 
 namespace Google.ProtocolBuffers {
+
+  /// <summary>
+  /// Tests for generated service classes.
+  /// TODO(jonskeet): Convert the mocking tests using Rhino.Mocks.
+  /// </summary>
   [TestFixture]
   public class ServiceTest {
+
+    private static readonly MethodDescriptor FooDescriptor = TestService.Descriptor.Methods[0];
+    private static readonly MethodDescriptor BarDescriptor = TestService.Descriptor.Methods[1];
+
+    [Test]
+    public void GetRequestPrototype() {
+      TestService mockService = new TestServiceImpl();
+
+      Assert.AreSame(mockService.GetRequestPrototype(FooDescriptor), FooRequest.DefaultInstance);
+      Assert.AreSame(mockService.GetRequestPrototype(BarDescriptor), BarRequest.DefaultInstance);
+    }
+
+    [Test]
+    public void GetResponsePrototype() {
+      TestService mockService = new TestServiceImpl();
+
+      Assert.AreSame(mockService.GetResponsePrototype(FooDescriptor), FooResponse.DefaultInstance);
+      Assert.AreSame(mockService.GetResponsePrototype(BarDescriptor), BarResponse.DefaultInstance);
+    }
+    
+    class TestServiceImpl : TestService {
+      public override void Foo(IRpcController controller, FooRequest request, Action<FooResponse> done) {
+        throw new System.NotImplementedException();
+      }
+
+      public override void Bar(IRpcController controller, BarRequest request, Action<BarResponse> done) {
+        throw new System.NotImplementedException();
+      }
+    }
   }
 }
--- csharp/ProtocolBuffers/IRpcChannel.cs
+++ csharp/ProtocolBuffers/IRpcChannel.cs
@@ -1,14 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Google.ProtocolBuffers.Descriptors;
 
 namespace Google.ProtocolBuffers {
   /// <summary>
-  /// TODO(jonskeet): Do this properly.
+  /// Interface for an RPC channel. A channel represents a communication line to
+  /// a service (IService implementation) which can be used to call that service's
+  /// methods. The service may be running on another machine. Normally, you should
+  /// not call an IRpcChannel directly, but instead construct a stub wrapping it.
+  /// Generated service classes contain a CreateStub method for precisely this purpose.
   /// </summary>
   public interface IRpcChannel {
-    void CallMethod<T>(MethodDescriptor method, IRpcController controller,
-        IMessage request, IMessage responsePrototype, Action<T> done);
+    /// <summary>
+    /// Calls the given method of the remote service. This method is similar
+    /// to <see cref="IService.CallMethod" /> with one important difference: the
+    /// caller decides the types of the IMessage objects, not the implementation.
+    /// The request may be of any type as long as <c>request.Descriptor == method.InputType</c>.
+    /// The response passed to the callback will be of the same type as
+    /// <paramref name="responsePrototype"/> (which must be such that
+    /// <c>responsePrototype.Descriptor == method.OutputType</c>).
+    /// </summary>
+    void CallMethod(MethodDescriptor method, IRpcController controller,
+        IMessage request, IMessage responsePrototype, Action<IMessage> done);
   }
 }
