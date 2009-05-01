cc24f745e1770a891cacae81b701233e0bb06747
kenton@google.com <kenton@google.com@630680e5-0e50-0410-840e-4b1c322b438d>
2009-05-01 21:41:32 +0000

    Hopefully make autogen.sh run on solaris.

--- trunk/autogen.sh
+++ trunk/autogen.sh
@@ -5,7 +5,7 @@
 # are automatically generated.
 
 # Check that we're being run from the right directory.
-if test ! -e src/google/protobuf/stubs/common.h; then
+if test ! -f src/google/protobuf/stubs/common.h; then
   cat >&2 << __EOF__
 Could not find source code.  Make sure you are running this script from the
 root of the distribution tree.
@@ -13,7 +13,7 @@ __EOF__
   exit 1
 fi
 
-if test ! -e gtest; then
+if test ! -d gtest; then
   echo "gtest bundle not present.  Downloading gtest-1.3.0 automatically." >&2
   set -ex
   curl http://googletest.googlecode.com/files/gtest-1.3.0.tar.bz2 | tar jx
