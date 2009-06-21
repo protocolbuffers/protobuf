2bb7637797276287eb1823d644689dc0858bb48b
Joshua Haberman <joshua@reverberate.org>
2009-06-20 22:44:30 -0700

    Fixed conversion warning in tests.c.

--- tests.c
+++ tests.c
@@ -8,7 +8,7 @@ void test_get_v_uint64_t()
   upb_status_t status;
 
   uint8_t zero[] = {0x00};
-  uint8_t *zero_buf = zero;
+  void *zero_buf = zero;
   uint64_t zero_val = 0;
   status = get_v_uint64_t(&zero_buf, &zero_val);
   assert(status == UPB_STATUS_OK);
@@ -16,28 +16,28 @@ void test_get_v_uint64_t()
   assert(zero_buf == zero + sizeof(zero));
 
   uint8_t one[] = {0x01};
-  uint8_t *one_buf = one;
+  void *one_buf = one;
   uint64_t one_val = 0;
   status = get_v_uint64_t(&one_buf, &one_val);
   assert(status == UPB_STATUS_OK);
   assert(one_val == 1);
 
   uint8_t twobyte[] = {0xAC, 0x02};
-  uint8_t *twobyte_buf = twobyte;
+  void *twobyte_buf = twobyte;
   uint64_t twobyte_val = 0;
   status = get_v_uint64_t(&twobyte_buf, &twobyte_val);
   assert(status == UPB_STATUS_OK);
   assert(twobyte_val == 300);
 
   uint8_t tenbyte[] = {0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x7F};
-  uint8_t *tenbyte_buf = tenbyte;
+  void *tenbyte_buf = tenbyte;
   uint64_t tenbyte_val = 0;
   status = get_v_uint64_t(&tenbyte_buf, &tenbyte_val);
   assert(status == UPB_STATUS_OK);
   assert(tenbyte_val == 0x89101c305080c101);
 
   uint8_t elevenbyte[] = {0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x01};
-  uint8_t *elevenbyte_buf = elevenbyte;
+  void *elevenbyte_buf = elevenbyte;
   uint64_t elevenbyte_val = 0;
   status = get_v_uint64_t(&elevenbyte_buf, &elevenbyte_val);
   assert(status == UPB_ERROR_UNTERMINATED_VARINT);
