f8f689ad31bd3f70f163f9afe340989126f9f4d5
Joshua Haberman <joshua@reverberate.org>
2009-06-22 17:58:04 -0700

    Add upb_inlinedefs.c, which emits definitions for inline functions.

---
+++ upb_inlinedefs.c
@@ -0,0 +1,10 @@
+/*
+ * upb - a minimalist implementation of protocol buffers.
+ *
+ * Copyright (c) 2009 Joshua Haberman.  See LICENSE for details.
+ */
+
+#define INLINE
+#include "upb_parse.h"
+#include "upb_msg.h"
+#include "upb_table.h"
