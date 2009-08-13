3fe9c14b02f2f6e886265f85837752e84904cd63
kenton@google.com <kenton@google.com@630680e5-0e50-0410-840e-4b1c322b438d>
2009-08-13 22:41:37 +0000

    Set version number to 2.2.1 in trunk.

--- trunk/src/google/protobuf/stubs/common.h
+++ trunk/src/google/protobuf/stubs/common.h
@@ -83,7 +83,7 @@ namespace internal {
 
 // The current version, represented as a single integer to make comparison
 // easier:  major * 10^6 + minor * 10^3 + micro
-#define GOOGLE_PROTOBUF_VERSION 2002000
+#define GOOGLE_PROTOBUF_VERSION 2002001
 
 // The minimum library version which works with the current version of the
 // headers.
