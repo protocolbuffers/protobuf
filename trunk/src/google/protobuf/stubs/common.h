283ef7ec06e6fb5134b1e81b2be7221e9f38459c
kenton@google.com <kenton@google.com@630680e5-0e50-0410-840e-4b1c322b438d>
2009-08-14 01:21:01 +0000

    Don't use RTTI in down_cast if GOOGLE_PROTOBUF_NO_RTTI is defined.  Patch from Chris Masone (of Google).

--- trunk/src/google/protobuf/stubs/common.h
+++ trunk/src/google/protobuf/stubs/common.h
@@ -283,7 +283,9 @@ inline To down_cast(From* f) {                   // so we only accept pointers
     implicit_cast<From*, To>(0);
   }
 
+#if !defined(NDEBUG) && !defined(GOOGLE_PROTOBUF_NO_RTTI)
   assert(f == NULL || dynamic_cast<To>(f) != NULL);  // RTTI: debug mode only!
+#endif
   return static_cast<To>(f);
 }
 
