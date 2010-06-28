7d0a2a3c88bf183c7d2c6795df85830027af5278
Wink Saville <wink@google.com>
2010-06-28 15:05:24 -0700

    Add libprotobuf-cpp-2.3.0-full.
    Add full c++ version of libprotobuf as a static library.
    
    Change-Id: I153bc913ff51e6a904284b733cac000497e1caac

--- Android.mk
+++ Android.mk
@@ -16,6 +16,23 @@
 
 LOCAL_PATH := $(call my-dir)
 
+CC_LITE_SRC_FILES := \
+  src/google/protobuf/stubs/common.cc                              \
+  src/google/protobuf/stubs/once.cc                                \
+  src/google/protobuf/stubs/hash.cc                                \
+  src/google/protobuf/stubs/hash.h                                 \
+  src/google/protobuf/stubs/map-util.h                             \
+  src/google/protobuf/stubs/stl_util-inl.h                         \
+  src/google/protobuf/extension_set.cc                             \
+  src/google/protobuf/generated_message_util.cc                    \
+  src/google/protobuf/message_lite.cc                              \
+  src/google/protobuf/repeated_field.cc                            \
+  src/google/protobuf/wire_format_lite.cc                          \
+  src/google/protobuf/io/coded_stream.cc                           \
+  src/google/protobuf/io/coded_stream_inl.h                        \
+  src/google/protobuf/io/zero_copy_stream.cc                       \
+  src/google/protobuf/io/zero_copy_stream_impl_lite.cc
+
 # Java micro library
 # =======================================================
 include $(CLEAR_VARS)
@@ -56,22 +73,7 @@ LOCAL_MODULE := libprotobuf-cpp-2.3.0-lite
 
 LOCAL_CPP_EXTENSION := .cc
 
-LOCAL_SRC_FILES := \
-  src/google/protobuf/stubs/common.cc                              \
-  src/google/protobuf/stubs/once.cc                                \
-  src/google/protobuf/stubs/hash.cc                                \
-  src/google/protobuf/stubs/hash.h                                 \
-  src/google/protobuf/stubs/map-util.h                             \
-  src/google/protobuf/stubs/stl_util-inl.h                         \
-  src/google/protobuf/extension_set.cc                             \
-  src/google/protobuf/generated_message_util.cc                    \
-  src/google/protobuf/message_lite.cc                              \
-  src/google/protobuf/repeated_field.cc                            \
-  src/google/protobuf/wire_format_lite.cc                          \
-  src/google/protobuf/io/coded_stream.cc                           \
-  src/google/protobuf/io/coded_stream_inl.h                        \
-  src/google/protobuf/io/zero_copy_stream.cc                       \
-  src/google/protobuf/io/zero_copy_stream_impl_lite.cc
+LOCAL_SRC_FILES := $(CC_LITE_SRC_FILES)
 
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/android \
@@ -80,6 +82,71 @@ LOCAL_C_INCLUDES := \
 
 LOCAL_SHARED_LIBRARIES := \
     libcutils libutils
+
+# stlport conflicts with the host stl library
+ifneq ($(TARGET_SIMULATOR),true)
+LOCAL_C_INCLUDES += external/stlport/stlport
+LOCAL_SHARED_LIBRARIES += libstlport
+endif
+
+# Define the header files to be copied
+#LOCAL_COPY_HEADERS := \
+#    src/google/protobuf/stubs/once.h \
+#    src/google/protobuf/stubs/common.h \
+#    src/google/protobuf/io/coded_stream.h \
+#    src/google/protobuf/generated_message_util.h \
+#    src/google/protobuf/repeated_field.h \
+#    src/google/protobuf/extension_set.h \
+#    src/google/protobuf/wire_format_lite_inl.h
+#
+#LOCAL_COPY_HEADERS_TO := $(LOCAL_MODULE)
+
+LOCAL_CFLAGS := -DGOOGLE_PROTOBUF_NO_RTTI
+
+include $(BUILD_STATIC_LIBRARY)
+
+# C++ full library
+# =======================================================
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libprotobuf-cpp-2.3.0-full
+
+LOCAL_CPP_EXTENSION := .cc
+
+LOCAL_SRC_FILES := \
+  $(CC_LITE_SRC_FILES)                                             \
+  src/google/protobuf/stubs/strutil.cc                             \
+  src/google/protobuf/stubs/strutil.h                              \
+  src/google/protobuf/stubs/substitute.cc                          \
+  src/google/protobuf/stubs/substitute.h                           \
+  src/google/protobuf/stubs/structurally_valid.cc                  \
+  src/google/protobuf/descriptor.cc                                \
+  src/google/protobuf/descriptor.pb.cc                             \
+  src/google/protobuf/descriptor_database.cc                       \
+  src/google/protobuf/dynamic_message.cc                           \
+  src/google/protobuf/extension_set_heavy.cc                       \
+  src/google/protobuf/generated_message_reflection.cc              \
+  src/google/protobuf/message.cc                                   \
+  src/google/protobuf/reflection_ops.cc                            \
+  src/google/protobuf/service.cc                                   \
+  src/google/protobuf/text_format.cc                               \
+  src/google/protobuf/unknown_field_set.cc                         \
+  src/google/protobuf/wire_format.cc                               \
+  src/google/protobuf/io/gzip_stream.cc                            \
+  src/google/protobuf/io/printer.cc                                \
+  src/google/protobuf/io/tokenizer.cc                              \
+  src/google/protobuf/io/zero_copy_stream_impl.cc                  \
+  src/google/protobuf/compiler/importer.cc                         \
+  src/google/protobuf/compiler/parser.cc
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/android \
+    bionic \
+    external/zlib \
+    $(LOCAL_PATH)/src
+
+LOCAL_SHARED_LIBRARIES := \
+    libz libcutils libutils
 
 # stlport conflicts with the host stl library
 ifneq ($(TARGET_SIMULATOR),true)
