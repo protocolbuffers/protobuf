2c94ecc74a32e23225c3e462b9d1700195089522
ArnoldZokas <arnold.zokas@coderoom.net>
2010-09-26 21:10:59 +0100

    Removed cfg folder in preparation for build configuration refactoring

--- cfg/build with diagnostics.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /l:FileLogger,Microsoft.Build.Engine;logfile=Build.log;append=true;verbosity=diagnostic;encoding=utf-8
-
-pause
