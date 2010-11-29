42e8ec7fd33ede9de812dbc62c553b95902c7358
ArnoldZokas <arnold.zokas@coderoom.net>
2010-11-29 22:18:08 +0000

    Added batch file for running 'GenerateReleasePackage'

--- build/BuildAll.bat
+++ build/BuildAll.bat
@@ -4,7 +4,6 @@
 IF ERRORLEVEL 1 GOTO END
 
 :SILVERLIGHT2
-ECHO RONG!
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build /p:BuildConfiguration=Debug_Silverlight2
 
 :END
---
+++ build/GenerateReleasePackage.bat
@@ -0,0 +1,14 @@
+:BEGIN
+@ECHO OFF
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build /p:BuildConfiguration=Release
+IF ERRORLEVEL 1 GOTO END
+
+:SILVERLIGHT2
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build /p:BuildConfiguration=Release_Silverlight2
+IF ERRORLEVEL 1 GOTO END
+
+:GENERATE_PACKAGE
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:GeneratePackage /p:PackageScope=ReleaseBinaries
+
+:END
+PAUSE
