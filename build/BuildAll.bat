b287a2de366cb8ab93d8a6ed466677fe50a0459d
ArnoldZokas <arnold.zokas@coderoom.net>
2010-11-29 22:43:16 +0000

    Added '_Test' target to build scripts

--- build/BuildAll.bat
+++ build/BuildAll.bat
@@ -1,9 +1,7 @@
-:BEGIN
 @ECHO OFF
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build /p:BuildConfiguration=Debug
 IF ERRORLEVEL 1 GOTO END
 
-:SILVERLIGHT2
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build /p:BuildConfiguration=Debug_Silverlight2
 
 :END
--- build/GenerateCompletePackage.bat
+++ build/GenerateCompletePackage.bat
@@ -1,13 +1,16 @@
-:BEGIN
 @ECHO OFF
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build /p:BuildConfiguration=Debug
+IF ERRORLEVEL 1 GOTO END
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build /p:BuildConfiguration=Debug_Silverlight2
+IF ERRORLEVEL 1 GOTO END
+
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build /p:BuildConfiguration=Release
 IF ERRORLEVEL 1 GOTO END
 
-:SILVERLIGHT2
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build /p:BuildConfiguration=Release_Silverlight2
 IF ERRORLEVEL 1 GOTO END
 
-:GENERATE_PACKAGE
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:GeneratePackage /p:PackageScope=AllBinariesAndSource
 
 :END
