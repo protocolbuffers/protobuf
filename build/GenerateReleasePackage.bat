1999a990bed07dd6f3d222f017bafa534b04e1eb
ArnoldZokas <arnold.zokas@coderoom.net>
2010-11-30 21:32:07 +0000

    Implemented distribution package generation

--- build/GenerateReleasePackage.bat
+++ build/GenerateReleasePackage.bat
@@ -1,11 +1,11 @@
 @ECHO OFF
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build /p:BuildConfiguration=Release
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build;PreparePackageComponent /p:BuildConfiguration=Release
 IF ERRORLEVEL 1 GOTO END
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build /p:BuildConfiguration=Release_Silverlight2
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build;PreparePackageComponent /p:BuildConfiguration=Release_Silverlight2
 IF ERRORLEVEL 1 GOTO END
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:GeneratePackage /p:PackageScope=ReleaseBinaries
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:GeneratePackage /p:PackageName=ReleaseBinaries.zip
 
 :END
 PAUSE
