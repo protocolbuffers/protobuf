0c58d060f7e919439210fff5ee107e023b1c8199
Jon Skeet <skeet@pobox.com>
2010-12-07 21:10:56 +0000

    Renamed benchmark batch file, and made all other batch files use Any CPU target.

--- build/BuildAll.bat
+++ build/BuildAll.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build /p:BuildConfiguration=Debug
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build /p:BuildConfiguration=Debug /p:Platform="Any CPU"
 IF ERRORLEVEL 1 GOTO END
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build /p:BuildConfiguration=Debug_Silverlight2
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build /p:BuildConfiguration=Debug_Silverlight2 /p:Platform="Any CPU"
 
 :END
 PAUSE
