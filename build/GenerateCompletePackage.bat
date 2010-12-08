649eb54a99166127d1f4ff2df4953ae204f7da13
ArnoldZokas <arnold.zokas@coderoom.net>
2010-12-08 20:27:01 +0000

    Fixed a bug in the build process that caused ReleaseBinaries package to be populated with redundant empty directories

--- build/GenerateCompletePackage.bat
+++ build/GenerateCompletePackage.bat
@@ -1,4 +1,7 @@
 @ECHO OFF
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:PrepareOutputDirectory
+IF ERRORLEVEL 1 GOTO END
+
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build;PreparePackageComponent /p:BuildConfiguration=Debug /p:Platform="Any CPU"
 IF ERRORLEVEL 1 GOTO END
 
