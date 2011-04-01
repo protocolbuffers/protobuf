36d89694318d6c77ba0ed10e418869b319ea56c7
Joshua Haberman <joshua@reverberate.org>
2011-04-01 15:52:39 -0700

    Whoops, add src/jit_debug_elf_file.s

---
+++ src/jit_debug_elf_file.s
@@ -0,0 +1,7 @@
+   .file "JIT mcode"
+   .text
+upb_jit_compiled_decoder:
+   .global upb_jit_compiled_decoder
+   .size upb_jit_compiled_decoder, 0x321
+   .type upb_jit_compiled_decoder STT_FUNC
+   .space 0x321
