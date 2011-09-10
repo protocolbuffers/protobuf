f52ec95ad50b352ed82449f9cbef48f6edd50149
csharptest <roger@csharptest.net>
2011-09-09 21:55:02 -0500

    revert of accidental AssemblyInfo version changes

--- src/ProtoBench/Properties/AssemblyInfo.cs
+++ src/ProtoBench/Properties/AssemblyInfo.cs
@@ -36,7 +36,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.3.0.369")]
+// [assembly: AssemblyVersion("2.3.0.277")]
 
-[assembly: AssemblyVersion("2.3.0.369")]
-[assembly: AssemblyFileVersion("2.3.0.369")]
+[assembly: AssemblyVersion("2.3.0.277")]
+[assembly: AssemblyFileVersion("2.3.0.277")]
