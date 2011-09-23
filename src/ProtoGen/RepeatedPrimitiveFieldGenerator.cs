dc24b605e356dbfda692e952807b404cccf245bc
csharptest <roger@csharptest.net>
2011-09-23 11:13:41 -0500

    Generator changes to fix circular reference issues in static ctor

--- src/ProtoGen/RepeatedPrimitiveFieldGenerator.cs
+++ src/ProtoGen/RepeatedPrimitiveFieldGenerator.cs
@@ -119,7 +119,7 @@ namespace Google.ProtocolBuffers.ProtoGen
 
         public void GenerateBuildingCode(TextGenerator writer)
         {
-            writer.WriteLine("result.{0}_.MakeReadOnly();", Name);
+            writer.WriteLine("{0}_.MakeReadOnly();", Name);
         }
 
         public void GenerateParsingCode(TextGenerator writer)
