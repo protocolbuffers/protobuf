d444748edc798f48d80cb250cc3bfda913d8cfff
csharptest <roger@csharptest.net>
2011-10-02 16:33:22 -0500

    Several build related changes in this commit: 1. Refactoring of build to allow multiple target frameworks 2. Addition of multiple Silverlight versions 3. Renamed *_Silverlight2 configurations to *_Silverlight 4. Added batch files for each framework build (Build20, Build35, Build40) 5. Moved the package building into MSBuild tasks 6. BuildAll.bat now builds all packages in one build 7. Removed Generate*Package.bat batch files (replaced by BuildAll.bat) 8. Removed the ability to build with 3.5 MSBuild 9. Source is only generated with Release/2.0 build of ProtoGen 10. Removed unit testing proto files from packaging 11. Removed the remaining 'pause' statements from all batch files 12. RunBenchmarks target now builds with .NET 2.0 instead of 4.0 13. Benchmark arguments can now be specified with BenchmarkArgs parameter 14. ProtoBench now supports '/log:path' so console can still see progress 15. Updated PublishRelease.bat and *.nuspec for new build output 16. Updated ProtocolBuffers.Serialization.csproj to omit extensions for 2.0 17. Added NUnit console configurations for each .NET framework version

--- build/build35.bat
+++ build/build35.bat
@@ -1,8 +1,8 @@
 @echo off
-SET BUILD_TARGET=%1
-SET BUILD_CONFIG=%2
+SET BUILD_TARGET=%~1
+SET BUILD_CONFIG=%~2
 
 IF "%BUILD_TARGET%"=="" SET BUILD_TARGET=Rebuild
 IF "%BUILD_CONFIG%"=="" SET BUILD_CONFIG=Debug
 
-CMD.exe /Q /C "CD %~dp0 && %WINDIR%\Microsoft.NET\Framework\v3.5\MSBuild.exe build.csproj %3 %4 %5 %6 /t:%BUILD_TARGET% /p:BuildConfiguration=%BUILD_CONFIG% /p:Platform="Any CPU" /p:BuildTools=3.5 /toolsversion:3.5"
+CMD.exe /Q /C "CD %~dp0 && %WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe build.csproj /toolsversion:4.0 %3 %4 %5 %6 "/t:%BUILD_TARGET%" "/p:BuildConfiguration=%BUILD_CONFIG%;TargetVersion=3"
