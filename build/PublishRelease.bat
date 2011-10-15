f2c16f1d0d21e5a51a4d81fb786aeb032d83a1ab
csharptest <roger@csharptest.net>
2011-10-15 12:11:53 -0500

    version 2.4.1.473

--- build/PublishRelease.bat
+++ build/PublishRelease.bat
@@ -26,13 +26,8 @@ IF NOT EXIST "..\release-key" hg clone https://bitbucket.org/rknapp/protobuf-csh
 MD "%2"
 CMD.exe /Q /C "BuildAll.bat /verbosity:minimal "/p:AssemblyOriginatorKeyFile=%~dp0..\release-key\Google.ProtocolBuffers.snk"
 
-COPY /y ..\build_output\Release-v2.0.zip %2\protobuf-csharp-port-%2-net20-release-binaries.zip
-COPY /y ..\build_output\Release-v3.5.zip %2\protobuf-csharp-port-%2-net35-release-binaries.zip
-COPY /y ..\build_output\Release-v4.0.zip %2\protobuf-csharp-port-%2-net40-release-binaries.zip
-
-COPY /y ..\build_output\Full-v2.0.zip %2\protobuf-csharp-port-%2-net20-full-binaries.zip
-COPY /y ..\build_output\Full-v3.5.zip %2\protobuf-csharp-port-%2-net35-full-binaries.zip
-COPY /y ..\build_output\Full-v4.0.zip %2\protobuf-csharp-port-%2-net40-full-binaries.zip
+COPY /y ..\build_output\release-binaries.zip %2\protobuf-csharp-port-%2-release-binaries.zip
+COPY /y ..\build_output\full-binaries.zip %2\protobuf-csharp-port-%2-full-binaries.zip
 
 ..\lib\NuGet.exe pack Google.ProtocolBuffers.nuspec -Symbols -Version %2 -NoPackageAnalysis -OutputDirectory %2
 ..\lib\NuGet.exe pack Google.ProtocolBuffersLite.nuspec -Symbols -Version %2 -NoPackageAnalysis -OutputDirectory %2
@@ -45,14 +40,12 @@ hg archive %2\protobuf-csharp-port-%2-source.zip
 @ECHO IMPORTANT: Verify the above key output is: 55f7125234beb589
 @ECHO ***********************************************************
 @ECHO.
-@ECHO NEXT: Verify the output in %~dp0\%2 and then run "%0 push %2 {google-code-user} {google-code-password}"
+@ECHO NEXT: Verify the output in %~dp0\%2 and then run "%0 push %2"
 @ECHO.
 @GOTO EXIT
 
 :PUSH
 @IF "%2" == "" @GOTO HELP
-@IF "%3" == "" @GOTO HELP
-@IF "%4" == "" @GOTO HELP
 
 hg commit -m "version %2"
 hg tag %2
@@ -63,15 +56,14 @@ hg push
 @GOTO EXIT
 
 :FILEPUSH
+@IF "%2" == "" @GOTO HELP
+@IF "%3" == "" @GOTO HELP
+@IF "%4" == "" @GOTO HELP
 SET GOOGLEUPLOAD=python.exe googlecode_upload.py --project protobuf-csharp-port --user "%3" --password "%4"
 
 %GOOGLEUPLOAD% --labels Type-Source,Featured --summary "Version %2 source" %2\protobuf-csharp-port-%2-source.zip
-%GOOGLEUPLOAD% --labels Type-Executable,Featured --summary "Version %2 binaries for .NET 2.0 (all configurations)" %2\protobuf-csharp-port-%2-net20-full-binaries.zip
-%GOOGLEUPLOAD% --labels Type-Executable,Featured --summary "Version %2 binaries for .NET 3.5 (all configurations)" %2\protobuf-csharp-port-%2-net35-full-binaries.zip
-%GOOGLEUPLOAD% --labels Type-Executable,Featured --summary "Version %2 binaries for .NET 4.0 (all configurations)" %2\protobuf-csharp-port-%2-net40-full-binaries.zip
-%GOOGLEUPLOAD% --labels Type-Executable,Featured --summary "Version %2 binaries for .NET 2.0 (release only)" %2\protobuf-csharp-port-%2-net20-release-binaries.zip
-%GOOGLEUPLOAD% --labels Type-Executable,Featured --summary "Version %2 binaries for .NET 3.5 (release only)" %2\protobuf-csharp-port-%2-net35-release-binaries.zip
-%GOOGLEUPLOAD% --labels Type-Executable,Featured --summary "Version %2 binaries for .NET 4.0 (release only)" %2\protobuf-csharp-port-%2-net40-release-binaries.zip
+%GOOGLEUPLOAD% --labels Type-Executable,Featured --summary "Version %2 binaries (all configurations)" %2\protobuf-csharp-port-%2-full-binaries.zip
+%GOOGLEUPLOAD% --labels Type-Executable,Featured --summary "Version %2 binaries (release only)" %2\protobuf-csharp-port-%2-release-binaries.zip
 
 @SET GOOGLEUPLOAD=
 @ECHO.
@@ -96,7 +88,7 @@ SET GOOGLEUPLOAD=python.exe googlecode_upload.py --project protobuf-csharp-port 
 @ECHO Available commands, run in the following order:
 @ECHO 1. %0 version
 @ECHO 2. %0 build {version from step 1}
-@ECHO 3. %0 push {version from step 1} {google-code-user} {google-code-password}
+@ECHO 3. %0 push {version from step 1}
 @ECHO 4. %0 fpush {version from step 1} {google-code-user} {google-code-password}
 @ECHO 5. %0 nupush {version from step 1}
 @ECHO.
