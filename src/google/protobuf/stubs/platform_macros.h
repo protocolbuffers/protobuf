4d8c2f94fbdc1ef92acd2fc01d1f61e3ef440875
liujisi@google.com <liujisi@google.com@630680e5-0e50-0410-840e-4b1c322b438d>
2013-01-10 21:24:56 +0000

    Add arm_qnx atomic ops support and add missing header files in Makefile.am

--- src/google/protobuf/stubs/platform_macros.h
+++ src/google/protobuf/stubs/platform_macros.h
@@ -43,6 +43,9 @@
 #elif defined(_M_IX86) || defined(__i386__)
 #define GOOGLE_PROTOBUF_ARCH_IA32 1
 #define GOOGLE_PROTOBUF_ARCH_32_BIT 1
+#elif defined(__QNX__)
+#define GOOGLE_PROTOBUF_ARCH_ARM_QNX 1
+#define GOOGLE_PROTOBUF_ARCH_32_BIT 1
 #elif defined(__ARMEL__)
 #define GOOGLE_PROTOBUF_ARCH_ARM 1
 #define GOOGLE_PROTOBUF_ARCH_32_BIT 1
