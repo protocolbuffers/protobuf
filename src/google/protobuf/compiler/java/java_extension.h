64a4fd5e3d6b4e5634560c8906de237e147aecac
Daniel Martin <fizbin@gmail.com>
2014-11-25 10:37:57 -0500

    Fix java compilation issues when processing large .proto files
    Fix issues 485 and 501 on the code.google.com issues list.
    
    Specifically, large .proto files lead to too much static code, leading to a
    compilation error from javac: "code too large". This divides the code used
    in static initialization into multiple methods to avoid that error. Also,
    this incorporates the fix in issue 485 on the code.google.com issues list
    to call registry.add only once per extension.

--- src/google/protobuf/compiler/java/java_extension.h
+++ src/google/protobuf/compiler/java/java_extension.h
@@ -67,8 +67,8 @@ class ExtensionGenerator {
   virtual ~ExtensionGenerator() {}
 
   virtual void Generate(io::Printer* printer) = 0;
-  virtual void GenerateNonNestedInitializationCode(io::Printer* printer) = 0;
-  virtual void GenerateRegistrationCode(io::Printer* printer) = 0;
+  virtual int GenerateNonNestedInitializationCode(io::Printer* printer) = 0;
+  virtual int GenerateRegistrationCode(io::Printer* printer) = 0;
 
  protected:
   static void InitTemplateVars(const FieldDescriptor* descriptor,
@@ -88,8 +88,8 @@ class ImmutableExtensionGenerator : public ExtensionGenerator {
   virtual ~ImmutableExtensionGenerator();
 
   virtual void Generate(io::Printer* printer);
-  virtual void GenerateNonNestedInitializationCode(io::Printer* printer);
-  virtual void GenerateRegistrationCode(io::Printer* printer);
+  virtual int GenerateNonNestedInitializationCode(io::Printer* printer);
+  virtual int GenerateRegistrationCode(io::Printer* printer);
 
  protected:
   const FieldDescriptor* descriptor_;
