5fd12b84dad423b0482f663a844b44ab1f6fc5f6
Feng Xiao <xfxyjwf@gmail.com>
2014-12-11 15:17:04 -0800

    Update gtest version to v1.7.0.

--- autogen.sh
+++ autogen.sh
@@ -18,9 +18,11 @@ fi
 # Check that gtest is present.  Usually it is already there since the
 # directory is set up as an SVN external.
 if test ! -e gtest; then
-  echo "Google Test not present.  Fetching gtest-1.5.0 from the web..."
-  curl http://googletest.googlecode.com/files/gtest-1.5.0.tar.bz2 | tar jx
-  mv gtest-1.5.0 gtest
+  echo "Google Test not present.  Fetching gtest-1.7.0 from the web..."
+  curl -O https://googletest.googlecode.com/files/gtest-1.7.0.zip
+  unzip -q gtest-1.7.0.zip
+  rm gtest-1.7.0.zip
+  mv gtest-1.7.0 gtest
 fi
 
 set -ex
