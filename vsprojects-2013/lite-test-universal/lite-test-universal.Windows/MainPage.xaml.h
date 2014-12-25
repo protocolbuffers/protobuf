6ca0663330fb790e07e071d996173866ad0162e5
Amol Deshpande <amol.deshpande@outlook.com>
2014-12-25 11:50:23 -0800

    lite-test universal test app

---
+++ vsprojects-2013/lite-test-universal/lite-test-universal.Windows/MainPage.xaml.h
@@ -0,0 +1,23 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace lite_test_universal
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	private:
+		void Button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+	};
+}
