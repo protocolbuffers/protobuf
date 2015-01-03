90ce509732d210670f38922a06b007a3ee835901
Rick Sladkey <rick.sladkey@gmail.com>
2015-01-02 22:50:08 -0500

    Use portable sized type in ruby generator

--- src/google/protobuf/compiler/ruby/ruby_generator.cc
+++ src/google/protobuf/compiler/ruby/ruby_generator.cc
@@ -30,6 +30,7 @@
 
 #include <sstream>
 
+#include <google/protobuf/stubs/common.h>
 #include <google/protobuf/compiler/code_generator.h>
 #include <google/protobuf/compiler/plugin.h>
 #include <google/protobuf/descriptor.h>
