ed1494924ab1ce09889947373f1a6c533bccf7a6
Jisi Liu <jisi.liu@gmail.com>
2015-02-19 16:27:39 -0800

    fix rules.
    Change-Id: Ic4d96ba01e84812553b1ba6b45f918cf42077b99

--- rules.pl
+++ rules.pl
@@ -1,3 +1,12 @@
-submit_rule(submit(CR, V)) :-
-  CR = label('Code-Review', ok(_)),
-  V = label('Verified', ok(_)).
+submit_rule(submit(CR)) :-
+  base(CR),
+  gerrit:current_user(Reviewer),
+  gerrit:commit_author(Author),
+  CR = label(_, ok(Reviewer)),
+  Author \= Reviewer,
+  !.
+submit_rule(submit(CR, N)) :-
+  base(CR),
+  N = label('Non-Author-Code-Review', need(_)).
+base(CR) :-
+  gerrit:max_with_block(-2, 2, 'Code-Review', CR).
