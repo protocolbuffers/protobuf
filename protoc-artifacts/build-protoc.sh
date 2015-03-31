36093ca45e544bb92ec67962130aaea31aa7c9d1
Kun Zhang <zhangkun@google.com>
2015-03-31 22:07:13 +0100

    Tweak flags to make statically-linked binary under Cygwin

--- protoc-artifacts/build-protoc.sh
+++ protoc-artifacts/build-protoc.sh
@@ -2,7 +2,18 @@
 
 # Override the default value set in configure.ac that has '-g' which produces
 # huge binary.
-export CXXFLAGS=-DNDEBUG
+export CXXFLAGS="-DNDEBUG"
+
+# Statically link libgcc and libstdc++
+export LDFLAGS="-static-libgcc -static-libstdc++"
+
+# Under Cygwin we use MinGW GCC because the executable produced by Cygwin GCC
+# depends on Cygwin DLL.
+if [[ "$(uname)" == CYGWIN* ]]; then
+  export CC=i686-pc-mingw32-gcc
+  export CXX=i686-pc-mingw32-c++
+  export CXXCPP=i686-pc-mingw32-cpp
+fi
 
 cd $(dirname "$0")/.. && ./configure --disable-shared && make &&
   cd src && (strip protoc || strip protoc.exe)
