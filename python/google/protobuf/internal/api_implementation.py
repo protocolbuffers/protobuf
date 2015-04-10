ef1b747a425496a3ec2433a01f2177f8096d0c0b
Tamir Duberstein <tamird@gmail.com>
2015-04-10 09:45:10 -0400

    [PYTHON] Clarify API version comment

--- python/google/protobuf/internal/api_implementation.py
+++ python/google/protobuf/internal/api_implementation.py
@@ -80,8 +80,8 @@ if _implementation_type != 'python':
 
 # This environment variable can be used to switch between the two
 # 'cpp' implementations, overriding the compile-time constants in the
-# _api_implementation module. Right now only 1 and 2 are valid values. Any other
-# value will be ignored.
+# _api_implementation module. Right now only '2' is supported. Any other
+# value will cause an error to be raised.
 _implementation_version_str = os.getenv(
     'PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION', '2')
 
