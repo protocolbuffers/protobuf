9d9d0b70de43e4c397bbd583a6712d3f4cdf7043
Tamir Duberstein <tamird@gmail.com>
2015-04-11 20:23:45 -0700

    Don't hard-code the python path

--- examples/add_person.py
+++ examples/add_person.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 
 # See README.txt for information and build instructions.
 
--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 #
 # See README for usage instructions.
 import sys
