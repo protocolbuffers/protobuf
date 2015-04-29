cc058e1118d35c8b2fd2298841c40f9303e6ce09
Jon Skeet <jonskeet@google.com>
2015-04-29 08:55:07 +0100

    Remove RPC support. It is expected that third parties will generate service/RPC code themselves - see gRPC as an example.

--- csharp/src/ProtocolBuffers.Serialization/Extensions.cs
+++ csharp/src/ProtocolBuffers.Serialization/Extensions.cs
@@ -181,33 +181,5 @@ namespace Google.ProtocolBuffers
         }
 
         #endregion
-        #region IRpcServerStub Extensions
-
-        /// <summary>
-        /// Used to implement a service endpoint on an HTTP server.  This works with services generated with the
-        /// service_generator_type option set to IRPCDISPATCH.
-        /// </summary>
-        /// <param name="stub">The service execution stub</param>
-        /// <param name="methodName">The name of the method being invoked</param>
-        /// <param name="options">optional arguments for the format reader/writer</param>
-        /// <param name="contentType">The mime type for the input stream</param>
-        /// <param name="input">The input stream</param>
-        /// <param name="responseType">The mime type for the output stream</param>
-        /// <param name="output">The output stream</param>
-        public static void HttpCallMethod(
-#if !NOEXTENSIONS
-            this
-#endif
-            IRpcServerStub stub, string methodName, MessageFormatOptions options,
-            string contentType, Stream input, string responseType, Stream output)
-        {
-            ICodedInputStream codedInput = MessageFormatFactory.CreateInputStream(options, contentType, input);
-            codedInput.ReadMessageStart();
-            IMessageLite response = stub.CallMethod(methodName, codedInput, options.ExtensionRegistry);
-            codedInput.ReadMessageEnd();
-            WriteTo(response, options, responseType, output);
-        }
-
-        #endregion
     }
 }
