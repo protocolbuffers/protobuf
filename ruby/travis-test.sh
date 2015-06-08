716286808ebe1190cccb1b10e85ebdcd15f763cb
Josh Haberman <jhaberman@gmail.com>
2015-06-07 20:08:23 -0700

    Fixed Ruby Travis build.

--- ruby/travis-test.sh
+++ ruby/travis-test.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
-# Exit on any error.
-set -e
+set -e  # exit immediately on error
+set -x  # display all commands
+
+# RVM depends on running inside a login shell.
+# Re-invoke ourselves under a login shell, but prevent infinite recursion.
+if [ "$2" != "login" ]; then
+  bash --login $0 $1 login
+  exit
+fi
 
 test_version() {
   version=$1
-  bash --login -c \
-    "rvm install $version && rvm use $version && \
-     which ruby && \
-     gem install bundler && bundle && \
-     rake test"
+  rvm get stable
+  rvm reload
+  rvm install $version
+  rvm use $version
+  which ruby
+  gem install bundler
+  bundle
+  rake test
 }
 
 test_version $1
