1572be554fc00ba4e93ead9c3f07c05474c1dfb5
Jon Skeet <jonskeet@google.com>
2015-08-04 12:50:59 +0100

    Fix Travis build for conformance

--- travis.sh
+++ travis.sh
@@ -29,6 +29,10 @@ build_cpp_distcheck() {
 }
 
 build_csharp() {
+  # Just for the conformance Makefile
+  ./autogen.sh
+  ./configure
+
   # Install latest version of Mono
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
   echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
