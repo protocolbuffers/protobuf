5e47a68b327f609406038de7650ebcdd47960f75
Bo Yang <teboring@google.com>
2015-08-06 11:23:50 -0700

    Remove unused .pb.h #includes.

--- src/google/protobuf/compiler/objectivec/objectivec_file.cc
+++ src/google/protobuf/compiler/objectivec/objectivec_file.cc
@@ -35,7 +35,6 @@
 #include <google/protobuf/compiler/code_generator.h>
 #include <google/protobuf/io/printer.h>
 #include <google/protobuf/io/zero_copy_stream_impl.h>
-#include <google/protobuf/descriptor.pb.h>
 #include <google/protobuf/stubs/stl_util.h>
 #include <google/protobuf/stubs/strutil.h>
 #include <sstream>
