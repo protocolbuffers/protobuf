528de3bfa69d796d98775b8657443d20c46219c3
Jan Tattermusch <jtattermusch@google.com>
2015-09-23 15:12:17 -0700

    expose GetOutputFile in csharp_names.h

--- src/google/protobuf/compiler/csharp/csharp_helpers.h
+++ src/google/protobuf/compiler/csharp/csharp_helpers.h
@@ -76,8 +76,6 @@ std::string GetUmbrellaClassUnqualifiedName(const FileDescriptor* descriptor);
 // not including the GetFileNamespace part).
 std::string GetUmbrellaClassNestedNamespace(const FileDescriptor* descriptor);
 
-std::string GetClassName(const Descriptor* descriptor);
-
 std::string GetClassName(const EnumDescriptor* descriptor);
 
 std::string GetFieldName(const FieldDescriptor* descriptor);
