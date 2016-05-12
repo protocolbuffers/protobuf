cea1470faee7a840b5c4482d8c3880a833a5e36d
Josh Haberman <jhaberman@gmail.com>
2016-05-12 15:21:47 -0700

    Fixed path for building benchmarks.

--- tests.sh
+++ tests.sh
@@ -39,7 +39,7 @@ build_cpp() {
 
   # Verify benchmarking code can build successfully.
   git submodule update
-  cd third_party/benchmark && cmake -DCMAKE_BUILD_TYPE=Release && make && cd ../..
+  cd third_party/benchmark/src && cmake -DCMAKE_BUILD_TYPE=Release && make && cd ../../..
   cd benchmarks && make && ./generate-datasets && cd ..
 }
 
