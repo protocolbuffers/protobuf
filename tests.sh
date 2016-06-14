39ab16a8075daade1c7e759b0bd3f2325448e4a6
Sergio Campama <kaipi@google.com>
2016-06-14 08:04:42 -0700

    Adding comments on why we are updating ruby

--- tests.sh
+++ tests.sh
@@ -224,8 +224,12 @@ build_objectivec_osx() {
 }
 
 build_objectivec_cocoapods_integration() {
+  # First, load the RVM environment in bash, needed to update ruby.
   source ~/.rvm/scripts/rvm
+  # Update ruby to 2.2.3 as the default one crashes with segmentation faults
+  # when using pod.
   rvm use 2.2.3 --install --binary --fuzzy
+  # Update pod to the latest version.
   gem install cocoapods --no-ri --no-rdoc
   objectivec/Tests/CocoaPods/run_tests.sh
 }
