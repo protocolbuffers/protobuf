37fdbd06da0ec5ef47b42af673bb6f35fed63ff3
Bas Couwenberg <sebastic@xs4all.nl>
2016-08-24 18:57:24 +0200

    Add support for s390x architecture.
    Based on port of Google V8 JavaScript engine to z Systems:
    
    https://github.com/ibmruntimes/v8z
    
    See: #2002

--- src/google/protobuf/stubs/platform_macros.h
+++ src/google/protobuf/stubs/platform_macros.h
@@ -76,6 +76,13 @@
 #elif defined(__PPC__)
 #define GOOGLE_PROTOBUF_ARCH_PPC 1
 #define GOOGLE_PROTOBUF_ARCH_32_BIT 1
+#elif defined(__s390__) || defined(__s390x__)
+#define GOOGLE_PROTOBUF_ARCH_S390 1
+#if defined(__s390x__)
+#define GOOGLE_PROTOBUF_ARCH_64_BIT 1
+#else
+#define GOOGLE_PROTOBUF_ARCH_32_BIT 1
+#endif
 #elif defined(__GNUC__)
 # if (((__GNUC__ == 4) && (__GNUC_MINOR__ >= 7)) || (__GNUC__ > 4))
 // We fallback to the generic Clang/GCC >= 4.7 implementation in atomicops.h
