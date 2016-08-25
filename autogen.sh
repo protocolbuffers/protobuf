1760feb621a913189b90fe8595fffb74bce84598
Feng Xiao <xfxyjwf@gmail.com>
2016-08-25 14:28:59 -0700

    Update gmock links.
    The original link is no longer available.
    
    Change-Id: I2976d3f19c8e818689549fb31ff20af8eae92a07

--- autogen.sh
+++ autogen.sh
@@ -31,10 +31,15 @@ fi
 # directory is set up as an SVN external.
 if test ! -e gmock; then
   echo "Google Mock not present.  Fetching gmock-1.7.0 from the web..."
-  curl $curlopts -O https://googlemock.googlecode.com/files/gmock-1.7.0.zip
-  unzip -q gmock-1.7.0.zip
-  rm gmock-1.7.0.zip
-  mv gmock-1.7.0 gmock
+  curl $curlopts -L -O https://github.com/google/googlemock/archive/release-1.7.0.zip
+  unzip -q release-1.7.0.zip
+  rm release-1.7.0.zip
+  mv googlemock-release-1.7.0 gmock
+
+  curl $curlopts -L -O https://github.com/google/googletest/archive/release-1.7.0.zip
+  unzip -q release-1.7.0.zip
+  rm release-1.7.0.zip
+  mv googletest-release-1.7.0 gmock/gtest
 fi
 
 set -ex
