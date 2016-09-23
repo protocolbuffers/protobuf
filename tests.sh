a289d43608cbfdd27a04807882580afd83d5b048
Joshua Haberman <jhaberman@gmail.com>
2016-09-23 11:07:45 -0700

    Added C++ benchmark. (#1525)

--- tests.sh
+++ tests.sh
@@ -38,6 +38,9 @@ build_cpp() {
   cd conformance && make test_cpp && cd ..
 
   # Verify benchmarking code can build successfully.
+  git submodule init
+  git submodule update
+  cd third_party/benchmark && cmake -DCMAKE_BUILD_TYPE=Release && make && cd ../..
   cd benchmarks && make && ./generate-datasets && cd ..
 }
 
