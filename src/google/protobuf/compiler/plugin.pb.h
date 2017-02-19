4faf145de0c35018a6e74eb8d0e81114b92a7515
kvart2006 <bela.berde@gmail.com>
2017-02-19 03:44:33 +0100

    std coorections

--- src/google/protobuf/compiler/plugin.pb.h
+++ src/google/protobuf/compiler/plugin.pb.h
@@ -235,14 +235,14 @@ class LIBPROTOC_EXPORT Version : public ::google::protobuf::Message /* @@protoc_
   bool has_major() const;
   void clear_major();
   static const int kMajorFieldNumber = 1;
-  ::google::protobuf::int32 major() const;
+  ::google::protobuf::int32 majorV() const;
   void set_major(::google::protobuf::int32 value);
 
   // optional int32 minor = 2;
   bool has_minor() const;
   void clear_minor();
   static const int kMinorFieldNumber = 2;
-  ::google::protobuf::int32 minor() const;
+  ::google::protobuf::int32 minorV() const;
   void set_minor(::google::protobuf::int32 value);
 
   // optional int32 patch = 3;
@@ -702,7 +702,7 @@ inline void Version::clear_major() {
   major_ = 0;
   clear_has_major();
 }
-inline ::google::protobuf::int32 Version::major() const {
+inline ::google::protobuf::int32 Version::majorV() const {
   // @@protoc_insertion_point(field_get:google.protobuf.compiler.Version.major)
   return major_;
 }
@@ -726,7 +726,7 @@ inline void Version::clear_minor() {
   minor_ = 0;
   clear_has_minor();
 }
-inline ::google::protobuf::int32 Version::minor() const {
+inline ::google::protobuf::int32 Version::minorV() const {
   // @@protoc_insertion_point(field_get:google.protobuf.compiler.Version.minor)
   return minor_;
 }
