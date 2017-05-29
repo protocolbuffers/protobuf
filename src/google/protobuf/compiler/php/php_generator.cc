c3ae6a4c6d61c4e849f25474f40e8596c9f3ead6
Paul Yang <TeBoring@users.noreply.github.com>
2017-05-29 15:30:47 -0700

    Add ARRAY for reserved name (#3150)

--- src/google/protobuf/compiler/php/php_generator.cc
+++ src/google/protobuf/compiler/php/php_generator.cc
@@ -49,8 +49,8 @@ const std::string kDescriptorMetadataFile =
     "GPBMetadata/Google/Protobuf/Internal/Descriptor.php";
 const std::string kDescriptorDirName = "Google/Protobuf/Internal";
 const std::string kDescriptorPackageName = "Google\\Protobuf\\Internal";
-const char* const kReservedNames[] = {"Empty", "ECHO"};
-const int kReservedNamesSize = 2;
+const char* const kReservedNames[] = {"ARRAY", "Empty", "ECHO"};
+const int kReservedNamesSize = 3;
 
 namespace google {
 namespace protobuf {
