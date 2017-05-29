f7f4881604fb34de3a0b7b6466843e91cac09726
Paul Yang <TeBoring@users.noreply.github.com>
2017-05-29 15:30:47 -0700

    Add ARRAY for reserved name (#3150)

--- php/src/Google/Protobuf/descriptor.php
+++ php/src/Google/Protobuf/descriptor.php
@@ -236,7 +236,7 @@ function getClassNamePrefix(
         return $prefix;
     }
 
-    $reserved_words = array("Empty");
+    $reserved_words = array("Empty", "ECHO", "ARRAY");
     foreach ($reserved_words as $reserved_word) {
         if ($classname === $reserved_word) {
             if ($file_proto->getPackage() === "google.protobuf") {
