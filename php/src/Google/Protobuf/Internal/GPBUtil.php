d8bc5f5c557a1c11146d135eb23ab07c5cbf91fe
Ryan Gordon <ryan@quizlet.com>
2017-06-19 18:15:49 -0700

    Switch to addEnumType to fix fatal error (#3225)
    * Switch to addEnumType to fix fatal error
    
    * Fixing more cases of HHVM incompatibility
    
    * Updating tests to be hhvm compatible
    
    * Fixing tests
    
    * Fixing merge
    
    * Don't use call_user_func, should hopefully fix tests
    
    * Fixing spelling
    
    * Fixing another misspelling in a test
    
    * Fixing placement of append and kvUpdate functions
    
    * Actually fix function helpers
    
    * Remove double addEnumType. How did this ever work?
    
    * Fixing a couple more tests
    
    * Only use the setter if the return value isn't an object

--- php/src/Google/Protobuf/Internal/GPBUtil.php
+++ php/src/Google/Protobuf/Internal/GPBUtil.php
@@ -299,18 +299,6 @@ class GPBUtil
         $class_name_without_package =
             static::getClassNameWithoutPackage($message_name_without_package, $file_proto);
 
-        $option = $file_proto->getOptions();
-        if (!is_null($option) && $option->hasPhpNamespace()) {
-            $namespace = $option->getPhpNamespace();
-            if ($namespace !== "") {
-                $classname = $namespace . "\\" . $class_name_without_package;
-                return;
-            } else {
-                $classname = $class_name_without_package;
-                return;
-            }
-        }
-
         if ($package === "") {
             $classname = $class_name_without_package;
         } else {
--- php/src/Google/Protobuf/Internal/Message.php
+++ php/src/Google/Protobuf/Internal/Message.php
@@ -373,7 +373,7 @@ class Message
             $getter = $field->getGetter();
             while ($input->bytesUntilLimit() > 0) {
                 self::parseFieldFromStreamNoTag($input, $field, $value);
-                $this->$getter()[] = $value;
+                $this->appendHelper($field, $value);
             }
             $input->popLimit($limit);
             return;
@@ -382,11 +382,9 @@ class Message
         }
 
         if ($field->isMap()) {
-            $getter = $field->getGetter();
-            $this->$getter()[$value->getKey()] = $value->getValue();
+            $this->kvUpdateHelper($field, $value->getKey(), $value->getValue());
         } else if ($field->isRepeated()) {
-            $getter = $field->getGetter();
-            $this->$getter()[] = $value;
+            $this->appendHelper($field, $value);
         } else {
             $setter = $field->getSetter();
             $this->$setter($value);
@@ -533,9 +531,10 @@ class Message
                           $klass = $value_field->getMessageType()->getClass();
                           $copy = new $klass;
                           $copy->mergeFrom($value);
-                          $this->$getter()[$key] = $copy;
+
+                          $this->kvUpdateHelper($field, $key, $copy);
                       } else {
-                          $this->$getter()[$key] = $value;
+                          $this->kvUpdateHelper($field, $key, $value);
                       }
                   }
               }
@@ -546,9 +545,9 @@ class Message
                           $klass = $field->getMessageType()->getClass();
                           $copy = new $klass;
                           $copy->mergeFrom($tmp);
-                          $this->$getter()[] = $copy;
+                          $this->appendHelper($field, $copy);
                       } else {
-                          $this->$getter()[] = $tmp;
+                          $this->appendHelper($field, $tmp);
                       }
                   }
               }
@@ -895,5 +894,31 @@ class Message
             $size += $this->fieldByteSize($field);
         }
         return $size;
+    }
+
+    private function appendHelper($field, $append_value)
+    {
+        $getter = $field->getGetter();
+        $setter = $field->getSetter();
+
+        $field_arr_value = $this->$getter();
+        $field_arr_value[] = $append_value;
+
+        if (!is_object($field_arr_value)) {
+            $this->$setter($field_arr_value);
+        }
+    }
+
+    private function kvUpdateHelper($field, $update_key, $update_value)
+    {
+        $getter = $field->getGetter();
+        $setter = $field->getSetter();
+
+        $field_arr_value = $this->$getter();
+        $field_arr_value[$update_key] = $update_value;
+
+        if (!is_object($field_arr_value)) {
+            $this->$setter($field_arr_value);
+        }
     }
 }
