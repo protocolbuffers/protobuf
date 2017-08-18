4b7f1772fcef18507f6852fed0425ce553c422c5
zsx <zsx@zsxsoft.com>
2017-08-18 10:39:33 +0800

    Follow the appveyor document, add C++ building environment

--- appveyor.bat
+++ appveyor.bat
@@ -36,6 +36,9 @@ goto :EOF
 
 :build_php
 echo Preparing environment
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+
 echo Downloading PHP SDK
 curl -L -o php-sdk.zip https://github.com/OSTC/php-sdk-binary-tools/archive/php-sdk-2.0.9.zip || goto :error
 7z x php-sdk.zip
