2d930ab60b021fe120137d8199152dd96ac36616
Bo Yang <teboring@google.com>
2017-09-20 15:46:00 -0700

    Enforce all error report for php tests.

--- php/src/Google/Protobuf/Internal/DescriptorPool.php
+++ php/src/Google/Protobuf/Internal/DescriptorPool.php
@@ -109,18 +109,30 @@ class DescriptorPool
 
     public function getDescriptorByClassName($klass)
     {
-        return $this->class_to_desc[$klass];
+        if (isset($this->class_to_desc[$klass])) {
+            return $this->class_to_desc[$klass];
+        } else {
+            return null;
+        }
     }
 
     public function getEnumDescriptorByClassName($klass)
     {
-        return $this->class_to_enum_desc[$klass];
+        if (isset($this->class_to_enum_desc[$klass])) {
+            return $this->class_to_enum_desc[$klass];
+        } else {
+            return null;
+        }
     }
 
     public function getDescriptorByProtoName($proto)
     {
-        $klass = $this->proto_to_class[$proto];
-        return $this->class_to_desc[$klass];
+        if (isset($this->proto_to_class[$proto])) {
+            $klass = $this->proto_to_class[$proto];
+            return $this->class_to_desc[$klass];
+        } else {
+          return null;
+        }
     }
 
     public function getEnumDescriptorByProtoName($proto)
---
+++ php/tests/bootstrap_phpunit.php
@@ -0,0 +1,3 @@
+<?php
+
+error_reporting(E_ALL);
