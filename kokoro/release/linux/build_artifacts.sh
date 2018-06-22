7a0db04a102fe87c175fcfe1c1c50b7dd9bb57dc
Bo Yang <teboring@google.com>
2018-06-22 15:58:37 -0700

    Install bundler

--- kokoro/release/linux/build_artifacts.sh
+++ kokoro/release/linux/build_artifacts.sh
@@ -8,7 +8,7 @@ cd $(dirname $0)/../../..
 source kokoro/release/linux/prepare_build.sh
 
 # ruby environment
-bash kokoro/release/linux/ruby/ruby_build_environment.sh
+source kokoro/release/linux/ruby/ruby_build_environment.sh
 
 # build artifacts
 bash kokoro/release/linux/ruby/ruby_build.sh
