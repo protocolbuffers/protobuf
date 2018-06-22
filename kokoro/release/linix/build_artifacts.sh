9670c39192ac9e6729f8fbd822baea6fb6b66527
Bo Yang <teboring@google.com>
2018-06-22 14:50:17 -0700

    Add kokoro config to build ruby gem on linux

---
+++ kokoro/release/linix/build_artifacts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+# change to repo root
+cd $(dirname $0)/../../..
+
+source kokoro/release/linux/prepare_build
+
+# ruby environment
+bash kokoro/release/linux/ruby/ruby_build_environment.sh
+
+gem install rubygems-update
+update_rubygems
+
+# build artifacts
+bash kokoro/release/macos/ruby/ruby_build.sh
