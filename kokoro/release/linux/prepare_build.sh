f7ada1280fac4af717d478e6a9765d3f02b418b3
Paul Yang <TeBoring@users.noreply.github.com>
2018-06-25 11:14:49 -0700

    Build ruby gem on kokoro (#4819)
    * Install rake compiler
    
    * Add kokoro config to build ruby gem on linux
    
    * Rename from linix to linux
    
    * Fix prepare_build.sh name
    
    * Clean up
    
    * Install bundler
    
    * Install bundler
    
    * Use c99 in order to build gem on mingw-32 on ruby 2.0.0
    
    See https://github.com/rake-compiler/rake-compiler-dock/issues/4
    
    * Move c99 config to extcofig.rb

---
+++ kokoro/release/linux/prepare_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Move docker's storage location to scratch disk so we don't run out of space.
+echo 'DOCKER_OPTS="${DOCKER_OPTS} --graph=/tmpfs/docker"' | sudo tee --append /etc/default/docker
+# Use container registry mirror for pulling docker images (should make downloads faster)
+# See https://cloud.google.com/container-registry/docs/using-dockerhub-mirroring
+echo 'DOCKER_OPTS="${DOCKER_OPTS} --registry-mirror=https://mirror.gcr.io"' | sudo tee --append /etc/default/docker
+sudo service docker restart
+
+# Download Docker images from DockerHub
+export DOCKERHUB_ORGANIZATION=grpctesting
+
+# All artifacts come here
+mkdir artifacts
+export ARTIFACT_DIR=$(pwd)/artifacts
+
