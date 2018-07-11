b5975c109aeb3d4f77d662e7b7222b87c653c69d
Feng Xiao <xfxyjwf@gmail.com>
2018-07-11 16:34:58 -0700

    Add kokoro bazel configs for 3.6.x branch.

---
+++ kokoro/linux/bazel/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Build file to set up and run tests
+
+# Change to repo root
+cd $(dirname $0)/../../..
+
+git submodule update --init --recursive
+bazel test :protobuf_test
+
+cd examples
+bazel build :all
