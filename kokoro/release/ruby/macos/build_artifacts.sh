5fa1f6b2e9255616e68a03dee4274b0072d406ec
Feng Xiao <xfxyjwf@gmail.com>
2018-07-11 16:25:55 -0700

    Make ruby release configs consistent with protoc.

--- kokoro/release/ruby/macos/build_artifacts.sh
+++ kokoro/release/ruby/macos/build_artifacts.sh
@@ -3,17 +3,17 @@
 set -ex
 
 # change to repo root
-cd $(dirname $0)/../../..
+cd $(dirname $0)/../../../..
 
 # all artifacts come here
 mkdir artifacts
 export ARTIFACT_DIR=$(pwd)/artifacts
 
 # ruby environment
-bash kokoro/release/macos/ruby/ruby_build_environment.sh
+bash kokoro/release/ruby/macos/ruby/ruby_build_environment.sh
 
 gem install rubygems-update
 update_rubygems
 
 # build artifacts
-bash kokoro/release/macos/ruby/ruby_build.sh
+bash kokoro/release/ruby/macos/ruby/ruby_build.sh
