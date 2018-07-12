0a59054c30e4f0ba10f10acfc1d7f3814c63e1a7
Ben Webb <ben@salilab.org>
2018-07-12 10:58:10 -0700

    Add Python 3.7 compatibility (#4862)
    Compilation of Python wrappers fails with Python 3.7 because
    the Python folks changed their C API such that
    PyUnicode_AsUTF8AndSize() now returns a const char* rather
    than a char*. Add a patch to work around. Relates #4086.

--- python/google/protobuf/pyext/descriptor.cc
+++ python/google/protobuf/pyext/descriptor.cc
@@ -56,7 +56,7 @@
   #endif
   #define PyString_AsStringAndSize(ob, charpp, sizep) \
     (PyUnicode_Check(ob)? \
-       ((*(charpp) = PyUnicode_AsUTF8AndSize(ob, (sizep))) == NULL? -1: 0): \
+       ((*(charpp) = const_cast<char*>(PyUnicode_AsUTF8AndSize(ob, (sizep)))) == NULL? -1: 0): \
        PyBytes_AsStringAndSize(ob, (charpp), (sizep)))
 #endif
 
