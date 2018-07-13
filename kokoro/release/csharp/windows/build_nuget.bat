937884476e8a5873af4e2a44762ed76865ffd877
Jan Tattermusch <jtattermusch@google.com>
2018-07-13 13:26:56 +0200

    overcome the global.json constraint with a hack

--- kokoro/release/csharp/windows/build_nuget.bat
+++ kokoro/release/csharp/windows/build_nuget.bat
@@ -2,4 +2,13 @@
 cd /d %~dp0\..\..\..\..
 
 cd csharp
+
+@rem see what dotnet version is available
+dotnet --version
+
+@rem TODO(jtattermusch): Kokoro workers currently only have dotnet SDK 2.1.3
+@rem so we just overwrite the SDK requirement in global.json as the results
+@rem should be fully compatible.
+echo { "sdk": { "version": "2.1.3" } } >global.json
+
 call build_packages.bat
