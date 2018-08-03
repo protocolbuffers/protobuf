dd48cbf349f651d98c13aabf0cc4dbf6b64dc0ab
Bo Yang <teboring@google.com>
2018-08-03 14:14:43 -0700

    Add config for building python wheel

---
+++ kokoro/release/python/linux/build_artifacts.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -ex
+
+# change to repo root
+cd $(dirname $0)/../../../..
+
+git checkout python-wheel
+
+export REPO_DIR=protobuf
+export BUILD_COMMIT=v3.6.1
+export BUILD_VERSION=3.6.1
+export PLAT=x86_64
+export UNICODE_WIDTH=32
+export MACOSX_DEPLOYMENT_TARGET=10.9
+
+build_artifact_version() {
+  MB_PYTHON_VERSION=$1
+
+  source multibuild/common_utils.sh
+  source multibuild/travis_steps.sh
+  before_install
+
+  clean_code $REPO_DIR $BUILD_COMMIT
+  build_wheel $REPO_DIR/python $PLAT
+
+  install_run $PLAT
+}
+
+build_artifact_version 2.7
+build_artifact_version 3.3
+build_artifact_version 3.4
+build_artifact_version 3.5
+build_artifact_version 3.6
+
+
