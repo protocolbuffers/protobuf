f390af765d0800933500134b5a3b15b8bf8b0e74
Bo Yang <teboring@google.com>
2018-08-10 20:22:44 +0000

    Fix python dir name

--- kokoro/release/python/windows/build_wheel.bat
+++ kokoro/release/python/windows/build_wheel.bat
@@ -1,14 +1,14 @@
-if %PYTHON%==C:\Python35 set generator=Visual Studio 14
-if %PYTHON%==C:\Python35 set vcplatform=Win32
+if %PYTHON%==C:\python35_32bit set generator=Visual Studio 14
+if %PYTHON%==C:\python35_32bit set vcplatform=Win32
 
-if %PYTHON%==C:\Python35-x64 set generator=Visual Studio 14 Win64
-if %PYTHON%==C:\Python35-x64 set vcplatform=x64
+if %PYTHON%==C:\python35 set generator=Visual Studio 14 Win64
+if %PYTHON%==C:\python35 set vcplatform=x64
 
-if %PYTHON%==C:\Python36 set generator=Visual Studio 14
-if %PYTHON%==C:\Python36 set vcplatform=Win32
+if %PYTHON%==C:\python36_32bit set generator=Visual Studio 14
+if %PYTHON%==C:\python36_32bit set vcplatform=Win32
 
-if %PYTHON%==C:\Python36-x64 set generator=Visual Studio 14 Win64
-if %PYTHON%==C:\Python36-x64 set vcplatform=x64
+if %PYTHON%==C:\python36 set generator=Visual Studio 14 Win64
+if %PYTHON%==C:\python36 set vcplatform=x64
 
 CALL build_python_env.bat
 
