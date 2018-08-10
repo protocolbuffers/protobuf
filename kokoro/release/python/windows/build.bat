7cb631fa2bbb07b8370c02663f429cb637652c1e
Bo Yang <teboring@google.com>
2018-08-10 20:56:41 +0000

    Debug

--- kokoro/release/python/windows/build.bat
+++ kokoro/release/python/windows/build.bat
@@ -30,4 +30,5 @@ cd python
 REM sed -i 's/\ extra_compile_args\ =\ \[\]/\ extra_compile_args\ =\ \[\'\/MT\'\]/g' setup.py
 
 python setup.py bdist_wheel --cpp_implementation --compile_static_extension
+dir dist
 cd ..\..
