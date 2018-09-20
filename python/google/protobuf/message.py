55282b8cd551388acd07261c457b5546a6134c68
Josh Kelley <joshkel@gmail.com>
2018-09-20 08:37:43 -0400

    Fix docstring

--- python/google/protobuf/message.py
+++ python/google/protobuf/message.py
@@ -178,8 +178,7 @@ class Message(object):
   def ParseFromString(self, serialized):
     """Parse serialized protocol buffer data into this message.
 
-    Like MergeFromString(), except we clear the object first and
-    do not return the value that MergeFromString returns.
+    Like MergeFromString(), except we clear the object first.
     """
     self.Clear()
     return self.MergeFromString(serialized)
