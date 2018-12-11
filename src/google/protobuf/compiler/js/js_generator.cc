e319d738a575bdaf220e21a4e3bd66d28c7f5f6e
Travis DePrato <travigd@umich.edu>
2018-12-11 10:41:26 -0500

    Add Javascript convenience getters/setters.

--- src/google/protobuf/compiler/js/js_generator.cc
+++ src/google/protobuf/compiler/js/js_generator.cc
@@ -2840,6 +2840,21 @@ void Generator::GenerateClassField(const GeneratorOptions& options,
     }
   }
 
+  // Define getter method
+  printer->Print(
+      "Object.defineProperty($class$.prototype, \"$fieldname$\", {\n"
+      "  set: function(value) {\n"
+      "    this.$settername$(value);\n"
+      "  },\n"
+      "  get: function() {\n"
+      "    return this.$gettername$();\n"
+      "  },\n"
+      "});\n\n\n",
+      "class", GetMessagePath(options, field->containing_type()),
+      "fieldname", JSObjectFieldName(options, field),
+      "gettername", "get" + JSGetterName(options, field),
+      "settername", "set" + JSGetterName(options, field));
+
   // Generate clearFoo() method for map fields, repeated fields, and other
   // fields with presence.
   if (field->is_map()) {
