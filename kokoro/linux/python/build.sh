8f0d747c202d34a1450d8cb3827c1f20768544d5
Bo Yang <teboring@google.com>
2018-12-18 13:12:21 -0800

    Update docker image

--- kokoro/linux/python/build.sh
+++ kokoro/linux/python/build.sh
@@ -11,7 +11,7 @@
 cd $(dirname $0)/../../..
 
 export DOCKERFILE_DIR=kokoro/linux/64-bit
-export DOCKER_RUN_SCRIPT=kokoro/linux/pull_request_in_docker2.sh
+export DOCKER_RUN_SCRIPT=kokoro/linux/pull_request_in_docker.sh
 export OUTPUT_DIR=testoutput
 export TEST_SET="python_jessie"
-./kokoro/linux/build_and_run_docker.sh
+./kokoro/linux/build_and_run_docker2.sh
