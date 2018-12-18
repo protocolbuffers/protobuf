f919615b384309fae1a6dc299f25f7ba747e324a
Bo Yang <teboring@google.com>
2018-12-18 13:27:45 -0800

    Change call to build_and_run_docker.sh

--- kokoro/linux/python/build.sh
+++ kokoro/linux/python/build.sh
@@ -14,4 +14,4 @@ export DOCKERFILE_DIR=kokoro/linux/64-bit
 export DOCKER_RUN_SCRIPT=kokoro/linux/pull_request_in_docker.sh
 export OUTPUT_DIR=testoutput
 export TEST_SET="python_jessie"
-./kokoro/linux/build_and_run_docker2.sh
+./kokoro/linux/build_and_run_docker.sh
