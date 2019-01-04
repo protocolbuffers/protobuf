4aa809b7f985f22c2707bc6de2f49bd854fee368
Bo Yang <teboring@google.com>
2019-01-04 11:59:31 -0800

    Add docker hub organization

--- kokoro/release/ruby/linux/prepare_build.sh
+++ kokoro/release/ruby/linux/prepare_build.sh
@@ -8,6 +8,7 @@ echo 'DOCKER_OPTS="${DOCKER_OPTS} --registry-mirror=https://mirror.gcr.io"' | su
 sudo service docker restart
 
 # Download Docker images from DockerHub
+DOCKERHUB_ORGANIZATION=protobuftesting
 DOCKERFILE_DIR=kokoro/linux/dockerfile/release/ruby_rake_compiler
 DOCKERFILE_PREFIX=$(basename $DOCKERFILE_DIR)
 export RAKE_COMPILER_DOCK_IMAGE=${DOCKERHUB_ORGANIZATION}/${DOCKERFILE_PREFIX}_$(sha1sum $DOCKERFILE_DIR/Dockerfile | cut -f1 -d\ )
