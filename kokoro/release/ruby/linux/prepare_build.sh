ed78083039a8a9922e01ecd827abdb8b66467cf5
Bo Yang <teboring@google.com>
2019-01-04 10:25:02 -0800

    Update raker compiler docker image for ruby2.6

--- kokoro/release/ruby/linux/prepare_build.sh
+++ kokoro/release/ruby/linux/prepare_build.sh
@@ -9,6 +9,7 @@ sudo service docker restart
 
 # Download Docker images from DockerHub
 export DOCKERHUB_ORGANIZATION=grpctesting
+export RAKE_COMPILER_DOCK_IMAGE=grpctesting/rake-compiler-dock_53c22085d091183c528303791e7771359f699bcf
 
 # All artifacts come here
 mkdir artifacts
