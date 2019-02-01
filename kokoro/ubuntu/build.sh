eb633dbe3c6cae3c97c49b2c9c4c88ccc2f4ad56
Josh Haberman <jhaberman@gmail.com>
2019-02-01 15:54:51 -0800

    Query CMake version.

--- kokoro/ubuntu/build.sh
+++ kokoro/ubuntu/build.sh
@@ -3,6 +3,11 @@
 # Install the latest version of Bazel.
 use_bazel.sh latest
 
+# Verify/query CMake
+echo PATH=$PATH
+ls -l `which cmake`
+cmake --version
+
 # Log the bazel path and version.
 which bazel
 bazel version
