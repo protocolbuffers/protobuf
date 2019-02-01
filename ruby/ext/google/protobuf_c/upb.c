ba28d504a4a6c2cee3563ab7b1991ab0f57e3611
Bo Yang <paulyang1211@gmail.com>
2019-01-31 21:17:19 -0800

    Define XOPEN_SOURCE directly

--- ruby/ext/google/protobuf_c/upb.c
+++ ruby/ext/google/protobuf_c/upb.c
@@ -1,9 +1,7 @@
 // Amalgamated source file
 /* Need to define _XOPEN_SOURCE before including time.h to make strptime work. */
 
-#ifndef _XOPEN_SOURCE
 #define _XOPEN_SOURCE 700
-#endif
 #include <time.h>
 
 #include "upb.h"
