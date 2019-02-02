2eca0a7c46df8e125d81764fdfdb40829abcad44
Bo Yang <teboring@google.com>
2019-02-01 16:19:07 -0800

    Make test run on the current commit

--- kokoro/release/python/windows/build_artifacts.bat
+++ kokoro/release/python/windows/build_artifacts.bat
@@ -10,8 +10,15 @@ set UNICODE=ON
 set PB_TEST_DEP="six==1.9"
 set OTHER_TEST_DEP="setuptools==38.5.1"
 set OLD_PATH=C:\Program Files (x86)\MSBuild\14.0\bin\;%PATH%
-for /f "tokens=*" %%i in ( 'grep -i "version" python/google/protobuf/__init__.py ^| grep -o "'.*'"' ) do set BUILD_VERSION=%%i
-set BUILD_COMMIT=v%BUILD_VERSION:'=%
+REM for /f "tokens=*" %%i in ( 'grep -i "version" python/google/protobuf/__init__.py ^| grep -o "'.*'"' ) do set BUILD_VERSION=%%i
+REM set BUILD_COMMIT=v%BUILD_VERSION:'=%
+
+if not "%KOKORO_JOB_NAME%" (
+  set BUILD_COMMIT=master
+) else (
+  set BUILD_COMMIT=`echo "%KOKORO_JOB_NAME%" | cut -d '/' -f 3`
+  for /f "tokens=3 delims=/ " %%i in ("%KOKORO_JOB_NAME%") do set BUILD_COMMIT=%%i
+)
 
 REM Fetch multibuild
 git clone https://github.com/matthew-brett/multibuild.git
