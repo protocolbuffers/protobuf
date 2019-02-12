39c0947893a31123b705e17ef11bd62f262cea68
Joe Bolinger <jbolinger@google.com>
2019-02-12 09:50:57 -0800

    implement to_s for message types (#5710)

--- ruby/ext/google/protobuf_c/message.c
+++ ruby/ext/google/protobuf_c/message.c
@@ -626,6 +626,7 @@ VALUE build_class_from_descriptor(Descriptor* desc) {
   rb_define_method(klass, "to_h", Message_to_h, 0);
   rb_define_method(klass, "to_hash", Message_to_h, 0);
   rb_define_method(klass, "inspect", Message_inspect, 0);
+  rb_define_method(klass, "to_s", Message_inspect, 0);
   rb_define_method(klass, "[]", Message_index, 1);
   rb_define_method(klass, "[]=", Message_index_set, 2);
   rb_define_singleton_method(klass, "decode", Message_decode, 1);
