48e591af1bb4bbe2bd27d36b6b911854008a81ea
Mike Moore <mike@blowmage.com>
2019-02-13 21:07:20 -0700

    Correct assertion on frozen error message
    The second argument for the method assert_raise is the message
    to show when the assertion fails. It does not check the error
    object's message.
    Add an additional assertion that does check the error's message.

--- ruby/tests/common_tests.rb
+++ ruby/tests/common_tests.rb
@@ -1125,9 +1125,8 @@ module CommonTests
     m.optional_int32 = 10
     m.freeze
 
-    assert_raise(FrozenError, 'can\'t modify frozen BasicTestProto2::TestMessage') do
-      m.optional_int32 = 20
-    end
+    frozen_error = assert_raise(FrozenError) { m.optional_int32 = 20 }
+    assert_equal "can't modify frozen #{proto_module}::TestMessage", frozen_error.message
     assert_equal 10, m.optional_int32
     assert_equal true, m.frozen?
 
