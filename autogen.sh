08f0318d27570233c281081550f8a1c986c1f0c2
claud.kim <claud.kim81@gmail.com>
2019-02-26 02:35:16 +0900

    autogen test url not found bug fix

--- autogen.sh
+++ autogen.sh
@@ -19,8 +19,9 @@ fi
 # directory is set up as an SVN external.
 if test ! -e gtest; then
   echo "Google Test not present.  Fetching gtest-1.5.0 from the web..."
-  curl http://googletest.googlecode.com/files/gtest-1.5.0.tar.bz2 | tar jx
-  mv gtest-1.5.0 gtest
+  curl -L https://github.com/google/googletest/archive/release-1.5.0.tar.gz | tar zx
+  mv googletest-release-1.5.0 gtest
+
 fi
 
 set -ex
