80ee4e1cbea6a08da29242e5ce146b8031d930b2
Daniel Kurka <dankurka@google.com>
2019-03-21 14:26:17 -0700

    Revert "Remove unused f expression from jscode gen. (#5573)"
    The removed variable here is actually used breaking downstream bazel
    builds.

--- src/google/protobuf/compiler/js/js_generator.cc
+++ src/google/protobuf/compiler/js/js_generator.cc
@@ -2272,7 +2272,7 @@ void Generator::GenerateClassToObject(const GeneratorOptions& options,
       " * @suppress {unusedLocalVariables} f is only used for nested messages\n"
       " */\n"
       "$classname$.toObject = function(includeInstance, msg) {\n"
-      "  var obj = {",
+      "  var f, obj = {",
       "classname", GetMessagePath(options, desc));
 
   bool first = true;
