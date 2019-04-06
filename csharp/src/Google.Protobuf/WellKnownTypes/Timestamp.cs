83670075a8e8a3b0b91d309af300634676faba4f
Hao Nguyen <haon@google.com>
2019-04-05 18:52:05 -0700

    Update generated proto files

--- csharp/src/Google.Protobuf/WellKnownTypes/Timestamp.cs
+++ csharp/src/Google.Protobuf/WellKnownTypes/Timestamp.cs
@@ -113,11 +113,13 @@ namespace Google.Protobuf.WellKnownTypes {
   /// 01:30 UTC on January 15, 2017.
   ///
   /// In JavaScript, one can convert a Date object to this format using the
-  /// standard [toISOString()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString)
+  /// standard
+  /// [toISOString()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString)
   /// method. In Python, a standard `datetime.datetime` object can be converted
-  /// to this format using [`strftime`](https://docs.python.org/2/library/time.html#time.strftime)
-  /// with the time format spec '%Y-%m-%dT%H:%M:%S.%fZ'. Likewise, in Java, one
-  /// can use the Joda Time's [`ISODateTimeFormat.dateTime()`](
+  /// to this format using
+  /// [`strftime`](https://docs.python.org/2/library/time.html#time.strftime) with
+  /// the time format spec '%Y-%m-%dT%H:%M:%S.%fZ'. Likewise, in Java, one can use
+  /// the Joda Time's [`ISODateTimeFormat.dateTime()`](
   /// http://www.joda.org/joda-time/apidocs/org/joda/time/format/ISODateTimeFormat.html#dateTime%2D%2D
   /// ) to obtain a formatter capable of generating timestamps in this format.
   /// </summary>
