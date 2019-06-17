2a6adf63c8501b840d0e4360fe2a9db6922244d2
Benjamin Peterson <benjamin@python.org>
2019-06-17 15:15:34 -0700

    Import MutableSequence from collections.abc on Python 3.
    Fixes https://github.com/protocolbuffers/protobuf/issues/5335.

--- python/google/protobuf/pyext/message.cc
+++ python/google/protobuf/pyext/message.cc
@@ -2991,8 +2991,12 @@ bool InitProto2MessageModule(PyObject *m) {
         reinterpret_cast<PyObject*>(
             &RepeatedCompositeContainer_Type));
 
-    // Register them as collections.Sequence
+    // Register them as MutableSequence.
+#if PY_MAJOR_VERSION >= 3
+    ScopedPyObjectPtr collections(PyImport_ImportModule("collections.abc"));
+#else
     ScopedPyObjectPtr collections(PyImport_ImportModule("collections"));
+#endif
     if (collections == NULL) {
       return false;
     }
