e3106bb6fb40735f8a37d291c3590defb009360e
Tomo Suzuki <suztomo@google.com>
2019-06-28 14:19:54 -0400

    Added comment

--- tests.sh
+++ tests.sh
@@ -231,20 +231,24 @@ build_java_compatibility() {
   ./test.sh 3.0.0-beta-4
 }
 build_java_linkage_monitor() {
+  # Linkage Monitor checks compatibility with other Google libraries
+  # https://github.com/GoogleCloudPlatform/cloud-opensource-java/tree/master/linkage-monitor
+
   use_java jdk8
   internal_build_cpp
   cd java
 
-  # Set java artifact version with SNAPSHOT. Linkage Monitor works with SNAPSHOT versions.
+  # Sets java artifact version with SNAPSHOT, as Linkage Monitor looks for SNAPSHOT versions.
   # Example: "3.9.0" (without 'rc')
   VERSION=`grep '<version>' pom.xml |head -1 |perl -nle 'print $1 if m/<version>(\d+\.\d+.\d+)/'`
   cd bom
   $MVN versions:set -DnewVersion=${VERSION}-SNAPSHOT
   cd ..
   $MVN versions:set -DnewVersion=${VERSION}-SNAPSHOT
+  # Installs the snapshot version locally
   $MVN install -Dmaven.test.skip=true
 
-  # Linkage Monitor
+  # Linkage Monitor uses the snapshot versions to verify compatibility
   JAR=linkage-monitor-latest-all-deps.jar
   curl -v -O "https://storage.googleapis.com/cloud-opensource-java-linkage-monitor/${JAR}"
   # Fails if there's new linkage errors compared with baseline
