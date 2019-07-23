fa4befb6dcbfb705c56d115488cbc69f5ff12605
Jie Luo <jieluo@google.com>
2019-07-23 15:50:14 -0700

    Update last version number

--- tests.sh
+++ tests.sh
@@ -148,8 +148,8 @@ build_csharp() {
   # Run csharp compatibility test between 3.0.0 and the current version.
   csharp/compatibility_tests/v3.0.0/test.sh 3.0.0
 
-  LAST_MAJOR=3.9.0
-  # Run csharp compatibility test between lat major and the curretn version.
+  LAST_MAJOR=3.8.0
+  # Run csharp compatibility test between last major and the current version.
   csharp/compatibility_tests/v3.0.0/test.sh $LAST_MAJOR
 }
 
