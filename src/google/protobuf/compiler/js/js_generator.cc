ee7d87326996a3df4adc702f7bdc33bae3dde8f3
Paul Yang <TeBoring@users.noreply.github.com>
2019-07-28 12:52:33 -0700

    Revert "Fixed JS parsing of default map values (#6394)"
    This reverts commit dcc8ffd9c6fab3c82d3d6642a35a584aa0a3f64b.

--- src/google/protobuf/compiler/js/js_generator.cc
+++ src/google/protobuf/compiler/js/js_generator.cc
@@ -3142,7 +3142,6 @@ void Generator::GenerateClassDeserializeBinaryField(
       printer->Print(", null");
     }
     printer->Print(", $defaultKey$", "defaultKey", JSFieldDefault(key_field));
-    printer->Print(", $defaultValue$", "defaultValue", JSFieldDefault(value_field));
     printer->Print(");\n");
     printer->Print("         });\n");
   } else {
