2610eb6675f36062a70a105fccd436468169393c
Paul Yang <TeBoring@users.noreply.github.com>
2019-09-16 15:35:53 -0700

    Add license (#6651)

--- php/ext/google/protobuf/type_check.c
+++ php/ext/google/protobuf/type_check.c
@@ -28,6 +28,60 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+//                The Zend Engine License, version 2.00
+// Copyright (c) 1999-2002 Zend Technologies Ltd. All rights reserved.
+// --------------------------------------------------------------------
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, is permitted provided that the following conditions
+// are met:
+//
+//   1. Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//   2. Redistributions in binary form must reproduce the above
+//      copyright notice, this list of conditions and the following
+//      disclaimer in the documentation and/or other materials provided
+//      with the distribution.
+//
+//   3. The names "Zend" and "Zend Engine" must not be used to endorse
+//      or promote products derived from this software without prior
+//      permission from Zend Technologies Ltd. For written permission,
+//      please contact license@zend.com.
+//
+//   4. Zend Technologies Ltd. may publish revised and/or new versions
+//      of the license from time to time. Each version will be given a
+//      distinguishing version number.
+//      Once covered code has been published under a particular version
+//      of the license, you may always continue to use it under the
+//      terms of that version. You may also choose to use such covered
+//      code under the terms of any subsequent version of the license
+//      published by Zend Technologies Ltd. No one other than Zend
+//      Technologies Ltd. has the right to modify the terms applicable
+//      to covered code created under this License.
+//
+//   5. Redistributions of any form whatsoever must retain the following
+//      acknowledgment:
+//      "This product includes the Zend Engine, freely available at
+//      http://www.zend.com"
+//
+//   6. All advertising materials mentioning features or use of this
+//      software must display the following acknowledgment:
+//      "The Zend Engine is freely available at http://www.zend.com"
+//
+// THIS SOFTWARE IS PROVIDED BY ZEND TECHNOLOGIES LTD. ``AS IS'' AND
+// ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL ZEND
+// TECHNOLOGIES LTD.  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+// USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
 #include <Zend/zend_operators.h>
 #include <Zend/zend_exceptions.h>
 
