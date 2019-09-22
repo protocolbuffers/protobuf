7b473fbec78dcbdba5e1d24d497385ab43d1d831
Andrei-Florin BENCSIK <andrei.bencsik@gmail.com>
2019-09-22 10:01:30 +0300

    Remove unused function and add more UTs
    When browsing around the strutil files I found a function
    that was never referenced inside the code base
    "void StripString(string* s, const char* remove,
    -                                 char replacewith);"
    
    The name was kind of misleading as well and it seems like
    it's a carbon copy of
    "void ReplaceCharacters(string* s, const char* remove,
                                           char replacewith);"
    (even the parameter names are the same, the code is the same..)
    
    Is it intentional? Maybe for compatibility reasons? If so,
    let's make it deprecated and use the ReplaceCharacters method inside
    or the other way around.
    
    Also, noticed there were no tests for "StripString" or "Replace".
    Added some for both and planning on maybe making it more C++ish (?)
    in another commit.

--- src/google/protobuf/stubs/strutil.cc
+++ src/google/protobuf/stubs/strutil.cc
@@ -81,21 +81,6 @@ inline bool isprint(char c) {
 }
 
 // ----------------------------------------------------------------------
-// StripString
-//    Replaces any occurrence of the character 'remove' (or the characters
-//    in 'remove') with the character 'replacewith'.
-// ----------------------------------------------------------------------
-void StripString(string* s, const char* remove, char replacewith) {
-  const char * str_start = s->c_str();
-  const char * str = str_start;
-  for (str = strpbrk(str, remove);
-       str != nullptr;
-       str = strpbrk(str + 1, remove)) {
-    (*s)[str - str_start] = replacewith;
-  }
-}
-
-// ----------------------------------------------------------------------
 // ReplaceCharacters
 //    Replaces any occurrence of the character 'remove' (or the characters
 //    in 'remove') with the character 'replacewith'.
