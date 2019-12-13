4494acf6cfdc847ea982318e7c348b62b32c8396
Richard Belleville <rbellevi@google.com>
2019-12-12 18:11:05 -0800

    Modify example as demonstration of new API

--- examples/list_people.py
+++ examples/list_people.py
@@ -3,7 +3,8 @@
 # See README.txt for information and build instructions.
 
 from __future__ import print_function
-import addressbook_pb2
+from google import protobuf
+protos = protobuf.protos("addressbook.proto")
 import sys
 
 
@@ -16,11 +17,11 @@ def ListPeople(address_book):
       print("  E-mail address:", person.email)
 
     for phone_number in person.phones:
-      if phone_number.type == addressbook_pb2.Person.MOBILE:
+      if phone_number.type == protos.Person.MOBILE:
         print("  Mobile phone #:", end=" ")
-      elif phone_number.type == addressbook_pb2.Person.HOME:
+      elif phone_number.type == protos.Person.HOME:
         print("  Home phone #:", end=" ")
-      elif phone_number.type == addressbook_pb2.Person.WORK:
+      elif phone_number.type == protos.Person.WORK:
         print("  Work phone #:", end=" ")
       print(phone_number.number)
 
@@ -31,7 +32,7 @@ if len(sys.argv) != 2:
   print("Usage:", sys.argv[0], "ADDRESS_BOOK_FILE")
   sys.exit(-1)
 
-address_book = addressbook_pb2.AddressBook()
+address_book = protos.AddressBook()
 
 # Read the existing address book.
 with open(sys.argv[1], "rb") as f:
