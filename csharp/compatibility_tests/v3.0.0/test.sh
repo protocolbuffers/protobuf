e8016753e3817d44d78806d5ab43b5482cba6efa
Paul Yang <TeBoring@users.noreply.github.com>
2020-01-22 12:29:56 -0800

    Maven requires https connection (#7110) (#7114)

--- csharp/compatibility_tests/v3.0.0/test.sh
+++ csharp/compatibility_tests/v3.0.0/test.sh
@@ -40,7 +40,7 @@ TEST_VERSION=3.0.0
 # is also possible to use this same test set to test the compatibiilty of the
 # latest version against other versions.
 OLD_VERSION=$1
-OLD_VERSION_PROTOC=http://repo1.maven.org/maven2/com/google/protobuf/protoc/$OLD_VERSION/protoc-$OLD_VERSION-linux-x86_64.exe
+OLD_VERSION_PROTOC=https://repo1.maven.org/maven2/com/google/protobuf/protoc/$OLD_VERSION/protoc-$OLD_VERSION-linux-x86_64.exe
 
 echo "Running compatibility tests with $OLD_VERSION"
 
