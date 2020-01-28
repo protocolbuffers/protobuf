8c75cab522f4529db25dfe414cbbb4bd1de1edf8
Rafi Kamal <rafikamal@google.com>
2020-01-28 15:29:56 -0800

    Update Gem version to fix Ruby release failure
    Ref: https://bundler.io/blog/2019/05/14/solutions-for-cant-find-gem-bundler-with-executable-bundle.html

--- kokoro/release/ruby/linux/ruby/ruby_build_environment.sh
+++ kokoro/release/ruby/linux/ruby/ruby_build_environment.sh
@@ -4,5 +4,6 @@ set +ex
 [[ -s /etc/profile.d/rvm.sh ]] && . /etc/profile.d/rvm.sh
 set -e  # rvm commands are very verbose
 rvm --default use ruby-2.4.1
+gem update --system
 gem install bundler --update
 set -ex
