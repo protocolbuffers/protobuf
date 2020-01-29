f93a086dc017665cecfeb735ab16102d6d235ddf
Rafi Kamal <rafikamal@google.com>
2020-01-28 17:01:49 -0800

    Update ruby_build_environment.sh

--- kokoro/release/ruby/linux/ruby/ruby_build_environment.sh
+++ kokoro/release/ruby/linux/ruby/ruby_build_environment.sh
@@ -4,8 +4,6 @@ set +ex
 [[ -s /etc/profile.d/rvm.sh ]] && . /etc/profile.d/rvm.sh
 set -e  # rvm commands are very verbose
 rvm --default use ruby-2.4.1
-# Install the bundler version specified in Gemfile.lock.
-# This command can be replaced with "gem install bundler --update" 
-# if we stop specifying the bundler version in Gemfile.lock.
-gem install bundler -v "$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)"
+# The version needs to be updated if the version specified in Gemfile.lock is changed
+gem install bundler -v '1.17.3'
 set -ex
