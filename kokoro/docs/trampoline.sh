d1f4e25e159d3143fedb1da5c65dc664ea3510d2
Tim Swast <swast@google.com>
2020-01-30 15:04:02 -0600

    python: publish sphinx docs to googleapis.dev
    **Background:**
    
    This is a follow-up to the PR that adds sphinx docs.
    
    Googleapis.dev hosts client library documentation for several languages as
    well as documentation for core packages such as google-api-core. It is owned
    and maintained by the Cloud DevRel Core team.
    
    **About this change:**
    
    The `kokoro/docs/publish-python.sh` script builds the docs with sphinx, then
    uses the [googleapis/docuploader](https://github.com/googleapis/docuploader)
    tool to publish to googleapis.dev. The publish script is triggered manually
    with a Kokoro RELEASE type job. Googlers, see go link
    protobuf-docs-python-design for additional internal references.

---
+++ kokoro/docs/trampoline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Copied from https://github.com/googleapis/google-cloud-python/blob/master/.kokoro/trampoline.sh
+
+set -eo pipefail
+
+python3 "${KOKORO_GFILE_DIR}/trampoline_v1.py"  || ret_code=$?
+
+chmod +x ${KOKORO_GFILE_DIR}/trampoline_cleanup.sh
+${KOKORO_GFILE_DIR}/trampoline_cleanup.sh || true
+
+exit ${ret_code}
