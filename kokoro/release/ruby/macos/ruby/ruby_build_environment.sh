3631382c8dee7f65bc2a11b2851611c75407408d
Masaki Hara <hara@wantedly.com>
2020-04-14 14:42:02 +0900

    Use Ruby 2.5 for building Ruby <= 2.6

--- kokoro/release/ruby/macos/ruby/ruby_build_environment.sh
+++ kokoro/release/ruby/macos/ruby/ruby_build_environment.sh
@@ -5,6 +5,10 @@ set -ex
 set +ex  # rvm script is very verbose and exits with errorcode
 source $HOME/.rvm/scripts/rvm
 set -e  # rvm commands are very verbose
+time rvm install 2.5.0
+rvm use 2.5.0
+gem install rake-compiler --no-document
+gem install bundler --no-document
 time rvm install 2.7.0
 rvm use 2.7.0 --default
 gem install rake-compiler --no-document
@@ -47,10 +51,25 @@ EOF
 
 MAKE="make -j8"
 
-for v in 2.7.0 2.6.0 2.5.1 2.4.0 2.3.0 ; do
+set +x # rvm commands are very verbose
+rvm use 2.7.0
+set -x
+ruby --version | grep 'ruby 2.7.0'
+for v in 2.7.0 ; do
   ccache -c
   rake -f "$CROSS_RUBY" cross-ruby VERSION="$v" HOST=x86_64-darwin11 MAKE="$MAKE"
 done
+set +x
+rvm use 2.5.0
+set -x
+ruby --version | grep 'ruby 2.5.0'
+for v in 2.6.0 2.5.1 2.4.0 2.3.0 ; do
+  ccache -c
+  rake -f "$CROSS_RUBY" cross-ruby VERSION="$v" HOST=x86_64-darwin11 MAKE="$MAKE"
+done
+set +x
+rvm use 2.7.0
+set -x
 
 sed 's/x86_64-darwin-11/universal-darwin/' ~/.rake-compiler/config.yml > "$CROSS_RUBY"
 mv "$CROSS_RUBY" ~/.rake-compiler/config.yml
