7daf0aa7b00261bc8e0a433c58e74083cd58ea43
Misha Seltzer <mishas@users.noreply.github.com>
2020-05-14 21:53:32 -0400

    Removed the unnecessary setuptools package dependency for Python package.
    The setuptools package was added to allow definition of namespaces using
    the now outdated (and discouraged from use) pkg_resources-style.
    
    The code here, for a long while now, uses a try/except (`ImportError`)
    protection around the setuptools code, and falls back to the more
    encoraged pkgutil-style.
    
    Removing this library won't affect any current workflow, and in the case
    of setuptools not found, it'll actually use a more modern (and
    encouraged) flow.

--- python/setup.py
+++ python/setup.py
@@ -254,7 +254,7 @@ if __name__ == '__main__':
     os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'cpp'
 
   # Keep this list of dependencies in sync with tox.ini.
-  install_requires = ['six>=1.9', 'setuptools']
+  install_requires = ['six>=1.9']
   if sys.version_info <= (2,7):
     install_requires.append('ordereddict')
     install_requires.append('unittest2')
