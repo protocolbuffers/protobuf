43b213c936239eed01fa14458610ee7c6ddec69a
Joshua Haberman <jhaberman@gmail.com>
2020-06-09 14:19:58 -0700

    Added proper support for aggregate_metadata tests. But now I get a stack overflow.
    Stack overflow:
    /Users/haberman/code/protobuf/php/tests/generated/GPBMetadata/Proto/TestDescriptors.php:16
    /Users/haberman/code/protobuf/php/tests/generated/GPBMetadata/Proto/TestDescriptors.php:16
    /Users/haberman/code/protobuf/php/tests/generated/GPBMetadata/Proto/TestDescriptors.php:16
    /Users/haberman/code/protobuf/php/tests/generated/GPBMetadata/Proto/TestDescriptors.php:16
    /Users/haberman/code/protobuf/php/tests/generated/GPBMetadata/Proto/TestDescriptors.php:16
    /Users/haberman/code/protobuf/php/tests/generated/GPBMetadata/Proto/TestDescriptors.php:16
    /Users/haberman/code/protobuf/php/tests/generated/GPBMetadata/Proto/TestDescriptors.php:16
    /Users/haberman/code/protobuf/php/tests/generated/GPBMetadata/Proto/TestDescriptors.php:16
    
    namespace GPBMetadata\Proto;
    
    class TestDescriptors
    {
        public static $is_initialized = false;
    
        public static function initOnce() {
            $pool = \Google\Protobuf\Internal\DescriptorPool::getGeneratedPool();
            if (static::$is_initialized == true) {
              return;
            }
            \GPBMetadata\Proto\TestDescriptors::initOnce();
            $pool->internalAddGeneratedFile(hex2bin(
                ""
            ), true);
            static::$is_initialized = true;
        }
    }

--- php/tests/generate_protos.sh
+++ php/tests/generate_protos.sh
@@ -3,7 +3,14 @@
 set -ex
 
 cd `dirname $0`
+
+AGGREGATE_METADATA=""
+
+if [ "$1" = "--aggregate_metadata" ]; then
+  AGGREGATE_METADATA="aggregate_metadata=foo#bar:"
+fi
+
 rm -rf generated
 mkdir -p generated
-find proto -type f | xargs ../../src/protoc --php_out=generated -I../../src -I.
-../../src/protoc --php_out=generated -I../../src -I. proto/test_import_descriptor_proto.proto
+find proto -type f | xargs ../../src/protoc --php_out=${AGGREGATE_METADATA}generated -I../../src -I.
+../../src/protoc --php_out=${AGGREGATE_METADATA}generated -I../../src -I. proto/test_import_descriptor_proto.proto
