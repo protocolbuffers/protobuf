828b3132050fb01638d5b187a573180a5982bcb2
Joshua Haberman <jhaberman@gmail.com>
2020-06-25 11:30:10 -0700

    Delete both configure.ac and configure.in.

--- php/tests/compile_extension.sh
+++ php/tests/compile_extension.sh
@@ -15,14 +15,14 @@ fi
 
 pushd  ../ext/google/protobuf
 phpize --clean
-rm -f configure.in  # Some versions of phpize --clean don't delete this.
+rm -f configure.in configure.ac
 phpize && ./configure --with-php-config=$(which php-config) CFLAGS="$CFLAGS" && make
 popd
 
 if [ $(php -r "echo PHP_VERSION;" | cut -c1) != "5" ]; then
   pushd  ../ext/google/protobuf2
   phpize --clean
-  rm -f configure.in  # Some versions of phpize --clean don't delete this.
+  rm -f configure.in configure.ac
   php make-preload.php
   phpize && ./configure --with-php-config=$(which php-config) CFLAGS="$CFLAGS" && make
   popd
