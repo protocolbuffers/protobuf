93d8e89374947aa6c05c290b08efd211c6703087
Paul Yang <TeBoring@users.noreply.github.com>
2020-08-17 12:58:35 -0700

    Fix version number in php macro (#7822)
    Fixes #7812

--- php/ext/google/protobuf/message.c
+++ php/ext/google/protobuf/message.c
@@ -265,14 +265,14 @@ static PROTO_RETURN_VAL Message_write_property(
   }
 
   upb_msg_set(intern->msg, f, msgval, arena);
-#if PHP_VERSION_ID < 704000
+#if PHP_VERSION_ID < 70400
   return;
 #else
   return val;
 #endif
 
 error:
-#if PHP_VERSION_ID < 704000
+#if PHP_VERSION_ID < 70400
   return;
 #else
   return &EG(error_zval);
