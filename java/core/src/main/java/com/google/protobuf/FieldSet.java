37f34f0ad66f4f9fa8a2259318c478fd73ed9272
David Mollitor <dmollitor@apache.org>
2020-08-27 19:58:37 -0400

    Use ArrayList copy constructor

--- java/core/src/main/java/com/google/protobuf/FieldSet.java
+++ java/core/src/main/java/com/google/protobuf/FieldSet.java
@@ -1078,8 +1078,7 @@ final class FieldSet<T extends FieldSet.FieldDescriptorLite<T>> {
 
         // Wrap the contents in a new list so that the caller cannot change
         // the list's contents after setting it.
-        final List newList = new ArrayList();
-        newList.addAll((List) value);
+        final List newList = new ArrayList((List) value);
         for (final Object element : newList) {
           verifyType(descriptor.getLiteType(), element);
           hasNestedBuilders = hasNestedBuilders || element instanceof MessageLite.Builder;
