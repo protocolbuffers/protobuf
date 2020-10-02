f747f9a8f3a74c6fbe5476ec80b342f2f99d5c96
vraag00 <59297541+vraag00@users.noreply.github.com>
2020-10-02 14:34:09 +0530

    Update conf.py

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -42,8 +42,6 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 import google.protobuf
 
 # -- Project information -----------------------------------------------------
