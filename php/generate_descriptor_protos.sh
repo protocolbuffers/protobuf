ec4b19460896164cfe3a980de1a0d6ed200f7904
Joshua Haberman <jhaberman@gmail.com>
2020-10-05 22:27:56 -0700

    Regenerated protos with new script.

--- php/generate_descriptor_protos.sh
+++ php/generate_descriptor_protos.sh
@@ -3,6 +3,8 @@
 # Run this script to regenerate descriptor protos after the protocol compiler
 # changes.
 
+set -e
+
 if test ! -e src/google/protobuf/stubs/common.h; then
   cat >&2 << __EOF__
 Could not find source code.  Make sure you are running this script from the
@@ -13,7 +15,7 @@ fi
 
 pushd src
 ./protoc --php_out=internal:../php/src google/protobuf/descriptor.proto
-./protoc --php_out=internal_generate_c_wkt:src \
+./protoc --php_out=internal_generate_c_wkt:../php/src \
   google/protobuf/any.proto \
   google/protobuf/api.proto \
   google/protobuf/duration.proto \
