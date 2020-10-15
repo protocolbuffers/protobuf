2187eb0448fc101490b0fc9964e4e0b6f7b66213
Joshua Haberman <jhaberman@gmail.com>
2020-10-14 21:23:29 -0700

    Updated PHP generator to output raw binary strings instead of requiring hex2bin() at runtime.

--- src/google/protobuf/compiler/php/php_generator.cc
+++ src/google/protobuf/compiler/php/php_generator.cc
@@ -999,16 +999,29 @@ void GenerateAddFileToPool(
       string files_data;
       files.SerializeToString(&files_data);
 
-      printer->Print("$pool->internalAddGeneratedFile(hex2bin(\n");
+      printer->Print("$pool->internalAddGeneratedFile(\n");
       Indent(printer);
+      printer->Print("'");
 
-      printer->Print(
-          "\"^data^\"\n",
-          "data", BinaryToHex(files_data));
+      for (auto ch : files_data) {
+        switch (ch) {
+          case '\\':
+            printer->Print(R"(\\)");
+            break;
+          case '\'':
+            printer->Print(R"(\')");
+            break;
+          default:
+            printer->Print("^char^", "char", std::string(1, ch));
+            break;
+        }
+      }
+
+      printer->Print("'\n");
 
       Outdent(printer);
       printer->Print(
-          "), true);\n\n");
+          ", true);\n\n");
     }
     printer->Print(
         "static::$is_initialized = true;\n");
