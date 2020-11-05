27100b40fd45b384b690020d9306d7e501a65d16
Adam Cozzette <acozzette@google.com>
2020-11-05 13:43:22 -0800

    Update protobuf version

--- php/src/GPBMetadata/Google/Protobuf/FieldMask.php
+++ php/src/GPBMetadata/Google/Protobuf/FieldMask.php
@@ -14,9 +14,14 @@ class FieldMask
         if (static::$is_initialized == true) {
           return;
         }
-        $pool->internalAddGeneratedFile(hex2bin(
-            "0adf010a20676f6f676c652f70726f746f6275662f6669656c645f6d61736b2e70726f746f120f676f6f676c652e70726f746f627566221a0a094669656c644d61736b120d0a0570617468731801200328094285010a13636f6d2e676f6f676c652e70726f746f627566420e4669656c644d61736b50726f746f50015a32676f6f676c652e676f6c616e672e6f72672f70726f746f6275662f74797065732f6b6e6f776e2f6669656c646d61736b7062f80101a20203475042aa021e476f6f676c652e50726f746f6275662e57656c6c4b6e6f776e5479706573620670726f746f33"
-        ), true);
+        $pool->internalAddGeneratedFile(
+            '
+ß
+ google/protobuf/field_mask.protogoogle.protobuf"
+	FieldMask
+paths (	B…
+com.google.protobufBFieldMaskProtoPZ2google.golang.org/protobuf/types/known/fieldmaskpbø¢GPBªGoogle.Protobuf.WellKnownTypesbproto3'
+        , true);
 
         static::$is_initialized = true;
     }
