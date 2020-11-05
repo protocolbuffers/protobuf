7b34e9622c8b8bc86d1198b1d7bed8685d7ea390
Adam Cozzette <acozzette@google.com>
2020-11-05 13:43:22 -0800

    Update protobuf version

--- php/src/GPBMetadata/Google/Protobuf/Duration.php
+++ php/src/GPBMetadata/Google/Protobuf/Duration.php
@@ -14,9 +14,15 @@ class Duration
         if (static::$is_initialized == true) {
           return;
         }
-        $pool->internalAddGeneratedFile(hex2bin(
-            "0aeb010a1e676f6f676c652f70726f746f6275662f6475726174696f6e2e70726f746f120f676f6f676c652e70726f746f627566222a0a084475726174696f6e120f0a077365636f6e6473180120012803120d0a056e616e6f731802200128054283010a13636f6d2e676f6f676c652e70726f746f627566420d4475726174696f6e50726f746f50015a31676f6f676c652e676f6c616e672e6f72672f70726f746f6275662f74797065732f6b6e6f776e2f6475726174696f6e7062f80101a20203475042aa021e476f6f676c652e50726f746f6275662e57656c6c4b6e6f776e5479706573620670726f746f33"
-        ), true);
+        $pool->internalAddGeneratedFile(
+            '
+ë
+google/protobuf/duration.protogoogle.protobuf"*
+Duration
+seconds (
+nanos (Bƒ
+com.google.protobufBDurationProtoPZ1google.golang.org/protobuf/types/known/durationpbø¢GPBªGoogle.Protobuf.WellKnownTypesbproto3'
+        , true);
 
         static::$is_initialized = true;
     }
