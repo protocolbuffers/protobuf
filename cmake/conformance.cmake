6a5014cf99974fe0f129f34ef9d64287810782ff
Biswapriyo Nath <nathbappai@gmail.com>
2020-12-01 19:36:21 +0530

    Conformance/CMake: Remove non-existence binary_json_conformance_main.cc file.

--- cmake/conformance.cmake
+++ cmake/conformance.cmake
@@ -21,7 +21,6 @@ add_custom_command(
 add_executable(conformance_test_runner
   ${protobuf_source_dir}/conformance/conformance.pb.cc
   ${protobuf_source_dir}/conformance/conformance_test.cc
-  ${protobuf_source_dir}/conformance/binary_json_conformance_main.cc
   ${protobuf_source_dir}/conformance/binary_json_conformance_suite.cc
   ${protobuf_source_dir}/conformance/binary_json_conformance_suite.h
   ${protobuf_source_dir}/conformance/conformance_test_runner.cc
