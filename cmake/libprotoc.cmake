90f317ca6adf9307d909f343f0e5f51a88ea102c
Jorge López <jorgelopezfueyo@gmail.com>
2020-12-14 12:07:23 +0100

    Disabled version.rc for clang-cl

--- cmake/libprotoc.cmake
+++ cmake/libprotoc.cmake
@@ -159,7 +159,7 @@ set(libprotoc_headers
   ${protobuf_source_dir}/src/google/protobuf/compiler/zip_writer.h
 )
 
-if (MSVC)
+if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
 set(libprotoc_rc_files
   ${CMAKE_CURRENT_BINARY_DIR}/version.rc
 )
