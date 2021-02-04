protocolbuffers/protobuf

commit a91adf173b355924070dd50caba3aff41ae0fcdf

Don't include sys/param.h for _BYTE_ORDER
It includes a bunch of other files including a lot of macros which can
reduce the namespace available for actual protobuf.

For example, create a protobuf with a member called SIGSEGV. Since
macros cannot be namespaced in C++ this results in code which cannot be
compiled.

Fix this by just directly including endian.h

src/google/protobuf/io/coded_stream.h
@@ -131,7 +131,11 @@
 #pragma runtime_checks("c", off)
 #endif
 #else
-#include <sys/param.h>  // __BYTE_ORDER
+#ifdef __APPLE__
+#include <machine/endian.h>  // __BYTE_ORDER
+#else
+#include <endian.h>  // __BYTE_ORDER
+#endif
 #if ((defined(__LITTLE_ENDIAN__) && !defined(__BIG_ENDIAN__)) ||    \
      (defined(__BYTE_ORDER) && __BYTE_ORDER == __LITTLE_ENDIAN)) && \
     !defined(PROTOBUF_DISABLE_LITTLE_ENDIAN_OPT_FOR_TEST)

src/google/protobuf/stubs/port.h
@@ -57,9 +57,10 @@
 #pragma runtime_checks("c", off)
 #endif
 #else
-  #include <sys/param.h>   // __BYTE_ORDER
-  #if defined(__OpenBSD__)
-    #include <endian.h>
+  #ifdef __APPLE__
+    #include <machine/endian.h>  // __BYTE_ORDER
+  #else
+    #include <endian.h>  // __BYTE_ORDER
   #endif
   #if ((defined(__LITTLE_ENDIAN__) && !defined(__BIG_ENDIAN__)) || \
          (defined(__BYTE_ORDER) && __BYTE_ORDER == __LITTLE_ENDIAN) || \

commit 9f47f790c5ad2cdcfa8b753ceed37fe1a4510643

io/coded_stream: assume Windows is little endian
mingw-w64 lacks endian.h, but had sys/param.h meaning
a91adf173b355924070dd50caba3aff41ae0fcdf broke non-msvc windows
compilation

Signed-off-by: Christopher Degawa <ccom@randomderp.com>

src/google/protobuf/io/coded_stream.h
@@ -120,12 +120,12 @@
 #include <type_traits>
 #include <utility>
 
-#ifdef _MSC_VER
+#ifdef _WIN32
 // Assuming windows is always little-endian.
 #if !defined(PROTOBUF_DISABLE_LITTLE_ENDIAN_OPT_FOR_TEST)
 #define PROTOBUF_LITTLE_ENDIAN 1
 #endif
-#if _MSC_VER >= 1300 && !defined(__INTEL_COMPILER)
+#if defined(_MSC_VER) && _MSC_VER >= 1300 && !defined(__INTEL_COMPILER)
 // If MSVC has "/RTCc" set, it will complain about truncating casts at
 // runtime.  This file contains some intentional truncating casts.
 #pragma runtime_checks("c", off)