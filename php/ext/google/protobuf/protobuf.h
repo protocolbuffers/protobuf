7feebfd990fcafd0a7a936953856aa244c7cd9b6
Adam Cozzette <acozzette@google.com>
2021-05-03 16:44:16 -0700

    Update protobuf version

--- php/ext/google/protobuf/protobuf.h
+++ php/ext/google/protobuf/protobuf.h
@@ -76,7 +76,7 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_setter, 0, 0, 1)
   ZEND_ARG_INFO(0, value)
 ZEND_END_ARG_INFO()
 
-#define PHP_PROTOBUF_VERSION "3.16.0RC1"
+#define PHP_PROTOBUF_VERSION "3.16.0RC2"
 
 // ptr -> PHP object cache. This is a weak map that caches lazily-created
 // wrapper objects around upb types:
