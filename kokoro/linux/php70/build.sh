f87e97242edb4a41ce405e2727c25bffbb3b3ce5
Joshua Haberman <jhaberman@gmail.com>
2021-05-11 17:00:22 -0700

    Move all jobs to use php80 instead of lots of separate jobs.

--- kokoro/linux/php70/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# This is the entry point for kicking off a Kokoro job. This path is referenced
-# from the .cfg files in this directory.
-
-exec ../test_php.sh gcr.io/protobuf-build/php/linux:7.0.33-dbg-14a06550010c0649bf69b6c9b803c1ca609bbb6d
--- kokoro/linux/test_php.sh
+++ kokoro/linux/test_php.sh
@@ -2,4 +2,4 @@
 
 set -ex
 
-docker run -v$(realpath $(dirname $0)/../..):/workspace $1 "composer test && composer test_c"
+docker run -it -v$(realpath $(dirname $0)/../..):/workspace $1 "composer test && composer test_c"
