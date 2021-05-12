95d0b295ca1a5697fb9c4f8fab8ccc30dd2dfa48
Joshua Haberman <jhaberman@gmail.com>
2021-05-11 17:07:48 -0700

    Only pass -t flag if we are running in a terminal.

--- kokoro/linux/test_php.sh
+++ kokoro/linux/test_php.sh
@@ -2,4 +2,5 @@
 
 set -ex
 
-docker run -it -v$(realpath $(dirname $0)/../..):/workspace $1 "composer test && composer test_c"
+test -t 1 && USE_TTY="-it" 
+docker run ${USE_TTY} -v$(realpath $(dirname $0)/../..):/workspace $1 "composer test && composer test_c"
