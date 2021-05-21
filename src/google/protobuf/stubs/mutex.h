12a9feca47c1995ceb4bfbe0a0a32e7d1e8a5c85
Daniel Schopf <schopf.dan@gmail.com>
2021-05-21 09:32:11 +0200

    Fix compiler error with brace initializer (conflicts with #8492)

--- src/google/protobuf/stubs/mutex.h
+++ src/google/protobuf/stubs/mutex.h
@@ -116,7 +116,11 @@ class CallOnceInitializedMutex {
 // mutexes.
 class GOOGLE_PROTOBUF_CAPABILITY("mutex") PROTOBUF_EXPORT WrappedMutex {
  public:
+#if defined(__QNX__)
+  constexpr WrappedMutex() = default;
+#else
   constexpr WrappedMutex() {}
+#endif
   void Lock() GOOGLE_PROTOBUF_ACQUIRE() { mu_.lock(); }
   void Unlock() GOOGLE_PROTOBUF_RELEASE() { mu_.unlock(); }
   // Crash if this Mutex is not held exclusively by this thread.
