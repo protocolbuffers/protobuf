55b0e11f528e41dc96ad5902fb5abc5b153486c9
Adam Cozzette <acozzette@google.com>
2021-09-30 13:16:53 -0700

    Fix Ruby tests on Mac
    It appears that this extra conditional in travis-test.sh is no longer
    necessary, and in fact we need to avoid using a custom version of
    OpenSSL since that is causing its own error.

--- ruby/travis-test.sh
+++ ruby/travis-test.sh
@@ -18,7 +18,7 @@ test_version() {
        rake gc_test &&
        cd ../conformance && make test_jruby &&
        cd ../ruby/compatibility_tests/v3.0.0 && ./test.sh"
-  elif [ "$version" == "ruby-2.6.0" -o "$version" == "ruby-2.7.0" -o "$version" == "ruby-3.0.2" ] ; then
+  else
     bash --login -c \
       "rvm install $version && rvm use $version && \
        which ruby && \
@@ -29,19 +29,6 @@ test_version() {
        cd ../conformance && make ${RUBY_CONFORMANCE} &&
        cd ../ruby/compatibility_tests/v3.0.0 &&
        cp -R ../../lib lib && ./test.sh"
-  else
-    # Recent versions of OSX have deprecated OpenSSL, so we have to explicitly
-    # provide a path to the OpenSSL directory installed via Homebrew.
-    bash --login -c \
-      "rvm install $version --with-openssl-dir=`brew --prefix openssl` && \
-       rvm use $version && \
-       which ruby && \
-       git clean -f && \
-       gem install bundler -v 1.17.3 && bundle && \
-       rake test &&
-       rake gc_test &&
-       cd ../conformance && make ${RUBY_CONFORMANCE} &&
-       cd ../ruby/compatibility_tests/v3.0.0 && ./test.sh"
   fi
 }
 
