9eda1af7bf725e5b78b900f615c13e89fe0df55c
Thomas Hisch <thomas.hisch@ims.co.at>
2021-10-30 09:49:01 +0200

    Remove unittest2 imports
    They were only needed for python versions <= 2.6
    
    Related: #6658

--- python/google/protobuf/internal/generator_test.py
+++ python/google/protobuf/internal/generator_test.py
@@ -39,10 +39,7 @@ further ensures that we can use Python protocol message objects as we expect.
 
 __author__ = 'robinson@google.com (Will Robinson)'
 
-try:
-  import unittest2 as unittest  #PY26
-except ImportError:
-  import unittest
+import unittest
 
 from google.protobuf.internal import test_bad_identifiers_pb2
 from google.protobuf import unittest_custom_options_pb2
