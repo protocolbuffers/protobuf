204971e5dbd7aca27bc804149869b66ef1bacbfa
Bernardo Bruning <bernardo.bruning@gmail.com>
2022-01-27 20:23:54 -0300

    Remove memory leak into tests

--- tests/test_cpp.cc
+++ tests/test_cpp.cc
@@ -171,7 +171,7 @@ TEST(Cpp, TimestampEncoder) {
   for(long timestamp: timestamps) {
     google_protobuf_Timestamp_set_seconds(timestamp_upb, timestamp);
 
-    char* json = new char[23];
+    char json[23];
     size_t size = upb_JsonEncode(timestamp_upb, md.ptr(), NULL, 0, json, 23, NULL);
     bool result = upb_JsonDecode(json, size, timestamp_upb_decoded, md.ptr(), NULL, 0, arena.ptr(), NULL);
     const long timestamp_decoded = google_protobuf_Timestamp_seconds(timestamp_upb_decoded); 
