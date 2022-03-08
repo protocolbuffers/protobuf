f4fed16f00a9f23a2355f65b63281461ad2cbd14
Sandy Zhang <sandyzhang@google.com>
2022-03-08 00:30:28 +0000

    Enable GC.stress in gc_test for ruby 2.7. This was previously disabled due to a ruby GC bug which has since been closed:  https://bugs.ruby-lang.org/issues/16807

--- ruby/tests/gc_test.rb
+++ ruby/tests/gc_test.rb
@@ -4,9 +4,7 @@
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
 
 old_gc = GC.stress
-# Ruby 2.7.0 - 2.7.1 has a GC bug in its parser, so turn off stress for now
-# See https://bugs.ruby-lang.org/issues/16807
-GC.stress = 0x01 | 0x04 unless RUBY_VERSION.match?(/^2\.7\./)
+GC.stress = 0x01 | 0x04
 require 'generated_code_pb'
 require 'generated_code_proto2_pb'
 GC.stress = old_gc
@@ -94,7 +92,6 @@ class GCTest < Test::Unit::TestCase
     from = get_msg_proto3
     data = A::B::C::TestMessage.encode(from)
     to = A::B::C::TestMessage.decode(data)
-
     # This doesn't work for proto2 on JRuby because there is a nested required message.
     # A::B::Proto2::TestMessage has :required_msg which is of type:
     # A::B::Proto2::TestMessage so there is no way to generate a valid
