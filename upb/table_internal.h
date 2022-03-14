570332dfeed850c6afe8ca25e37db4f0a6bddcc7
Joshua Haberman <jhaberman@gmail.com>
2022-03-14 10:08:15 -0700

    Ruby needs the "seed" parameter in our hash function.

--- upb/table_internal.h
+++ upb/table_internal.h
@@ -374,7 +374,7 @@ void upb_inttable_iter_setdone(upb_inttable_iter* i);
 bool upb_inttable_iter_isequal(const upb_inttable_iter* i1,
                                const upb_inttable_iter* i2);
 
-uint32_t _upb_Hash(const char* p, size_t n);
+uint32_t _upb_Hash(const char* p, size_t n, uint64_t seed);
 
 #ifdef __cplusplus
 } /* extern "C" */
