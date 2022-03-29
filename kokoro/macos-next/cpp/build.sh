fe5632cd59d3f16afab243b545689a1ee73466de
David L. Jones <dlj@google.com>
2022-03-29 15:29:35 -0700

    debugging

--- kokoro/macos-next/cpp/build.sh
+++ kokoro/macos-next/cpp/build.sh
@@ -20,7 +20,87 @@ cd cmake/build
 # Print some basic info
 xcode-select --print-path
 xcodebuild -version
+xcodebuild -showsdks
 xcrun --show-sdk-path
+
+echo "#include <cmath>" | \
+  /Applications/Xcode_13.2.1.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang \
+  -v \
+  -x c++ \
+  -target x86_64-apple-macos11.4 \
+  -fmessage-length\=0 \
+  -fdiagnostics-show-note-include-stack \
+  -fmacro-backtrace-limit\=0 \
+  -Wno-trigraphs \
+  -fpascal-strings \
+  -O0 \
+  -Wno-missing-field-initializers \
+  -Wno-missing-prototypes \
+  -Wno-return-type \
+  -Wno-non-virtual-dtor \
+  -Wno-overloaded-virtual \
+  -Wno-exit-time-destructors \
+  -Wno-missing-braces \
+  -Wparentheses \
+  -Wswitch \
+  -Wno-unused-function \
+  -Wno-unused-label \
+  -Wno-unused-parameter \
+  -Wno-unused-variable \
+  -Wunused-value \
+  -Wno-empty-body \
+  -Wno-uninitialized \
+  -Wno-unknown-pragmas \
+  -Wno-shadow \
+  -Wno-four-char-constants \
+  -Wno-conversion \
+  -Wno-constant-conversion \
+  -Wno-int-conversion \
+  -Wno-bool-conversion \
+  -Wno-enum-conversion \
+  -Wno-float-conversion \
+  -Wno-non-literal-null-conversion \
+  -Wno-objc-literal-conversion \
+  -Wno-shorten-64-to-32 \
+  -Wno-newline-eof \
+  -Wno-c++11-extensions \
+  -DCMAKE_INTDIR\=\"Debug\" \
+  -DGOOGLE_PROTOBUF_CMAKE_BUILD \
+  -DHAVE_ZLIB \
+  -isysroot /Applications/Xcode_13.2.1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX12.1.sdk \
+  -fasm-blocks \
+  -fstrict-aliasing \
+  -Wdeprecated-declarations \
+  -Winvalid-offsetof \
+  -g \
+  -Wno-sign-conversion \
+  -Wno-infinite-recursion \
+  -Wno-move \
+  -Wno-comma \
+  -Wno-block-capture-autoreleasing \
+  -Wno-strict-prototypes \
+  -Wno-range-loop-analysis \
+  -Wno-semicolon-before-method-body \
+  -I/tmpfs/src/github/protobuf/cmake/build/Debug/include \
+  -I/tmpfs/src/github/protobuf/cmake/build \
+  -I/tmpfs/src/github/protobuf/src \
+  -I/tmpfs/src/github/protobuf/third_party/googletest/googlemock \
+  -I/tmpfs/src/github/protobuf/third_party/googletest/googletest \
+  -I/tmpfs/src/github/protobuf/third_party/googletest/googletest/include \
+  -I/tmpfs/src/github/protobuf/third_party/googletest/googlemock/include \
+  -isystem /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX12.1.sdk/usr/include \
+  -I/tmpfs/src/github/protobuf/cmake/build/protobuf.build/Debug/libprotobuf.build/DerivedSources-normal/x86_64 \
+  -I/tmpfs/src/github/protobuf/cmake/build/protobuf.build/Debug/libprotobuf.build/DerivedSources/x86_64 \
+  -I/tmpfs/src/github/protobuf/cmake/build/protobuf.build/Debug/libprotobuf.build/DerivedSources \
+  -F/tmpfs/src/github/protobuf/cmake/build/Debug \
+  -std\=c++11 \
+  -MMD \
+  -MT dependencies \
+  -MF /tmpfs/src/github/protobuf/cmake/build/protobuf.build/Debug/libprotobuf.build/Objects-normal/x86_64/zero_copy_stream_impl_lite.d \
+  --serialize-diagnostics /tmpfs/src/github/protobuf/cmake/build/protobuf.build/Debug/libprotobuf.build/Objects-normal/x86_64/zero_copy_stream_impl_lite.dia \
+  -c - \
+  -o /dev/null
+
 
 # Build everything first
 cmake -G Xcode ../.. \
