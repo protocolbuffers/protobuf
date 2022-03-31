36cf2b23d6af76cdea54b1e95a4b467e823e3a36
David L. Jones <dlj@google.com>
2022-03-31 15:05:59 -0700

    Revert changes to cmake/tests.cmake

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -1,7 +1,5 @@
 option(protobuf_USE_EXTERNAL_GTEST "Use external Google Test (i.e. not the one in third_party/googletest)" OFF)
 
-option(protobuf_TEST_XML_OUTDIR "Output directory for XML logs from tests." "")
-
 option(protobuf_ABSOLUTE_TEST_PLUGIN_PATH
   "Using absolute test_plugin path in tests" ON)
 mark_as_advanced(protobuf_ABSOLUTE_TEST_PLUGIN_PATH)
@@ -150,35 +148,6 @@ set(tests_files
   ${protobuf_SOURCE_DIR}/src/google/protobuf/arena_unittest.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/arenastring_unittest.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/arenaz_sampler_test.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/descriptor_database_unittest.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/descriptor_unittest.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/drop_unknown_fields_test.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/dynamic_message_unittest.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/extension_set_unittest.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/generated_message_reflection_unittest.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/generated_message_tctable_lite_test.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/inlined_string_field_unittest.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/map_field_test.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/map_test.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/map_test.inc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/message_unittest.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/message_unittest.inc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/no_field_presence_test.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/preserve_unknown_enum_test.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/proto3_arena_lite_unittest.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/proto3_arena_unittest.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/proto3_lite_unittest.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/proto3_lite_unittest.inc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/reflection_ops_unittest.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/repeated_field_reflection_unittest.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/repeated_field_unittest.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/text_format_unittest.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/unknown_field_set_unittest.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/wire_format_unittest.cc
-  ${protobuf_SOURCE_DIR}/src/google/protobuf/wire_format_unittest.inc
-)
-
-set(compiler_test_files
   ${protobuf_SOURCE_DIR}/src/google/protobuf/compiler/annotation_test_util.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/compiler/command_line_interface_unittest.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/compiler/cpp/bootstrap_unittest.cc
@@ -197,17 +166,33 @@ set(compiler_test_files
   ${protobuf_SOURCE_DIR}/src/google/protobuf/compiler/parser_unittest.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/compiler/python/plugin_unittest.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/compiler/ruby/ruby_generator_unittest.cc
-)
-
-set(io_test_files
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/descriptor_database_unittest.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/descriptor_unittest.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/drop_unknown_fields_test.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/dynamic_message_unittest.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/extension_set_unittest.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/generated_message_reflection_unittest.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/generated_message_tctable_lite_test.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/inlined_string_field_unittest.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/io/coded_stream_unittest.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/io/io_win32_unittest.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/io/printer_unittest.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/io/tokenizer_unittest.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/io/zero_copy_stream_unittest.cc
-)
-
-set(stubs_test_files
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/map_field_test.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/map_test.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/map_test.inc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/message_unittest.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/message_unittest.inc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/no_field_presence_test.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/preserve_unknown_enum_test.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/proto3_arena_lite_unittest.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/proto3_arena_unittest.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/proto3_lite_unittest.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/proto3_lite_unittest.inc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/reflection_ops_unittest.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/repeated_field_reflection_unittest.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/repeated_field_unittest.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/stubs/bytestream_unittest.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/stubs/common_unittest.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/stubs/int128_unittest.cc
@@ -219,9 +204,8 @@ set(stubs_test_files
   ${protobuf_SOURCE_DIR}/src/google/protobuf/stubs/strutil_unittest.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/stubs/template_util_unittest.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/stubs/time_test.cc
-)
-
-set(util_test_files
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/text_format_unittest.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/unknown_field_set_unittest.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/util/delimited_message_util_test.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/util/field_comparator_test.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/util/field_mask_util_test.cc
@@ -236,6 +220,8 @@ set(util_test_files
   ${protobuf_SOURCE_DIR}/src/google/protobuf/util/time_util_test.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/util/type_resolver_util_test.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/well_known_types_unittest.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/wire_format_unittest.cc
+  ${protobuf_SOURCE_DIR}/src/google/protobuf/wire_format_unittest.inc
 )
 
 if(protobuf_ABSOLUTE_TEST_PLUGIN_PATH)
@@ -253,21 +239,13 @@ if(MINGW)
 
 endif()
 
-if(protobuf_TEST_XML_OUTDIR)
-  if(NOT "${protobuf_TEST_XML_OUTDIR}" MATCHES "[/\\]$")
-    string(APPEND protobuf_TEST_XML_OUTDIR "/")
-  endif()
-  set(protobuf_GTEST_ARGS "--gtest_output=xml:${protobuf_TEST_XML_OUTDIR}")
-endif()
-
 add_executable(tests ${tests_files})
 if (MSVC)
   target_compile_options(tests PRIVATE
     /wd4146 # unary minus operator applied to unsigned type, result still unsigned
   )
 endif()
-target_link_libraries(tests protobuf-lite-test-common protobuf-test-common libprotobuf GTest::gmock_main)
-add_test(NAME tests COMMAND tests ${protobuf_GTEST_ARGS})
+target_link_libraries(tests protobuf-lite-test-common protobuf-test-common libprotoc libprotobuf GTest::gmock_main)
 
 set(test_plugin_files
   ${protobuf_SOURCE_DIR}/src/google/protobuf/compiler/mock_code_generator.cc
@@ -275,42 +253,27 @@ set(test_plugin_files
   ${protobuf_SOURCE_DIR}/src/google/protobuf/testing/file.cc
   ${protobuf_SOURCE_DIR}/src/google/protobuf/testing/file.h
 )
+
 add_executable(test_plugin ${test_plugin_files})
 target_link_libraries(test_plugin libprotoc libprotobuf GTest::gmock)
-
-add_executable(compiler_test ${compiler_test_files})
-target_link_libraries(compiler_test protobuf-lite-test-common protobuf-test-common libprotoc libprotobuf-lite libprotobuf GTest::gmock_main)
-add_test(NAME compiler_test
-  COMMAND compiler_test ${protobuf_GTEST_ARGS}
-  WORKING_DIRECTORY "${protobuf_SOURCE_DIR}")
 
 set(lite_test_files
   ${protobuf_SOURCE_DIR}/src/google/protobuf/lite_unittest.cc
 )
-add_executable(lite_test ${lite_test_files})
-target_link_libraries(lite_test protobuf-lite-test-common libprotobuf-lite GTest::gmock_main)
-add_test(NAME lite_test COMMAND lite_test ${protobuf_GTEST_ARGS})
+add_executable(lite-test ${lite_test_files})
+target_link_libraries(lite-test protobuf-lite-test-common libprotobuf-lite GTest::gmock_main)
 
 set(lite_arena_test_files
   ${protobuf_SOURCE_DIR}/src/google/protobuf/lite_arena_unittest.cc
 )
-add_executable(lite_arena_test ${lite_arena_test_files})
-target_link_libraries(lite_arena_test protobuf-lite-test-common libprotobuf-lite GTest::gmock_main)
-add_test(NAME lite_arena_test COMMAND lite_arena_test ${protobuf_GTEST_ARGS})
-
-add_executable(io_test ${io_test_files})
-target_link_libraries(io_test protobuf-lite-test-common protobuf-test-common libprotobuf libprotobuf-lite GTest::gmock_main)
-add_test(NAME io_test COMMAND io_test ${protobuf_GTEST_ARGS})
-
-add_executable(stubs_test ${stubs_test_files})
-target_link_libraries(stubs_test protobuf-lite-test-common protobuf-test-common libprotobuf libprotobuf-lite GTest::gmock_main)
-add_test(NAME stubs_test COMMAND stubs_test ${protobuf_GTEST_ARGS})
-
-add_executable(util_test ${util_test_files})
-target_link_libraries(util_test protobuf-lite-test-common protobuf-test-common libprotobuf libprotobuf-lite GTest::gmock_main)
-add_test(NAME util_test COMMAND util_test ${protobuf_GTEST_ARGS})
+add_executable(lite-arena-test ${lite_arena_test_files})
+target_link_libraries(lite-arena-test protobuf-lite-test-common libprotobuf-lite GTest::gmock_main)
 
 add_custom_target(check
   COMMAND tests
   DEPENDS tests test_plugin
   WORKING_DIRECTORY ${protobuf_SOURCE_DIR})
+
+add_test(NAME check
+  COMMAND tests
+  WORKING_DIRECTORY "${protobuf_SOURCE_DIR}")
