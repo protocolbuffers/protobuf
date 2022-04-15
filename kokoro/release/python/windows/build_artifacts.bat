2038715121837c60b232b66a442b4b70425bfe72
Mike Kruskal <mkruskal@google.com>
2022-04-15 14:58:20 -0700

    Fix 'git cd' typo

--- kokoro/release/python/windows/build_artifacts.bat
+++ kokoro/release/python/windows/build_artifacts.bat
@@ -37,10 +37,10 @@ del /Q zlib-src.zip
 REM Install Abseil
 mkdir absl
 git clone https://github.com/abseil/abseil-cpp.git
-git cd abseil-cpp
 # Checkout lts_2021_11_02
+cd abseil-cpp
 git checkout 215105818dfde3174fe799600bb0f3cae233d0bf
-git cd ..
+cd ..
 SET ABSL_ROOT_DIR=%cd%\abseil-cpp
 
 REM Create directory for artifacts
