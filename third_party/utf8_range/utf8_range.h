d2fd4419534af311e280789a0d5b9b7e72058e9c
なつき <i@ntk.me>
2022-04-18 16:12:20 -0700

    Do not use range based UTF-8 validation in truffleruby (#9769)

--- third_party/utf8_range/utf8_range.h
+++ third_party/utf8_range/utf8_range.h
@@ -1,5 +1,5 @@
 
-#if (defined(__ARM_NEON) && defined(__aarch64__)) || defined(__SSE4_1__)
+#if ((defined(__ARM_NEON) && defined(__aarch64__)) || defined(__SSE4_1__)) && !defined(TRUFFLERUBY)
 int utf8_range2(const unsigned char* data, int len);
 #else
 int utf8_naive(const unsigned char* data, int len);
