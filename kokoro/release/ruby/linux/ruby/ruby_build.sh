e6c06c2c686599b2750288f98b8c691709792f84
Adam Cozzette <acozzette@google.com>
2022-04-26 21:45:08 +0000

    Update Ruby build to rely on  environment variable

--- kokoro/release/ruby/linux/ruby/ruby_build.sh
+++ kokoro/release/ruby/linux/ruby/ruby_build.sh
@@ -6,6 +6,7 @@ set -ex
 use_bazel.sh 5.1.1
 bazel build //:protoc
 cp bazel-bin/protoc src/protoc
+export PROTOC=$PWD/src/protoc
 
 umask 0022
 pushd ruby
--- kokoro/release/ruby/macos/ruby/ruby_build.sh
+++ kokoro/release/ruby/macos/ruby/ruby_build.sh
@@ -5,6 +5,7 @@ set -ex
 # Build protoc
 use_bazel.sh 5.1.1
 bazel build //:protoc
+export PROTOC=$PWD/bazel-bin/protoc
 
 umask 0022
 pushd ruby
