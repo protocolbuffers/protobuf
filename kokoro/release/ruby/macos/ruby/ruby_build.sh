9c8c3dea305b254b4854b56f850054958376e9ed
Adam Cozzette <acozzette@google.com>
2022-04-27 10:40:01 -0700

    Start reducing our reliance on autotools (#9824)
    Start reducing our reliance on autotools
    
    This commit updates a few of our CI scripts to build protoc using Bazel
    instead of autotools. I also updated the setuptools and Rake build
    configs to look for protoc under bazel-bin/ so that they will
    preferentially use a Bazel-built protoc binary. The goal is to start
    chipping away at our use of autotools in preparation for eventually
    dropping autotools support.

--- kokoro/release/ruby/macos/ruby/ruby_build.sh
+++ kokoro/release/ruby/macos/ruby/ruby_build.sh
@@ -3,11 +3,9 @@
 set -ex
 
 # Build protoc
-if test ! -e src/protoc; then
-  ./autogen.sh
-  ./configure
-  make -j4
-fi
+use_bazel.sh 5.1.1
+bazel build //:protoc
+export PROTOC=$PWD/bazel-bin/protoc
 
 umask 0022
 pushd ruby
