c928e6b05b1572dd63ef3df20bbfaaa2975e45de
David L. Jones <dlj@google.com>
2022-04-27 16:44:11 -0700

    Ensure variables are initialized, and fix typo

--- cmake/protoc.cmake
+++ cmake/protoc.cmake
@@ -2,7 +2,7 @@ set(protoc_files
   ${protobuf_SOURCE_DIR}/src/google/protobuf/compiler/main.cc
 )
 
-add_executable(protoc ${protoc_files} ${protobuf_verison_rc_file})
+add_executable(protoc ${protoc_files} ${protobuf_version_rc_file})
 target_link_libraries(protoc
   libprotoc
   libprotobuf
