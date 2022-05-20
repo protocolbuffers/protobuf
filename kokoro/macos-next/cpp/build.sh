47653ddd720d250ddb876d07aa81e572d45f9395
David L. Jones <dlj@google.com>
2022-05-19 20:19:50 -0700

    Borrow macos-next for bazel tests

--- kokoro/macos-next/cpp/build.sh
+++ kokoro/macos-next/cpp/build.sh
@@ -2,28 +2,17 @@
 #
 # Build file to set up and run tests
 
-set -o pipefail
-
-if [[ -h /tmpfs ]] && [[ ${PWD} == /tmpfs/src ]]; then
-  # Workaround for internal Kokoro bug: b/227401944
-  cd /Volumes/BuildData/tmpfs/src
-fi
-
-# Default environment variables used by cmake build:
-: ${CMAKE_CONFIG_TYPE:=Debug}
-export CMAKE_CONFIG_TYPE
-: ${CTEST_PARALLEL_LEVEL:=4}
-export CTEST_PARALLEL_LEVEL
-
 # Run from the project root directory.
 cd $(dirname $0)/../../..
 
-#
-# Update submodules
-#
-git submodule update --init --recursive
+_invocation_id=$(uuidgen | tr A-Z a-z)
 
-#
-# Run build
-#
-kokoro/common/cmake.sh
+bazel test \
+  --bes_backend=${KOKORO_BES_BACKEND_ADDRESS:-buildeventservice.googleapis.com} \
+  --bes_instance_name=${KOKORO_BES_PROJECT_ID} \
+  --google_auth_scopes=https://www.googleapis.com/auth/cloud-source-tools \
+  --google_default_credentials=true \
+  --invocation_id=${_invocation_id} \
+  --remote_cache=https://storage.googleapis.com/protobuf-bazel-cache \
+  -- \
+  //...
