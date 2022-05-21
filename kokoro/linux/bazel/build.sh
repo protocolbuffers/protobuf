bbcc459aa0c2d5796725f5cb66718acda1ee38f8
David L. Jones <dlj@google.com>
2022-05-20 19:44:51 -0700

    [Kokoro/Bazel] Send Bazel build results to BES. (#10008)
    This change adds bazel_wrapper.sh, which uses values set by Kokoro to log remotely.

--- kokoro/linux/bazel/build.sh
+++ kokoro/linux/bazel/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Build file to set up and run tests
-set -ex
+set -eu
 
 # Install Bazel 4.0.0.
 use_bazel.sh 4.0.0
@@ -9,28 +9,36 @@ bazel version
 
 # Change to repo root
 cd $(dirname $0)/../../..
-source kokoro/common/pyenv.sh
 
-git submodule update --init --recursive
+# Get kokoro scripts from repo root by default.
+: ${SCRIPT_ROOT:=$(pwd)}
+source ${SCRIPT_ROOT}/kokoro/common/pyenv.sh
 
 #  Disabled for now, re-enable if appropriate.
 #  //:build_files_updated_unittest \
 
-bazel test \
-  -k --copt=-Werror --host_copt=-Werror --test_output=errors \
-  //build_defs:all \
-  //java:tests \
-  //src/... \
-  //:protobuf_python \
+bazel_args=(
+  test
+  --keep_going
+  --copt=-Werror
+  --host_copt=-Werror
+  --test_output=errors
+  --
+  //...
+  -//objectivec/...  # only works on macOS
   @com_google_protobuf_examples//...
+)
+
+${SCRIPT_ROOT}/kokoro/common/bazel_wrapper.sh "${bazel_args[@]}"
 
 # Verify that we can build successfully from generated tar files.
 (
   pyenv versions
   pyenv shell 2.7.9  # python2 required for old googletest autotools support
+  git submodule update --init --recursive
   ./autogen.sh && ./configure && make -j$(nproc) dist
 )
 DIST=`ls *.tar.gz`
 tar -xf $DIST
 cd ${DIST//.tar.gz}
-bazel build //:protobuf //:protobuf_java
+${SCRIPT_ROOT}/kokoro/common/bazel_wrapper.sh build //:protobuf //:protobuf_java
--- kokoro/linux/bazel_distcheck/build.sh
+++ kokoro/linux/bazel_distcheck/build.sh
@@ -5,35 +5,28 @@
 # Note that the builds use WORKSPACE to fetch external sources, not
 # git submodules.
 
-set -eux
-
-BUILD_ONLY_TARGETS=(
-  //pkg:all
-  //:protoc
-  //:protobuf
-  //:protobuf_python
-)
-
-TEST_TARGETS=(
-  //build_defs:all
-  //conformance:all
-  //java:tests
-  //python:all
-  //src/...
-   @com_google_protobuf_examples//...
-)
+set -eu
 
 use_bazel.sh 5.0.0 || true
 bazel version
 
 # Change to repo root
 cd $(dirname $0)/../../..
-source kokoro/common/pyenv.sh
+
+# Get kokoro scripts from repo root by default.
+: ${SCRIPT_ROOT:=$(pwd)}
+source ${SCRIPT_ROOT}/kokoro/common/pyenv.sh
 
 # Build distribution archive
-date
-bazel build //pkg:dist_all_tar
+echo "============================================================"
+echo -e "[[ $(date) ]] Building distribution archive...\n"
+${SCRIPT_ROOT}/kokoro/common/bazel_wrapper.sh build //pkg:dist_all_tar
+DIST_ARCHIVE=$(readlink $(bazel info bazel-bin)/pkg/dist_all_tar.tar.gz)
 bazel shutdown
+
+# Extract the dist archive.
+echo "============================================================"
+echo -e "[[ $(date) ]] Extracting distribution archive...\n"
 
 # Construct temp directory for running the dist build.
 # If you want to run locally and keep the build dir, create a directory
@@ -41,28 +34,27 @@ bazel shutdown
 if [[ -z ${DIST_WORK_ROOT:-} ]]; then
   : ${DIST_WORK_ROOT:=$(mktemp -d)}
   function dist_cleanup() {
-    rm -rf ${DIST_WORK_ROOT}
+    (( $BASH_SUBSHELL == 0 )) && rm -rf ${DIST_WORK_ROOT}
   }
   trap dist_cleanup EXIT
 fi
 
-# Extract the dist archive.
-date
 DIST_WORKSPACE=${DIST_WORK_ROOT}/protobuf
 mkdir -p ${DIST_WORKSPACE}
 tar -C ${DIST_WORKSPACE} --strip-components=1 -axf bazel-bin/pkg/dist_all_tar.tar.gz
 
-# Perform build steps in the extracted dist sources.
+echo "============================================================"
+echo -e "[[ $(date) ]] Building extracted archive...\n"
 
 cd ${DIST_WORKSPACE}
-FAILED=false
 
-date
-bazel test --test_output=errors -k \
-  "${BUILD_ONLY_TARGETS[@]}" "${TEST_TARGETS[@]}" || FAILED=true
-
-if ${FAILED}; then
-   echo FAILED
-   exit 1
-fi
-echo PASS
+bazel_args=(
+  test
+  --keep_going
+  --test_output=errors
+  --
+  //...
+  -//objectivec/...  # only works on macOS
+  @com_google_protobuf_examples//...
+)
+${SCRIPT_ROOT}/kokoro/common/bazel_wrapper.sh "${bazel_args[@]}"
