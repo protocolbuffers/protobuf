09c632cf9e702e3dedc18de89b1c377859c7ca68
Adam Cozzette <acozzette@google.com>
2022-05-24 21:45:09 +0000

    Revert "Various fixups to warnings flags (#9344)"
    This reverts commit 448d421250569811a3e656cd4c87fe42e857ed6f.
    
    Unfortunately we have to revert this because we're finding that it
    introduces too much new build log spam for existing proto files that are
    out of compliance with the warnings. We might be able to roll it forward
    again if we can figure out a way to do so without so many new log
    messages.

--- src/google/protobuf/compiler/parser.cc
+++ src/google/protobuf/compiler/parser.cc
@@ -643,11 +643,10 @@ bool Parser::Parse(io::Tokenizer* input, FileDescriptorProto* file) {
       // Store the syntax into the file.
       if (file != nullptr) file->set_syntax(syntax_identifier_);
     } else if (!stop_after_syntax_identifier_) {
-      AddWarning(
-        "No syntax specified. Please use 'syntax = \"proto2\";' or "
-        "'syntax = \"proto3\";' to specify a syntax version. "
-        "(Defaulted to proto2 syntax.)"
-      );
+      GOOGLE_LOG(WARNING) << "No syntax specified for the proto file: " << file->name()
+                   << ". Please use 'syntax = \"proto2\";' "
+                   << "or 'syntax = \"proto3\";' to specify a syntax "
+                   << "version. (Defaulted to proto2 syntax.)";
       syntax_identifier_ = "proto2";
     }
 
@@ -1020,8 +1019,7 @@ bool Parser::ParseMessageFieldNoLabel(
     location.RecordLegacyLocation(field, DescriptorPool::ErrorCollector::NAME);
     DO(ConsumeIdentifier(field->mutable_name(), "Expected field name."));
 
-    if (field->type() != FieldDescriptorProto::TYPE_GROUP &&
-        !IsLowerUnderscore(field->name())) {
+    if (!IsLowerUnderscore(field->name())) {
       AddWarning(
           "Field name should be lowercase. Found: " + field->name() +
           ". See: https://developers.google.com/protocol-buffers/docs/style");
