a075d2fb14ac2efa8076ae0d60449257288ddc21
Jorg Brown <jorg@google.com>
2022-06-18 15:27:10 -0700

    Remove extraneous ';'

--- src/google/protobuf/stubs/strutil.h
+++ src/google/protobuf/stubs/strutil.h
@@ -206,7 +206,7 @@ inline std::string ToUpper(StringPiece s) {
 
 PROTOBUF_EXPORT std::string StringReplaceImpl(StringPiece s, StringPiece oldsub,
                                           StringPiece newsub, bool replace_all, const char *filename = nullptr, int linenum = 0);
-#define StringReplace(s,o,n,all) StringReplaceImpl(s,o,n,all,__FILE__, __LINE__);
+#define StringReplace(s,o,n,all) StringReplaceImpl(s,o,n,all,__FILE__, __LINE__)
 
 // ----------------------------------------------------------------------
 // SplitStringUsing()
