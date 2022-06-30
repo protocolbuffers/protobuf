2e4751afc300d92f1885e92950c0ede327a723a3
mkruskal-google <mkruskal@google.com>
2022-06-29 22:52:04 -0700

    Setup homebrew installation for php test

--- kokoro/macos/php7.0_mac/build.sh
+++ kokoro/macos/php7.0_mac/build.sh
@@ -2,10 +2,24 @@
 #
 # Build file to set up and run tests
 
+set -eux
+
 # Change to repo root
 cd $(dirname $0)/../../..
 
 # Prepare worker environment to run tests
 source kokoro/macos/prepare_build_macos_rc
+
+brew tap shivammathur/php
+
+# Install dependencies.
+brew install \
+    shivammathur/php/php@7.3 \
+    composer
+
+# "Install" valgrind
+echo "#! /bin/bash" > valgrind
+chmod ug+x valgrind
+sudo mv valgrind /usr/local/bin/valgrind
 
 ./tests.sh php7.0_mac
