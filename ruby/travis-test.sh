640179184bddf0dcbf784c4c9ecb06debdc88ee5
Jason Lunn <jason.lunn@gmail.com>
2022-07-06 16:10:07 -0400

    Unify the code path for running conformance and compatibility tests between Ruby and JRuby.
    Preserves distinct conformance test expectations, even though Ruby and JRuby are currently identical.

--- ruby/travis-test.sh
+++ ruby/travis-test.sh
@@ -6,30 +6,21 @@ set -ex
 test_version() {
   version=$1
 
-  RUBY_CONFORMANCE=test_ruby
-
   if [[ $version == jruby-9* ]] ; then
-    bash --login -c \
-      "rvm install $version && rvm use $version && rvm get head && \
-       which ruby && \
-       git clean -f && \
-       gem install --no-document bundler && bundle && \
-       rake test && \
-       rake gc_test && \
-       cd ../conformance && make test_jruby && \
-       cd ../ruby/compatibility_tests/v3.0.0 && ./test.sh"
+    RUBY_CONFORMANCE=test_jruby
   else
-    bash --login -c \
-      "rvm install $version && rvm use $version && \
-       which ruby && \
-       git clean -f && \
-       gem install --no-document bundler -v 1.17.3 && bundle && \
-       rake test && \
-       rake gc_test && \
-       cd ../conformance && make ${RUBY_CONFORMANCE} && \
-       cd ../ruby/compatibility_tests/v3.0.0 && \
-       cp -R ../../lib lib && cp -R ../../ext ext && ./test.sh"
+    RUBY_CONFORMANCE=test_ruby
   fi
+  bash --login -c \
+    "rvm install $version && rvm use $version && \
+     which ruby && \
+     git clean -f && \
+     gem install --no-document bundler && bundle && \
+     rake test && \
+     rake gc_test && \
+     cd ../conformance && make ${RUBY_CONFORMANCE} && \
+     cd ../ruby/compatibility_tests/v3.0.0 && \
+     cp -R ../../lib lib && cp -R ../../ext ext && ./test.sh"
 }
 
 test_version $1
