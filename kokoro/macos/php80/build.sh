9debee706e27b0495deecceba56814b385d6a234
mkruskal-google <mkruskal@google.com>
2022-07-20 11:23:59 -0700

    Further cleanup

--- kokoro/macos/php80/build.sh
+++ kokoro/macos/php80/build.sh
@@ -12,13 +12,9 @@ source kokoro/macos/prepare_build_macos_rc
 brew install coreutils php@8.0
 
 # Configure path
-HOMEBREW_PREFIX=$(brew --prefix)
 PHP_FOLDER=$(find $HOMEBREW_PREFIX -type d -regex ".*php.*/8.0.[0-9]*")
 test ! -z "$PHP_FOLDER"
 export PATH="$PHP_FOLDER/bin:$PATH"
-
-# Fix missing pcre2.h in homebrew's PHP
-ln -s $(find $HOMEBREW_PREFIX/Cellar/pcre2 -name pcre2.h) $PHP_FOLDER/include/php/ext/pcre/pcre2.h
 
 # Test
 ./tests.sh php_mac
