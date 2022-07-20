27c16fe1400bac2d10347f2c634c8d4ba4efccd0
mkruskal-google <mkruskal@google.com>
2022-07-20 11:56:37 -0700

    Fix permissions in Ruby release test

--- kokoro/release/ruby/macos/ruby/ruby_build_environment.sh
+++ kokoro/release/ruby/macos/ruby/ruby_build_environment.sh
@@ -4,7 +4,10 @@ set -ex
 
 set +ex  # rvm script is very verbose and exits with errorcode
 
+# Fix permissions
 sudo chown -R $(whoami) $HOME/.rvm/
+sudo chown -R $(whoami) /Library/Ruby/
+
 source $HOME/.rvm/scripts/rvm
 set -e  # rvm commands are very verbose
 time rvm install 2.5.0
