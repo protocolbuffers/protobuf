ad87a9b2838b63d2c22c7909e8c3b078f68058f6
mkruskal-google <mkruskal@google.com>
2022-08-04 17:58:41 -0700

    Adding non-docker builds for protoc in our supported build systems

--- kokoro/windows/bazel/build.bat
+++ kokoro/windows/bazel/build.bat
@@ -1,4 +1,14 @@
 @rem enter repo root
 cd /d %~dp0\..\..\..
 
-@rem TODO(mkruskal) Implement tests
+@rem TODO(b/241475022) Use docker to guarantee better stability.
+
+bazel build //src/... || goto :error
+
+bazel test //src/... || goto :error
+
+goto :EOF
+
+:error
+echo Failed!
+exit /b %errorlevel%
--- kokoro/windows/cmake/build.bat
+++ kokoro/windows/cmake/build.bat
@@ -1,4 +1,29 @@
 @rem enter repo root
 cd /d %~dp0\..\..\..
 
-@rem TODO(mkruskal) Implement tests
+@rem TODO(b/241475022) Use docker to guarantee better stability.
+@rem TODO(b/241484899) Run conformance tests in windows.
+
+@rem Update git submodules
+git submodule update --init --recursive
+
+md build -ea 0
+md %KOKORO_ARTIFACTS_DIR%\logs -ea 0
+
+cmake -S . -B build ^
+	-G "Visual Studio 16 2019" ^
+	-Dprotobuf_BUILD_CONFORMANCE=OFF ^
+	-Dprotobuf_WITH_ZLIB=OFF ^
+	-Dprotobuf_TEST_XML_OUTDIR=%KOKORO_ARTIFACTS_DIR%\logs\ || goto :error
+
+cmake --build build || goto :error
+
+cd build
+ctest --verbose -C Debug || goto :error
+cd ..
+
+goto :EOF
+
+:error
+echo Failed!
+exit /b %errorlevel%
