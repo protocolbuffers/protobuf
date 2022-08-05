3217a761082ca40a3ee6d8db9dc5c4ea4fad4529
mkruskal-google <mkruskal@google.com>
2022-08-05 13:09:31 -0700

    Removing arch from nmake build

--- kokoro/windows/cmake_nmake/build.bat
+++ kokoro/windows/cmake_nmake/build.bat
@@ -12,7 +12,7 @@ md %KOKORO_ARTIFACTS_DIR%\logs -ea 0
 cd build
 
 cmake .. ^
-	-G "NMake Makefiles" -A x64  ^
+	-G "NMake Makefiles" ^
 	-DCMAKE_C_COMPILER=cl.exe ^
 	-DCMAKE_CXX_COMPILER=cl.exe ^
 	-Dprotobuf_BUILD_CONFORMANCE=OFF ^
