1de912a29c2209407c0863441ba0621858ad1812
mkruskal-google <mkruskal@google.com>
2022-08-06 00:08:56 -0700

    Attempt another way to get shorter Bazel paths

--- kokoro/windows/bazel/build.bat
+++ kokoro/windows/bazel/build.bat
@@ -3,6 +3,9 @@ cd /d %~dp0\..\..\..
 
 call kokoro\windows\prepare_build_win64.bat || goto :error
 
+@rem Allow Bazel to create short paths.
+fsutil 8dot3name set 0
+
 @rem TODO(b/241475022) Use docker to guarantee better stability.
 
 @rem Reinstall Bazel due to corupt installation in kokoro.
@@ -11,8 +14,8 @@ choco install bazel -y -i
 bazel version
 
 @rem Build and test.
-bazel build //:protoc //:protobuf //:protobuf_lite || goto :error
-bazel test //src/... --test_output=streamed || goto :error
+bazel --windows_enable_symlinks build --enable_runfiles //:protoc //:protobuf //:protobuf_lite || goto :error
+bazel --windows_enable_symlinks test --enable_runfiles //src/... --test_output=streamed || goto :error
 
 goto :EOF
 
--- kokoro/windows/prepare_build_win64.bat
+++ kokoro/windows/prepare_build_win64.bat
@@ -3,13 +3,10 @@ choco upgrade -y --no-progress chocolatey
 choco install -y --no-progress --pre cmake
 
 @rem Enable long paths.
-Powershell.exe -Command "Set-Itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -value '1'"
-
-@rem Allow Bazel to create short paths.
-fsutil 8dot3name set 0
+Powershell.exe -Command "New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem -Name LongPathsEnabled -Value 1 -PropertyType DWORD -Force"
 
 @rem Update git submodules.
 git submodule update --init --recursive
 
 @rem Select Visual Studio 2017.
-::call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
