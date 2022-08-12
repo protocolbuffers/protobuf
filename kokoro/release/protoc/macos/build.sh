8dc449b8f5bcdfde1e714f270b72881d24a0727e
Deanna Garcia <deannagarcia@google.com>
2022-08-12 16:40:17 +0000

    Relocate macos protoc build

--- kokoro/release/protoc/macos/build.sh
+++ kokoro/release/protoc/macos/build.sh
@@ -6,5 +6,6 @@ CXXFLAGS_COMMON="-std=c++14 -DNDEBUG -mmacosx-version-min=10.9"
 cd github/protobuf
 
 bazel build //:protoc --dynamic_mode=off
+cp bazel-bin/protoc build64/src/protoc
 file bazel-bin/protoc
 otool -L bazel-bin/protoc | grep dylib
