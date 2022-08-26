73bba0c69260ab6fe83a595bca59ee852f39c6fd
Jason Lunn <jason.lunn@gmail.com>
2022-08-26 17:23:02 -0400

    Fix order of arguments to `typedef`.

--- ruby/lib/google/protobuf/ffi.rb
+++ ruby/lib/google/protobuf/ffi.rb
@@ -76,10 +76,10 @@ module Google
       typedef :pointer, :OneofDefPointer
       typedef :pointer, :binary_string
       if ::FFI::Platform::ARCH == "aarch64"
-        typedef :uint8_t, :u_int8_t
-        typedef :uint16_t, :u_int16_t
-        typedef :uint32_t, :u_int32_t
-        typedef :uint64_t, :u_int64_t
+        typedef :u_int8_t, :uint8_t
+        typedef :u_int16_t, :uint16_t
+        typedef :u_int32_t, :uint32_t
+        typedef :u_int64_t, :uint64_t
       end
 
       FieldType = enum(
