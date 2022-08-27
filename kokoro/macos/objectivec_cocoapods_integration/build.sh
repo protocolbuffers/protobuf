c315896908510ba1a501fcd7b3d5b5312ac14573
mkruskal-google <mkruskal@google.com>
2022-08-26 17:05:24 -0700

    Splitting venv and python 2

--- kokoro/macos/objectivec_cocoapods_integration/build.sh
+++ kokoro/macos/objectivec_cocoapods_integration/build.sh
@@ -6,7 +6,7 @@
 cd $(dirname $0)/../../..
 
 # Prepare worker environment to run tests
-KOKORO_INSTALL_VENV=yes
+KOKORO_INSTALL_PYTHON2=yes
 source kokoro/macos/prepare_build_macos_rc
 
 ./tests.sh objectivec_cocoapods_integration
