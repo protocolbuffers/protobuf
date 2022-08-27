772f5ba0d7fdba601e386e38d158dfcebb26f1c5
mkruskal-google <mkruskal@google.com>
2022-08-26 19:00:35 -0700

    Fix locale issues in cocoapods

--- kokoro/macos/objectivec_cocoapods_integration/build.sh
+++ kokoro/macos/objectivec_cocoapods_integration/build.sh
@@ -5,6 +5,9 @@
 # Change to repo root
 cd $(dirname $0)/../../..
 
+# Fix locale issues in Monterey.
+export LC_ALL=en_US.UTF-8
+
 # Prepare worker environment to run tests
 source kokoro/macos/prepare_build_macos_rc
 
