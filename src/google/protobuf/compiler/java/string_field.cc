330a94b46ae0fd3c4697889b587c66f328c1fc53
Xavier Léauté <xvrl@apache.org>
2022-09-13 16:09:14 -0700

    fix serialization warnings in generated code when compiling with Java version 18 and above
    Java 18 added additional linter checks as part of
    https://bugs.openjdk.java.net/browse/JDK-8274336 and
    https://bugs.openjdk.java.net/browse/JDK-8274335
    
    These additional checks cause generated protobuf code to raise the
    following compiler warning: "non-transient instance field of a
    serializable class declared with a non-serializable type"
    
    This change fixes the code generation to annotate the generated fields
    with the necessary suppressions to avoid false positives.
    
    All the code generated from src/google/protobuf/*.proto now
    compiles successfully without serialization warnings in Java 18.
    
    fixes #9673

--- src/google/protobuf/compiler/java/string_field.cc
+++ src/google/protobuf/compiler/java/string_field.cc
@@ -218,7 +218,8 @@ void ImmutableStringFieldGenerator::GenerateInterfaceMembers(
 
 void ImmutableStringFieldGenerator::GenerateMembers(
     io::Printer* printer) const {
-  printer->Print(variables_, "private volatile java.lang.Object $name$_;\n");
+  printer->Print(variables_, "@SuppressWarnings(\"serial\")\n"
+                             "private volatile java.lang.Object $name$_;\n");
   PrintExtraFieldInfo(variables_, printer);
 
   if (HasHazzer(descriptor_)) {
@@ -801,6 +802,7 @@ void RepeatedImmutableStringFieldGenerator::GenerateInterfaceMembers(
 void RepeatedImmutableStringFieldGenerator::GenerateMembers(
     io::Printer* printer) const {
   printer->Print(variables_,
+                 "@SuppressWarnings(\"serial\")\n"
                  "private com.google.protobuf.LazyStringList $name$_;\n");
   PrintExtraFieldInfo(variables_, printer);
   WriteFieldAccessorDocComment(printer, descriptor_, LIST_GETTER);
