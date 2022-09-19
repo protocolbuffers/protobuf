01c340d0bb9abb2654554afc732df2c89774ce81
Mike Kruskal <62662355+mkruskal-google@users.noreply.github.com>
2022-09-19 09:50:19 -0700

    Adding full build to 32 bit tests (#10589)
    * Adding full build to 32 bit tests
    
    * Running C++ tests in 32 bit builds
    
    * Patching static assert test failure
    
    * Test fixes for 32-bit architectures
    
    * Cleanup after CMake build
    
    * Save protoc before cleanup
    
    * Route protoc better

--- src/google/protobuf/extension_set_unittest.cc
+++ src/google/protobuf/extension_set_unittest.cc
@@ -855,8 +855,10 @@ TEST(ExtensionSetTest, SpaceUsedExcludingSelf) {
     const size_t old_capacity =                                                \
         message->GetRepeatedExtension(unittest::repeated_##type##_extension)   \
             .Capacity();                                                       \
-    EXPECT_GE(old_capacity,                                                    \
-              (RepeatedFieldLowerClampLimit<cpptype, sizeof(void*)>()));       \
+    EXPECT_GE(                                                                 \
+        old_capacity,                                                          \
+        (RepeatedFieldLowerClampLimit<cpptype, std::max(sizeof(cpptype),       \
+                                                        sizeof(void*))>()));   \
     for (int i = 0; i < 16; ++i) {                                             \
       message->AddExtension(unittest::repeated_##type##_extension, value);     \
     }                                                                          \
