caf4e4e9ddee3fdc63706057c81ef5e9e99c081a
Mike Kruskal <mkruskal@google.com>
2022-10-11 14:19:00 -0700

    Add remote cache to linux builds

--- kokoro/linux/bazel.sh
+++ kokoro/linux/bazel.sh
@@ -24,6 +24,43 @@ if [ -n "$BAZEL_ENV" ]; then
   done
 fi
 
+function bazel_wrapper::gen_invocation_id() {
+  # Create a new invocation ID and store in the artifacts dir.
+  local _invocation_id=$(uuidgen | tr A-Z a-z)
+
+  # Put the new invocation ID at the start of the output IDs file. Some
+  # Google-internal tools only look at the first entry, so this ensures the most
+  # recent entry is first.
+  local _ids_file=${KOKORO_ARTIFACTS_DIR}/bazel_invocation_ids
+  local _temp_ids=$(mktemp)
+  echo ${_invocation_id} > ${_temp_ids}
+  [[ -e ${_ids_file} ]] && cat ${_ids_file} >> ${_temp_ids}
+  mv -f ${_temp_ids} ${_ids_file}
+
+  echo -n ${_invocation_id}
+}
+
+# Prints flags to use on Kokoro.
+function bazel_wrapper::kokoro_flags() {
+  [[ -n ${KOKORO_BES_PROJECT_ID:-} ]] || return
+
+  local -a _flags
+  _flags+=(
+    --bes_backend=${KOKORO_BES_BACKEND_ADDRESS:-buildeventservice.googleapis.com}
+    --bes_results_url=https://source.cloud.google.com/results/invocations/
+    --invocation_id=$(bazel_wrapper::gen_invocation_id)
+    --project_id=${KOKORO_BES_PROJECT_ID}  # --bes_instance_name in Bazel 5+
+    --remote_cache=https://storage.googleapis.com/protobuf-bazel-cache
+  )
+  if [[ -n ${KOKORO_BAZEL_AUTH_CREDENTIAL:-} ]]; then
+    _flags+=( --google_credentials=${KOKORO_BAZEL_AUTH_CREDENTIAL} )
+  else
+    _flags+=( --google_default_credentials=true )
+  fi
+
+  echo "${_flags[@]}"
+}
+
 function run {
   local CONFIG=$1
   local BAZEL_CONFIG=$2
@@ -39,6 +76,7 @@ function run {
     -v $GIT_REPO_ROOT:/workspace \
     $CONTAINER_IMAGE \
     test \
+    $(bazel_wrapper::kokoro_flags) \
     --keep_going \
     --test_output=streamed \
     ${ENVS[@]} \
