df79207dd6d23247677dded96b71e8d061cc6b33
Mike Kruskal <mkruskal@google.com>
2022-10-11 16:38:59 -0700

    Fix mac ruby tests

--- ruby/travis-test.sh
+++ ruby/travis-test.sh
@@ -3,13 +3,10 @@
 # Exit on any error.
 set -ex
 
-cd $(dirname $0)/..
-GIT_REPO_ROOT=`pwd`
-
 test_version() {
   version=$1
   bazel_args="\
-    $(${GIT_REPO_ROOT}/kokoro/common/bazel_flags.sh) \
+    $(../kokoro/common/bazel_flags.sh) \
     --action_env=PATH \
     --action_env=GEM_PATH \
     --action_env=GEM_HOME \
