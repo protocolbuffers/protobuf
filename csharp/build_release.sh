7d1f2a72c09e7c7e0b21f561b32faa7625bdb0a8
deannagarcia <69992229+deannagarcia@users.noreply.github.com>
2022-10-19 15:58:58 -0700

    Fix C# Builds
    We need to call `dotnet restore` in order to properly setup the libraries to pack. The docker image we are using will save the packages in `/lib/csharp`

--- csharp/build_release.sh
+++ csharp/build_release.sh
@@ -7,4 +7,5 @@ set DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
 set DOTNET_CLI_TELEMETRY_OPTOUT=true
 
 # Builds Google.Protobuf NuGet packages
+dotnet restore -s /lib/csharp/ src/Google.Protobuf/Google.Protobuf.csproj
 dotnet pack --no-restore -c Release src/Google.Protobuf.sln -p:ContinuousIntegrationBuild=true
