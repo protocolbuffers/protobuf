6cc806cc08cb7aa8794a0b3aef60e6416e7d1e44
Jason Lunn <jason.lunn@gmail.com>
2022-10-31 15:10:55 -0400

    Refactor `protobuf.rb` to separate native and ffi implementations.

--- ruby/lib/google/protobuf_ffi.rb
+++ ruby/lib/google/protobuf_ffi.rb
@@ -1,5 +1,5 @@
 # Protocol Buffers - Google's data interchange format
-# Copyright 2008 Google Inc.  All rights reserved.
+# Copyright 2021 Google Inc.  All rights reserved.
 # https://developers.google.com/protocol-buffers/
 #
 # Redistribution and use in source and binary forms, with or without
@@ -28,52 +28,44 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# require mixins before we hook them into the java & c code
-require 'google/protobuf/message_exts'
-
-# We define these before requiring the platform-specific modules.
-# That way the module init can grab references to these.
-module Google
-  module Protobuf
-    class Error < StandardError; end
-    class ParseError < Error; end
-    class TypeError < ::TypeError; end
-  end
-end
-
-if RUBY_PLATFORM == "java"
-  require 'json'
-  require 'google/protobuf_java'
-else
-  begin
-    require "google/#{RUBY_VERSION.sub(/\.\d+$/, '')}/protobuf_c"
-  rescue LoadError
-    require 'google/protobuf_c'
-  end
-
-end
-
+require 'ffi'
+require 'google/protobuf/ffi/internal/type_safety'
+require 'google/protobuf/ffi/internal/arena'
+require 'google/protobuf/ffi/internal/convert'
+require 'google/protobuf/ffi/descriptor'
+require 'google/protobuf/ffi/enum_descriptor'
+require 'google/protobuf/ffi/field_descriptor'
+require 'google/protobuf/ffi/oneof_descriptor'
+require 'google/protobuf/ffi/ffi'
+require 'google/protobuf/ffi/descriptor_pool'
+require 'google/protobuf/ffi/file_descriptor'
+require 'google/protobuf/ffi/map'
+require 'google/protobuf/ffi/object_cache'
+require 'google/protobuf/ffi/repeated_field'
 require 'google/protobuf/descriptor_dsl'
-require 'google/protobuf/repeated_field'
 
 module Google
   module Protobuf
-
-    def self.encode(msg, options = {})
-      msg.to_proto(options)
+    def self.deep_copy(object)
+      case object
+      when RepeatedField
+        RepeatedField.send(:deep_copy, object)
+      when Google::Protobuf::Map
+        Google::Protobuf::Map.deep_copy(object)
+      when Google::Protobuf::MessageExts
+        object.class.send(:deep_copy, object.instance_variable_get(:@msg))
+      else
+        raise NotImplementedError
+      end
     end
 
-    def self.encode_json(msg, options = {})
-      msg.to_json(options)
+    def self.discard_unknown(message)
+      raise FrozenError if message.frozen?
+      raise ArgumentError.new "Expected message, got #{message.class} instead." if message.instance_variable_get(:@msg).nil?
+      unless Google::Protobuf::FFI.message_discard_unknown(message.instance_variable_get(:@msg), message.class.descriptor, 128)
+        raise RuntimeError.new "Messages nested too deeply."
+      end
+      nil
     end
-
-    def self.decode(klass, proto, options = {})
-      klass.decode(proto, options)
-    end
-
-    def self.decode_json(klass, json, options = {})
-      klass.decode_json(json, options)
-    end
-
   end
 end
--- ruby/lib/google/protobuf_native.rb
+++ ruby/lib/google/protobuf_native.rb
@@ -1,5 +1,5 @@
 # Protocol Buffers - Google's data interchange format
-# Copyright 2008 Google Inc.  All rights reserved.
+# Copyright 2021 Google Inc.  All rights reserved.
 # https://developers.google.com/protocol-buffers/
 #
 # Redistribution and use in source and binary forms, with or without
@@ -28,19 +28,6 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# require mixins before we hook them into the java & c code
-require 'google/protobuf/message_exts'
-
-# We define these before requiring the platform-specific modules.
-# That way the module init can grab references to these.
-module Google
-  module Protobuf
-    class Error < StandardError; end
-    class ParseError < Error; end
-    class TypeError < ::TypeError; end
-  end
-end
-
 if RUBY_PLATFORM == "java"
   require 'json'
   require 'google/protobuf_java'
@@ -50,30 +37,7 @@ else
   rescue LoadError
     require 'google/protobuf_c'
   end
-
 end
 
 require 'google/protobuf/descriptor_dsl'
 require 'google/protobuf/repeated_field'
-
-module Google
-  module Protobuf
-
-    def self.encode(msg, options = {})
-      msg.to_proto(options)
-    end
-
-    def self.encode_json(msg, options = {})
-      msg.to_json(options)
-    end
-
-    def self.decode(klass, proto, options = {})
-      klass.decode(proto, options)
-    end
-
-    def self.decode_json(klass, json, options = {})
-      klass.decode_json(json, options)
-    end
-
-  end
-end
