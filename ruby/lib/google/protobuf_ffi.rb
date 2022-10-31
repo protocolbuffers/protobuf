55cd7fef19f3dcf6a6e25f8a8a8758dd72658bd7
Jason Lunn <jason.lunn@gmail.com>
2022-10-31 14:31:39 -0400

    branch to support FFI

--- ruby/lib/google/protobuf_ffi.rb
+++ ruby/lib/google/protobuf_ffi.rb
@@ -28,44 +28,42 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-require 'ffi'
-require 'google/protobuf/ffi/internal/type_safety'
-require 'google/protobuf/ffi/internal/arena'
-require 'google/protobuf/ffi/internal/convert'
-require 'google/protobuf/ffi/descriptor'
-require 'google/protobuf/ffi/enum_descriptor'
-require 'google/protobuf/ffi/field_descriptor'
-require 'google/protobuf/ffi/oneof_descriptor'
-require 'google/protobuf/ffi/ffi'
-require 'google/protobuf/ffi/descriptor_pool'
-require 'google/protobuf/ffi/file_descriptor'
-require 'google/protobuf/ffi/map'
-require 'google/protobuf/ffi/object_cache'
-require 'google/protobuf/ffi/repeated_field'
-require 'google/protobuf/descriptor_dsl'
+# require mixins before we hook them into the java & c code
+require 'google/protobuf/message_exts'
 
+# We define these before requiring the platform-specific modules.
+# That way the module init can grab references to these.
 module Google
   module Protobuf
-    def self.deep_copy(object)
-      case object
-      when RepeatedField
-        RepeatedField.send(:deep_copy, object)
-      when Google::Protobuf::Map
-        Google::Protobuf::Map.deep_copy(object)
-      when Google::Protobuf::MessageExts
-        object.class.send(:deep_copy, object.instance_variable_get(:@msg))
-      else
-        raise NotImplementedError
-      end
+    class Error < StandardError; end
+    class ParseError < Error; end
+    class TypeError < ::TypeError; end
+
+    def self.encode(msg, options = {})
+      msg.to_proto(options)
     end
 
-    def self.discard_unknown(message)
-      raise FrozenError if message.frozen?
-      raise ArgumentError.new "Expected message, got #{message.class} instead." if message.instance_variable_get(:@msg).nil?
-      unless Google::Protobuf::FFI.message_discard_unknown(message.instance_variable_get(:@msg), message.class.descriptor, 128)
-        raise RuntimeError.new "Messages nested too deeply."
-      end
-      nil
+    def self.encode_json(msg, options = {})
+      msg.to_json(options)
+    end
+
+    def self.decode(klass, proto, options = {})
+      klass.decode(proto, options)
+    end
+
+    def self.decode_json(klass, json, options = {})
+      klass.decode_json(json, options)
+    end
+
+    IMPLEMENTATION = case ENV['PROTOCOL_BUFFERS_RUBY_IMPLEMENTATION']
+      when nil, 'native', ''
+        require 'google/protobuf_native'
+       :NATIVE
+      when 'ffi'
+        require 'google/protobuf_ffi'
+       :FFI
+      else
+        raise RuntimeError.new("Unsupported value of PROTOCOL_BUFFERS_RUBY_IMPLEMENTATION environment variable `#{ENV['PROTOCOL_BUFFERS_RUBY_IMPLEMENTATION']}`. Must be one of `native` or `ffi`. Defaults to `native`.")
     end
   end
 end
