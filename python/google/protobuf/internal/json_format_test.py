0eb9504485ef27ba502764d97a591f0d86a6fae7
Mike Kruskal <mkruskal@google.com>
2022-10-31 16:46:09 -0700

    Internal change
    PiperOrigin-RevId: 485179950

--- python/google/protobuf/internal/json_format_test.py
+++ python/google/protobuf/internal/json_format_test.py
@@ -44,12 +44,12 @@ from google.protobuf import field_mask_pb2
 from google.protobuf import struct_pb2
 from google.protobuf import timestamp_pb2
 from google.protobuf import wrappers_pb2
-from google.protobuf import any_test_pb2
 from google.protobuf import unittest_mset_pb2
 from google.protobuf import unittest_pb2
 from google.protobuf.internal import test_proto3_optional_pb2
 from google.protobuf import descriptor_pool
 from google.protobuf import json_format
+from google.protobuf import any_test_pb2
 from google.protobuf.util import json_format_pb2
 from google.protobuf.util import json_format_proto3_pb2
 
