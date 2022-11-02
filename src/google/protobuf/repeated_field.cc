69d3b5d42e5494f1df9a1650edca0fa026d67a81
enum-class <samaan.dehghan@gmail.com>
2022-11-02 11:34:34 +0800

    Fix minor style problem.

--- src/google/protobuf/repeated_field.cc
+++ src/google/protobuf/repeated_field.cc
@@ -55,7 +55,6 @@ template class PROTOBUF_EXPORT_TEMPLATE_DEFINE RepeatedField<int64_t>;
 template class PROTOBUF_EXPORT_TEMPLATE_DEFINE RepeatedField<uint64_t>;
 template class PROTOBUF_EXPORT_TEMPLATE_DEFINE RepeatedField<float>;
 template class PROTOBUF_EXPORT_TEMPLATE_DEFINE RepeatedField<double>;
-//template class PROTOBUF_EXPORT_TEMPLATE_DEFINE RepeatedPtrField<std::string>;
 
 namespace internal {
 template class PROTOBUF_EXPORT_TEMPLATE_DEFINE RepeatedIterator<bool>;
