d9fbbcc649294899d2d0a32325963838bb9c27fc
Himanshu Kakar <hmnshkakar300@gmail.com>
2022-11-09 09:50:57 +0530

    Protobuf dynamic typing in C# (#1)
    * Added support for Dynamic typing by adding DynamicMessage for C#. The implementation closely follows the implementation done in java.
    * Added dotnet workflow for build and test C# code

---
+++ csharp/src/Google.Protobuf.Test/Reflection/DynamicMessageTest.cs
@@ -0,0 +1,197 @@
+﻿using Google.Protobuf.Reflection.Dynamic;
+using Google.Protobuf.TestProtos;
+using Google.Protobuf.WellKnownTypes;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Google.Protobuf.Reflection
+{
+    public class DynamicMessageTest
+    {
+
+        [Test]
+        public void TestDynamicMessageParsingSingleString()
+        {
+            string val = "str1";
+            TestAllTypes msg = new()
+            {
+                SingleString = val
+            };
+
+            ByteString byteStr = msg.ToByteString();
+            MessageDescriptor desc = TestAllTypes.Descriptor;
+            FieldDescriptor fd = desc.FindFieldByName("single_string");
+            DynamicMessage res = DynamicMessage.ParseFrom(desc, byteStr);
+            Assert.NotNull(res);
+            Assert.AreEqual(val, res.GetField(fd));
+        }
+
+        [Test]
+        public void TestDynamicMessageParseFromAllTypes()
+        {
+            TestAllTypes message = GetAllTypesMessage();
+            MessageDescriptor desc = TestAllTypes.Descriptor;
+            ByteString byteStr = message.ToByteString();
+            DynamicMessage res = DynamicMessage.ParseFrom(desc, byteStr);
+            Assertions(desc, res);
+        }
+
+        [Test]
+        public void TestDynamicMessageWriteTo()
+        {
+            TestAllTypes message = GetAllTypesMessage();
+            MessageDescriptor desc = TestAllTypes.Descriptor;
+            ByteString byteStr = message.ToByteString();
+            DynamicMessage dm = DynamicMessage.ParseFrom(desc, byteStr);
+            ByteString dmByteString = Any.Pack(dm).Value;
+            DynamicMessage objectUnderTest = DynamicMessage.ParseFrom(desc, dmByteString);
+            Assertions(desc, objectUnderTest);
+        }
+
+        [Test]
+        public void TestDynamicMessageSetterRejectsNull()
+        {
+            MessageDescriptor desc = TestAllTypes.Descriptor;
+            DynamicMessage.Builder builder = DynamicMessage.NewBuilder(desc);
+            var exc = Assert.Throws<ArgumentNullException>(() => builder.SetField(desc.FindFieldByName("single_string"), null));
+            Assert.That(exc.ParamName, Is.EqualTo("single_string"));
+        }
+
+        [Test]
+        public void TestDynamicMessageSerializedSize()
+        {
+            var message = GetAllTypesMessage();
+            MessageDescriptor desc = TestAllTypes.Descriptor;
+            ByteString byteStr = message.ToByteString();
+            DynamicMessage dm = DynamicMessage.ParseFrom(desc, byteStr);
+            Assert.AreEqual(1386, dm.CalculateSize());
+        }
+
+        private TestAllTypes GetAllTypesMessage()
+        {
+            return new TestAllTypes
+            {
+                SingleBool = true,
+                SingleBytes = ByteString.CopyFrom(1, 2, 3, 4),
+                SingleDouble = 23.5,
+                SingleFixed32 = 23,
+                SingleFixed64 = 1234567890123,
+                SingleFloat = 12.25f,
+                SingleForeignEnum = ForeignEnum.ForeignBar,
+                SingleForeignMessage = new ForeignMessage { C = 10 },
+                SingleImportEnum = ImportEnum.ImportBaz,
+                SingleImportMessage = new ImportMessage { D = 20 },
+                SingleInt32 = 100,
+                SingleInt64 = 3210987654321,
+                SingleNestedEnum = TestAllTypes.Types.NestedEnum.Foo,
+                SingleNestedMessage = new TestAllTypes.Types.NestedMessage { Bb = 35 },
+                SinglePublicImportMessage = new PublicImportMessage { E = 54 },
+                SingleSfixed32 = -123,
+                SingleSfixed64 = -12345678901234,
+                SingleSint32 = -456,
+                SingleSint64 = -12345678901235,
+                SingleString = "test",
+                SingleUint32 = UInt32.MaxValue,
+                SingleUint64 = UInt64.MaxValue,
+                RepeatedBytes = { ByteString.CopyFrom(1, 2, 3, 4), ByteString.CopyFrom(5, 6), ByteString.CopyFrom(new byte[1000]) },
+                RepeatedForeignMessage = { new ForeignMessage(), new ForeignMessage { C = 10 } },
+                RepeatedImportMessage = { new ImportMessage { D = 20 }, new ImportMessage { D = 25 } },
+                RepeatedNestedMessage = { new TestAllTypes.Types.NestedMessage { Bb = 35 }, new TestAllTypes.Types.NestedMessage { Bb = 10 } },
+                RepeatedPublicImportMessage = { new PublicImportMessage { E = 54 }, new PublicImportMessage { E = -1 } },
+                RepeatedString = { "foo", "bar" },
+                OneofString = "Oneof string",
+                RepeatedBool = { true, false },
+                RepeatedSfixed32 = { -123, 123 },
+                RepeatedSfixed64 = { -12345678901234, 12345678901234 },
+                RepeatedSint32 = { -456, 100 },
+                RepeatedSint64 = { -12345678901235, 123 },
+                RepeatedInt32 = { 100, 200 },
+                RepeatedInt64 = { 3210987654321, Int64.MaxValue },
+                RepeatedDouble = { -12.25, 23.5 },
+                RepeatedFixed32 = { UInt32.MaxValue, 23 },
+                RepeatedFixed64 = { UInt64.MaxValue, 1234567890123 },
+                RepeatedFloat = { 100f, 12.25f },
+                RepeatedUint32 = { UInt32.MaxValue, UInt32.MinValue },
+                RepeatedUint64 = { UInt64.MaxValue, UInt64.MinValue }
+            };
+        }
+
+        private void Assertions(MessageDescriptor desc, DynamicMessage res)
+        {
+            Assert.NotNull(res);
+            Assert.AreEqual("test", GetField(desc, res, "single_string"));
+            Assert.AreEqual(100, GetField(desc, res, "repeated_int32[0]"));
+            Assert.AreEqual(4294967295, GetField(desc, res, "repeated_fixed32[0]"));
+            Assert.AreEqual(10, GetField(desc, res, "single_foreign_message.c"));
+            Assert.AreEqual(20, GetField(desc, res, "single_import_message.d"));
+            Assert.AreEqual((int) TestAllTypes.Types.NestedEnum.Foo, GetField(desc, res, "single_nested_enum"));
+            Assert.AreEqual(54, GetField(desc, res, "single_public_import_message.e"));
+            Assert.AreEqual((int) ForeignEnum.ForeignBar, GetField(desc, res, "single_foreign_enum"));
+            Assert.AreEqual(ByteString.CopyFrom(1, 2, 3, 4), GetField(desc, res, "repeated_bytes[0]"));
+            Assert.AreEqual(ByteString.CopyFrom(5, 6), GetField(desc, res, "repeated_bytes[1]"));
+            Assert.AreEqual(10, GetField(desc, res, "repeated_foreign_message[1].c"));
+            Assert.AreEqual(null, GetField(desc, res, "repeated_foreign_message[0].c"));
+            Assert.AreEqual(10, GetField(desc, res, "repeated_foreign_message[1].c"));
+            Assert.AreEqual(20, GetField(desc, res, "repeated_import_message[0].d"));
+            Assert.AreEqual(25, GetField(desc, res, "repeated_import_message[1].d"));
+            Assert.AreEqual(35, GetField(desc, res, "repeated_nested_message[0].bb"));
+            Assert.AreEqual(10, GetField(desc, res, "repeated_nested_message[1].bb"));
+            Assert.AreEqual(54, GetField(desc, res, "repeated_public_import_message[0].e"));
+            Assert.AreEqual(-1, GetField(desc, res, "repeated_public_import_message[1].e"));
+            Assert.AreEqual("foo", GetField(desc, res, "repeated_string[0]"));
+            Assert.AreEqual("bar", GetField(desc, res, "repeated_string[1]"));
+            Assert.AreEqual("Oneof string", GetField(desc, res, "oneof_string"));
+            Assert.AreEqual(UInt64.MaxValue, GetField(desc, res, "repeated_fixed64[0]"));
+            Assert.AreEqual(1234567890123, GetField(desc, res, "repeated_fixed64[1]"));
+            Assert.AreEqual(100f, GetField(desc, res, "repeated_float[0]"));
+            Assert.AreEqual(12.25f, GetField(desc, res, "repeated_float[1]"));
+            Assert.AreEqual(UInt32.MaxValue, GetField(desc, res, "repeated_uint32[0]"));
+            Assert.AreEqual(UInt32.MinValue, GetField(desc, res, "repeated_uint32[1]"));
+            Assert.AreEqual(UInt64.MaxValue, GetField(desc, res, "repeated_uint64[0]"));
+            Assert.AreEqual(UInt64.MinValue, GetField(desc, res, "repeated_uint64[1]"));
+        }
+
+        private object GetField(MessageDescriptor desc, DynamicMessage dm, String fieldFullName)
+        {
+            if (!fieldFullName.Contains(".") && !fieldFullName.Contains("["))
+            {
+                return dm.GetField(desc.FindFieldByName(fieldFullName));
+            }
+            else if (fieldFullName.Contains("[") && !fieldFullName.Contains("."))
+            {
+                string fieldName = fieldFullName.Substring(0, fieldFullName.IndexOf("["));
+                int index = int.Parse(fieldFullName.Substring(fieldFullName.IndexOf("[") + 1, 1));
+                List<object> list = (List<object>) GetField(desc, dm, fieldName);
+                Assert.NotNull(list);
+                return list[index];
+            }
+            else
+            {
+                string[] fields = fieldFullName.Split('.');
+                DynamicMessage tempDm = dm;
+                MessageDescriptor tempDesc = desc;
+                for (int i = 0; i < fields.Length; i++)
+                {
+                    string field = fields[i];
+
+                    if (i == fields.Length - 1)
+                        return GetField(tempDesc, tempDm, field);
+                    else
+                    {
+                        tempDm = (DynamicMessage) GetField(tempDesc, tempDm, field);
+                        string tempField = field;
+                        if (field.Contains("["))
+                        {
+                            tempField = field.Substring(0, field.IndexOf("["));
+                        }
+                        tempDesc = tempDesc.FindFieldByName(tempField).MessageType;
+                    }
+
+                }
+            }
+            return null;
+        }
+
+    }
+}
---
+++ csharp/src/Google.Protobuf/Reflection/Dynamic/DynamicMessage.cs
@@ -0,0 +1,387 @@
+﻿using Google.Protobuf.Collections;
+using System;
+using System.Collections;
+
+namespace Google.Protobuf.Reflection.Dynamic
+{
+
+    /// <summary>
+    /// An implementation of IMessage that can represent arbitrary types, given a MessageaDescriptor.
+    /// Unknown fields not supported.
+    /// </summary>
+    public sealed class DynamicMessage : IMessage
+    {
+        private readonly MessageDescriptor type;
+        private readonly FieldSet fields;
+        private int memoizedSize = -1;
+
+        private DynamicMessage(MessageDescriptor type, FieldSet fields)
+        {
+            this.type = type;
+            this.fields = fields;
+        }
+
+        /// <summary>
+        /// Descriptor for this message.
+        /// </summary>
+        public MessageDescriptor Descriptor => type;
+
+        /// <summary>
+        /// Default implementation of Overridden method.
+        /// This specific implementation is never used, mergeFrom is used from the builder class.
+        /// MergeFrom functionality comes into picture when, we do desrialization from ByteString using static ParseFrom method.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        void IMessage.MergeFrom(CodedInputStream input)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Writes the message to CodedOutputStream.
+        /// </summary>
+        /// <param name="output"></param>
+        public void WriteTo(CodedOutputStream output)
+        {
+            fields.WriteTo(output);
+        }
+
+        /// <summary>
+        /// Used to calculate the size of the serialized message.
+        /// </summary>
+        /// <returns></returns>
+        public int CalculateSize()
+        {
+            int size = memoizedSize;
+            if (size != -1)
+            {
+                return size;
+            }
+            size = fields.GetSerializedSize();
+            memoizedSize = size;
+            return size;
+        }
+
+        /// <summary>
+        /// Parse data as a message of the given type and return it.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static DynamicMessage ParseFrom(MessageDescriptor type, ByteString data)
+        {
+            Builder builder = NewBuilder(type);
+            builder.MergeFrom(data);
+            return builder.BuildParsed();
+        }
+
+        /// <summary>
+        /// Construct a IMessage.Builder for the given type.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static Builder NewBuilder(MessageDescriptor type)
+        {
+            return new Builder(type);
+        }
+
+        /// <summary>
+        /// Returns the value of the field, represented by field descriptor
+        /// </summary>
+        /// <param name="fd"></param>
+        /// <returns></returns>
+        public object GetField(FieldDescriptor fd)
+        {
+            return fields.GetField(fd);
+        }
+
+        /// <summary>
+        /// Builder for dynamic messages. Instances are created with DynamicMessage.CreateBuilder..
+        /// </summary>
+        public sealed class Builder : IMessage
+        {
+
+            private readonly MessageDescriptor type;
+            private FieldSet fields;
+
+            internal Builder(MessageDescriptor type)
+            {
+                this.type = type;
+                this.fields = FieldSet.CreateInstance();
+            }
+
+            private bool IsInitialized
+            {
+                get { return fields.IsInitializedWithRespectTo(type.Fields.ByJsonName()); }
+            }
+
+            MessageDescriptor IMessage.Descriptor => throw new NotImplementedException();
+
+            /// <summary>
+            /// Creates the DynamicMessage
+            /// </summary>
+            /// <returns></returns>
+            /// <exception cref="Exception"></exception>
+            public DynamicMessage Build()
+            {
+                if (fields != null && !IsInitialized)
+                {
+                    throw new Exception($"Message of type {typeof(DynamicMessage)} is missing required fields");
+                }
+                return BuildPartial();
+            }
+
+            /// <summary>
+            /// Used to set a field
+            /// </summary>
+            /// <param name="fd"></param>
+            /// <param name="value"></param>
+            public void SetField(FieldDescriptor fd, object value)
+            {
+                fields.SetField(fd, value);
+            }
+
+            /// <summary>
+            /// Used to add repeated field
+            /// </summary>
+            /// <param name="fd"></param>
+            /// <param name="v"></param>
+            public void AddRepeatedField(FieldDescriptor fd, object v)
+            {
+                fields.AddRepeatedField(fd, v);
+            }
+
+            private DynamicMessage BuildPartial()
+            {
+                if (fields == null)
+                {
+                    throw new InvalidOperationException("Build() has already been called on this Builder.");
+                }
+                DynamicMessage result = new DynamicMessage(type, fields);
+                fields = null;
+                return result;
+            }
+
+            /// <summary>
+            /// Reads every field and sets it in the FieldSet instance
+            /// </summary>
+            /// <param name="input"></param>
+            /// <exception cref="Exception"></exception>
+            public void MergeFrom(CodedInputStream input)
+            {
+                uint tag;
+                while ((tag = input.ReadTag()) != 0)
+                {
+                    int fieldNumber = WireFormat.GetTagFieldNumber(tag);
+                    FieldDescriptor fd = type.FindFieldByNumber(fieldNumber);
+
+                    if (fd == null)
+                    {
+                        throw new Exception($"Field descriptor not found for fieldNumber: {fieldNumber}");
+                    }
+                    if (fd.FieldType == FieldType.Message)
+                    {
+                        MergeFromMessageTypeField(input, fd);
+                    }
+                    else
+                    {
+                        MergeFromPrimitiveTypeField(input, tag, fd);
+                    }
+                }
+            }
+
+            private void MergeFromPrimitiveTypeField(CodedInputStream input, uint tag, FieldDescriptor fd)
+            {
+                if (fd.ToProto().Label != FieldDescriptorProto.Types.Label.Repeated)
+                {
+                    object value = ReadField(fd.FieldType, input);
+                    fields.SetField(fd, value);
+                }
+                else
+                {
+                    IEnumerator enumerator = GetFieldCodec(tag, fd.FieldType, input);
+                    while (enumerator.MoveNext())
+                    {
+                        fields.AddRepeatedField(fd, enumerator.Current);
+                    }
+                }
+
+            }
+
+            private void MergeFromMessageTypeField(CodedInputStream input, FieldDescriptor fd)
+            {
+                Builder value = NewBuilder(fd.MessageType);
+                input.ReadMessage(value);
+                DynamicMessage val = value.Build();
+                if (fd.ToProto().Label != FieldDescriptorProto.Types.Label.Repeated)
+                {
+                    fields.SetField(fd, val);
+                }
+                else
+                {
+                    fields.AddRepeatedField(fd, val);
+                }
+            }
+
+            /// <summary>
+            /// Used for repeated fields to get enumerator.
+            /// Based on the fieldType, creates Repeated field, adds entries from CodedInputStrem to repeatedField and returns enumerator on the repeated field.
+            /// </summary>
+            /// <param name="tag"></param>
+            /// <param name="fieldType"></param>
+            /// <param name="input"></param>
+            /// <returns></returns>
+            private static IEnumerator GetFieldCodec(uint tag, FieldType fieldType, CodedInputStream input)
+            {
+                switch (fieldType)
+                {
+                    case FieldType.Int32:
+                        RepeatedField<int> rf = new RepeatedField<int>();
+                        rf.AddEntriesFrom(input, FieldCodec.ForInt32(tag));
+                        return rf.GetEnumerator();
+                    case FieldType.Int64:
+                        RepeatedField<long> rf1 = new RepeatedField<long>();
+                        rf1.AddEntriesFrom(input, FieldCodec.ForInt64(tag));
+                        return rf1.GetEnumerator();
+                    case FieldType.Bool:
+                        RepeatedField<bool> rfBool = new RepeatedField<bool>();
+                        rfBool.AddEntriesFrom(input, FieldCodec.ForBool(tag));
+                        return rfBool.GetEnumerator();
+                    case FieldType.UInt32:
+                        RepeatedField<uint> rfUint = new RepeatedField<uint>();
+                        rfUint.AddEntriesFrom(input, FieldCodec.ForUInt32(tag));
+                        return rfUint.GetEnumerator();
+                    case FieldType.UInt64:
+                        RepeatedField<ulong> rfUint64 = new RepeatedField<ulong>();
+                        rfUint64.AddEntriesFrom(input, FieldCodec.ForUInt64(tag));
+                        return rfUint64.GetEnumerator();
+                    case FieldType.SInt32:
+                        RepeatedField<int> rfSint32 = new RepeatedField<int>();
+                        rfSint32.AddEntriesFrom(input, FieldCodec.ForSInt32(tag));
+                        return rfSint32.GetEnumerator();
+                    case FieldType.SInt64:
+                        RepeatedField<long> rfSint64 = new RepeatedField<long>();
+                        rfSint64.AddEntriesFrom(input, FieldCodec.ForSInt64(tag));
+                        return rfSint64.GetEnumerator();
+                    case FieldType.Fixed32:
+                        RepeatedField<uint> rfFixed32 = new RepeatedField<uint>();
+                        rfFixed32.AddEntriesFrom(input, FieldCodec.ForFixed32(tag));
+                        return rfFixed32.GetEnumerator();
+                    case FieldType.Fixed64:
+                        RepeatedField<ulong> rfFixed64 = new RepeatedField<ulong>();
+                        rfFixed64.AddEntriesFrom(input, FieldCodec.ForFixed64(tag));
+                        return rfFixed64.GetEnumerator();
+                    case FieldType.SFixed32:
+                        RepeatedField<int> rfSFixed32 = new RepeatedField<int>();
+                        rfSFixed32.AddEntriesFrom(input, FieldCodec.ForSFixed32(tag));
+                        return rfSFixed32.GetEnumerator();
+                    case FieldType.SFixed64:
+                        RepeatedField<long> rfSFixed64 = new RepeatedField<long>();
+                        rfSFixed64.AddEntriesFrom(input, FieldCodec.ForSFixed64(tag));
+                        return rfSFixed64.GetEnumerator();
+                    case FieldType.Float:
+                        RepeatedField<float> rfFloat = new RepeatedField<float>();
+                        rfFloat.AddEntriesFrom(input, FieldCodec.ForFloat(tag));
+                        return rfFloat.GetEnumerator();
+                    case FieldType.Double:
+                        RepeatedField<double> rfDouble = new RepeatedField<double>();
+                        rfDouble.AddEntriesFrom(input, FieldCodec.ForDouble(tag));
+                        return rfDouble.GetEnumerator();
+                    case FieldType.String:
+                        RepeatedField<String> rfString = new RepeatedField<String>();
+                        rfString.AddEntriesFrom(input, FieldCodec.ForString(tag));
+                        return rfString.GetEnumerator();
+                    case FieldType.Bytes:
+                        RepeatedField<ByteString> rfBytes = new RepeatedField<ByteString>();
+                        rfBytes.AddEntriesFrom(input, FieldCodec.ForBytes(tag));
+                        return rfBytes.GetEnumerator();
+                }
+                throw new Exception("FieldType:" + fieldType + ", not handled in GetFieldCodec method.");
+            }
+
+            /// <summary>
+            /// Reads the field from the CodedInputStream, based on the fieldType and returns it.
+            /// </summary>
+            /// <param name="fieldType"></param>
+            /// <param name="input"></param>
+            /// <returns></returns>
+            private object ReadField(FieldType fieldType, CodedInputStream input)
+            {
+                switch (fieldType)
+                {
+                    case FieldType.Int32:
+                        return input.ReadInt32();
+                    case FieldType.Int64:
+                        return input.ReadInt64();
+                    case FieldType.Bytes:
+                        return input.ReadBytes();
+                    case FieldType.String:
+                        return input.ReadString();
+                    case FieldType.Double:
+                        return input.ReadDouble();
+                    case FieldType.Float:
+                        return input.ReadFloat();
+                    case FieldType.Bool:
+                        return input.ReadBool();
+                    case FieldType.UInt32:
+                        return input.ReadUInt32();
+                    case FieldType.UInt64:
+                        return input.ReadUInt64();
+                    case FieldType.Fixed32:
+                        return input.ReadFixed32();
+                    case FieldType.Fixed64:
+                        return input.ReadFixed64();
+                    case FieldType.SFixed64:
+                        return input.ReadSFixed64();
+                    case FieldType.SFixed32:
+                        return input.ReadSFixed32();
+                    case FieldType.SInt32:
+                        return input.ReadSInt32();
+                    case FieldType.SInt64:
+                        return input.ReadSInt64();
+                    case FieldType.Enum:
+                        return input.ReadEnum();
+                }
+                throw new Exception("FieldType:" + fieldType + ", not handled while reading field.");
+            }
+
+            /// <summary>
+            /// Marks the fields as immutable and returns a newly created DynsmicMessage.
+            /// </summary>
+            /// <returns></returns>
+            internal DynamicMessage BuildParsed()
+            {
+                DynamicMessage result = new DynamicMessage(type, fields);
+                return result;
+            }
+
+            /// <summary>
+            /// Default implementation of Overridden method.
+            /// This implementation is never used, writeTo is used from DynamicMessage class.
+            /// WriteTo functionality is used when we do serialization of DynamicMessage to generate ByteString.
+            /// </summary>
+            /// <param name="output"></param>
+            /// <exception cref="NotImplementedException"></exception>
+            void IMessage.WriteTo(CodedOutputStream output)
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <summary>
+            /// Default implementation of Overridden method.
+            /// This implementation is never used, CalculateSize is used from DynamicMessage class.
+            /// CalculateSize functionality is used when we do serialization of DynamicMessage to generate ByteString.
+            /// </summary>
+            /// <returns></returns>
+            /// <exception cref="NotImplementedException"></exception>
+            int IMessage.CalculateSize()
+            {
+                throw new NotImplementedException();
+            }
+
+        }
+
+    }
+
+}
