170b7dbaa032126a2f52dd3a083ba0dd9d94a8c6
Deanna Garcia <deannagarcia@google.com>
2022-11-14 14:24:24 -0800

    Add newline to kotlin generated code
    PiperOrigin-RevId: 488468270

--- src/google/protobuf/compiler/java/file.cc
+++ src/google/protobuf/compiler/java/file.cc
@@ -697,7 +697,7 @@ void FileGenerator::GenerateKotlin(io::Printer* printer) {
       "filename", file_->name());
   printer->Print(
       "// Generated files should ignore deprecation warnings\n"
-      "@file:Suppress(\"DEPRECATION\")");
+      "@file:Suppress(\"DEPRECATION\")\n");
   if (!java_package_.empty()) {
     printer->Print(
         "package $package$;\n"
@@ -734,7 +734,7 @@ void FileGenerator::GenerateKotlinSiblings(
         "filename", descriptor->file()->name());
     printer.Print(
         "// Generated files should ignore deprecation warnings\n"
-        "@file:Suppress(\"DEPRECATION\")");
+        "@file:Suppress(\"DEPRECATION\")\n");
     if (!java_package_.empty()) {
       printer.Print(
           "package $package$;\n"
