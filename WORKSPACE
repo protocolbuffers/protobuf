11992f4322491be2dafbed08071f7ffe1141eba8
Philippe Antoine <contact@catenacyber.fr>
2022-11-21 22:11:07 +0100

    Adds fuzz targets for oss-fuzz integration

--- WORKSPACE
+++ WORKSPACE
@@ -87,3 +87,20 @@ bind(
     name = "python_headers",
     actual = "@local_config_python//:python_headers",
 )
+
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
+http_archive(
+    name = "rules_fuzzing",
+    sha256 = "23bb074064c6f488d12044934ab1b0631e8e6898d5cf2f6bde087adb01111573",
+    strip_prefix = "rules_fuzzing-0.3.1",
+    urls = ["https://github.com/bazelbuild/rules_fuzzing/archive/v0.3.1.zip"],
+)
+
+load("@rules_fuzzing//fuzzing:repositories.bzl", "rules_fuzzing_dependencies")
+
+rules_fuzzing_dependencies()
+
+load("@rules_fuzzing//fuzzing:init.bzl", "rules_fuzzing_init")
+
+rules_fuzzing_init()
---
+++ src/google/protobuf/fuzz/fuzz_pb3.cc
@@ -0,0 +1,38 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "google/protobuf/test_messages_proto3.pb.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+    // google::protobuf::
+    protobuf_test_messages::proto3::TestAllTypesProto3 t;
+    std::string out;
+
+    if (t.ParseFromArray(data, size)) {
+        t.DebugString();
+        t.ShortDebugString();
+        t.Utf8DebugString();
+        t.SerializeToString(&out);
+        // #include "google/protobuf/text_format.h"
+        // google::protobuf::TextFormat::PrintToString(t, &out);
+    } else if (t.ParsePartialFromArray(data, size)) {
+        t.DebugString();
+        t.ShortDebugString();
+        t.Utf8DebugString();
+        t.SerializePartialToString(&out);
+    }
+    return 0;
+}
