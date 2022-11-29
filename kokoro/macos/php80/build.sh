8c52a2b2bea0d9e85ee4adbcc6f7a8f635bee70d
deannagarcia <69992229+deannagarcia@users.noreply.github.com>
2022-11-29 13:20:13 -0800

    Separate installs

--- kokoro/macos/php80/build.sh
+++ kokoro/macos/php80/build.sh
@@ -10,8 +10,9 @@ cd $(dirname $0)/../../..
 source kokoro/macos/prepare_build_macos_rc
 
 # Install Dependencies
-brew install --debug coreutils php@8.0
 brew cleanup
+brew install --debug coreutils
+brew install --debug php@8.0
 
 # Configure path
 PHP_FOLDER=$(find $HOMEBREW_PREFIX -type d -regex ".*php.*/8.0.[0-9_.]*" | sort -n | tail -n 1)
