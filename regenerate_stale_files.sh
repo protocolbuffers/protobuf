3b032eaabf115a3450f419ffda7db600e8ffe340
Adam Cozzette <acozzette@google.com>
2022-12-20 22:41:08 +0000

    Regenerate CMake file list before each test run

---
+++ regenerate_stale_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script runs the staleness tests and uses them to update any stale
+# generated files.
+
+set -ex
+
+# Cd to the repo root.
+cd $(dirname -- "$0")
+
+bazel test //src:cmake_lists_staleness_test || ./bazel-bin/src/cmake_lists_staleness_test --fix
+bazel test //src/google/protobuf:well_known_types_staleness_test || ./bazel-bin/src/google/protobuf/well_known_types_staleness_test --fix
