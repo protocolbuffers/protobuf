2fbb6bf77583458a73b3877215206cc7908f07e8
Mike Kruskal <mkruskal@google.com>
2023-01-25 12:03:30 -0800

    Remove Abseil patch in tests and bump our Bazel version to 20230125.rc3
    PiperOrigin-RevId: 504625323

--- kokoro/macos-next/cpp/build.sh
+++ kokoro/macos-next/cpp/build.sh
@@ -24,10 +24,6 @@ cd $(dirname $0)/../../..
 git submodule update --init --recursive
 ./regenerate_stale_files.sh
 
-# TODO(b/265232445) Remove this once the patch is upstreamed
-sed -i '' 's:random/mock_helpers.h:random/internal/mock_helpers.h:' third_party/abseil-cpp/CMake/AbseilDll.cmake
-sed -i '' 's:ABSL_INTERNA_TEST_DLL_TARGETS:ABSL_INTERNAL_TEST_DLL_TARGETS:' third_party/abseil-cpp/CMake/AbseilDll.cmake
-
 #
 # Run build
 #
