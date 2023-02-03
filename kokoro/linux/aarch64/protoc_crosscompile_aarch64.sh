40acb318c0bcde32ef1abba56f1daefc7763ba65
Mike Kruskal <mkruskal@google.com>
2023-02-03 11:39:31 -0800

    Delete remaining cross-architecture Kokoro tests in favor of GHA.
    These have already been migrated, this just deleted dead code
    
    PiperOrigin-RevId: 506955344

--- kokoro/linux/aarch64/protoc_crosscompile_aarch64.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Builds protobuf C++ with aarch64 crosscompiler.
-
-set -ex
-
-cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON -Dprotobuf_WITH_ZLIB=0 -Dprotobuf_BUILD_TESTS=OFF .
-make -j8
