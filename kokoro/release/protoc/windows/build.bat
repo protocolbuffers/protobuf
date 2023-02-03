94687c0cbae128afc3fdbd04c4e043065e56ca4d
Mike Kruskal <mkruskal@google.com>
2023-02-03 14:33:07 -0800

    Delete remaining dead code from kokoro directory
    PiperOrigin-RevId: 506996158

--- kokoro/release/protoc/windows/build.bat
+++ kokoro/release/protoc/windows/build.bat
@@ -7,7 +7,39 @@ set configuration=Release
 echo Building protoc
 cd github\protobuf
 
-call kokoro\windows\prepare_build_win64.bat || goto :error
+@rem Update Chocolatey
+choco upgrade -y --no-progress chocolatey
+choco install -y python --version 3.10.0
+choco install -y --no-progress --pre cmake
+
+@rem Enable long paths.
+Powershell.exe -Command "New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem -Name LongPathsEnabled -Value 1 -PropertyType DWORD -Force"
+
+@rem Update git submodules.
+git submodule update --init --recursive
+
+@rem Select Visual Studio 2017.
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+@rem Convert Windows line breaks to Unix line breaks
+@rem This allows text-matching tests to pass
+@find . -type f -print0 | xargs -0 d2u
+
+@rem Use python3
+C:\python310\python.exe -m venv venv
+call venv\Scripts\activate.bat
+
+@rem Allow Bazel to create short paths.
+fsutil 8dot3name set 0
+
+@rem Reinstall Bazel due to corrupt installation in kokoro.
+bazel version
+choco install bazel -y -i --version 5.1.0
+bazel version
+
+@rem Regenerate stale files.
+bazel test //src:cmake_lists_staleness_test || call bazel-bin\src\cmake_lists_staleness_test.exe --fix
+bazel test //src/google/protobuf:well_known_types_staleness_test || call bazel-bin\src\google\protobuf\well_known_types_staleness_test.exe --fix
 
 set ABSL_ROOT_DIR=%cd%\third_party\abseil-cpp
 
