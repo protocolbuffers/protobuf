f1e746c491c2d1166233b0a7561f1b089d07345a
Mike Kruskal <mkruskal@google.com>
2023-02-02 17:42:30 -0800

    Migrate remaining Windows tests to GHA
    PiperOrigin-RevId: 506766851

--- regenerate_stale_files.sh
+++ regenerate_stale_files.sh
@@ -8,15 +8,17 @@ set -ex
 # Cd to the repo root.
 cd $(dirname -- "$0")
 
+readonly BazelBin="${BAZEL:-bazel} ${BAZEL_STARTUP_FLAGS}"
+
 # Run and fix all staleness tests.
 # LINT.IfChange(staleness_tests)
-bazel test //src:cmake_lists_staleness_test "$@" || ./bazel-bin/src/cmake_lists_staleness_test --fix
-bazel test //src/google/protobuf:well_known_types_staleness_test "$@" || ./bazel-bin/src/google/protobuf/well_known_types_staleness_test --fix
+${BazelBin} test src:cmake_lists_staleness_test "$@" || ./bazel-bin/src/cmake_lists_staleness_test --fix
+${BazelBin} test src/google/protobuf:well_known_types_staleness_test "$@" || ./bazel-bin/src/google/protobuf/well_known_types_staleness_test --fix
 # LINT.ThenChange(//depot/google3/third_party/protobuf/github/kokoro/windows/prepare_build_win64.bat:staleness_tests)
 
 # Generate C# code.
 # This doesn't currently have Bazel staleness tests, but there's an existing
 # shell script that generates everything required. The output files are stable,
 # so just regenerating in place should be harmless. 
-bazel build src/google/protobuf/compiler:protoc "$@"
+${BazelBin} build src/google/protobuf/compiler:protoc "$@"
 (export PROTOC=$PWD/bazel-bin/protoc && cd csharp && ./generate_protos.sh)
