4a2f566ff0293b456f18de0f55ada46dc1491e1a
Mike Kruskal <mkruskal@google.com>
2023-03-07 11:01:17 -0800

    Expand google.protobuf.Type handling to cover editions.
    PiperOrigin-RevId: 514783686

--- src/google/protobuf/util/type_resolver_util.h
+++ src/google/protobuf/util/type_resolver_util.h
@@ -33,7 +33,9 @@
 #ifndef GOOGLE_PROTOBUF_UTIL_TYPE_RESOLVER_UTIL_H__
 #define GOOGLE_PROTOBUF_UTIL_TYPE_RESOLVER_UTIL_H__
 
+#include "google/protobuf/type.pb.h"
 #include "absl/strings/string_view.h"
+#include "google/protobuf/descriptor.h"
 
 // Must be included last.
 #include "google/protobuf/port_def.inc"
@@ -48,6 +50,14 @@ class TypeResolver;
 // pool. Caller takes ownership of the returned TypeResolver.
 PROTOBUF_EXPORT TypeResolver* NewTypeResolverForDescriptorPool(
     absl::string_view url_prefix, const DescriptorPool* pool);
+
+// Performs a direct conversion from a descriptor to a type proto.
+PROTOBUF_EXPORT google::protobuf::Type ConvertDescriptorToType(
+    absl::string_view url_prefix, const Descriptor& descriptor);
+
+// Performs a direct conversion from an enum descriptor to a type proto.
+PROTOBUF_EXPORT google::protobuf::Enum ConvertDescriptorToType(
+    const EnumDescriptor& descriptor);
 
 }  // namespace util
 }  // namespace protobuf
