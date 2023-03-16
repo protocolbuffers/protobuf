b81c127e94d953aa9be5c9f00a1ad323552bae0e
Adam Cozzette <acozzette@google.com>
2023-03-16 13:28:05 -0700

    Undo the external contributions we have accepted to the well-known types
    For legal reasons we unfortunately cannot accept any external contributions to
    the well-known type protos, so this change reverts the small number of changes
    we have received so far.
    
    PiperOrigin-RevId: 517211904

--- objectivec/GPBTimestamp.pbobjc.h
+++ objectivec/GPBTimestamp.pbobjc.h
@@ -132,7 +132,7 @@ typedef GPB_ENUM(GPBTimestamp_FieldNumber) {
  * [`strftime`](https://docs.python.org/2/library/time.html#time.strftime) with
  * the time format spec '%Y-%m-%dT%H:%M:%S.%fZ'. Likewise, in Java, one can use
  * the Joda Time's [`ISODateTimeFormat.dateTime()`](
- * http://www.joda.org/joda-time/apidocs/org/joda/time/format/ISODateTimeFormat.html#dateTime%2D%2D
+ * http://joda-time.sourceforge.net/apidocs/org/joda/time/format/ISODateTimeFormat.html#dateTime()
  * ) to obtain a formatter capable of generating timestamps in this format.
  **/
 GPB_FINAL @interface GPBTimestamp : GPBMessage
