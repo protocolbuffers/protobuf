2645f5ee5ec62aab0d0d22fb1c08184898bc5b90
Adam Cozzette <acozzette@google.com>
2023-04-05 16:45:17 -0700

    Automated rollback of commit e664fff44ea5e966a85aa077f786bb03976e8a77.
    PiperOrigin-RevId: 522188474

--- src/google/protobuf/descriptor.cc
+++ src/google/protobuf/descriptor.cc
@@ -1867,7 +1867,6 @@ DescriptorPool::DescriptorPool()
       lazily_build_dependencies_(false),
       allow_unknown_(false),
       enforce_weak_(false),
-      enforce_special_extension_ranges_(true),
       disallow_enforce_utf8_(false),
       deprecated_legacy_json_field_conflicts_(false) {}
 
@@ -1882,7 +1881,6 @@ DescriptorPool::DescriptorPool(DescriptorDatabase* fallback_database,
       lazily_build_dependencies_(false),
       allow_unknown_(false),
       enforce_weak_(false),
-      enforce_special_extension_ranges_(true),
       disallow_enforce_utf8_(false),
       deprecated_legacy_json_field_conflicts_(false) {}
 
@@ -1896,7 +1894,6 @@ DescriptorPool::DescriptorPool(const DescriptorPool* underlay)
       lazily_build_dependencies_(false),
       allow_unknown_(false),
       enforce_weak_(false),
-      enforce_special_extension_ranges_(true),
       disallow_enforce_utf8_(false),
       deprecated_legacy_json_field_conflicts_(false) {}
 
