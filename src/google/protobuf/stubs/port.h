cfcc72912d652731dd49a51a72d7de2925c3eedc
William Lee <tao.william.li@aliyun.com>
2023-04-19 16:06:59 +0800

    Fix build for newlib

--- src/google/protobuf/stubs/port.h
+++ src/google/protobuf/stubs/port.h
@@ -59,7 +59,7 @@
 #pragma runtime_checks("c", off)
 #endif
 #else
-#ifdef __APPLE__
+#if (defined(__APPLE__) || defined(_NEWLIB__))
 #include <machine/endian.h>  // __BYTE_ORDER
 #elif defined(__FreeBSD__)
 #include <sys/endian.h>  // __BYTE_ORDER
