7f0092a8021466009e65367aed68f2a1867da880
Jie Luo <jieluo@google.com>
2023-05-23 11:15:54 -0700

    Internal change
    PiperOrigin-RevId: 534490554

--- python/pb_unit_tests/descriptor_pool_test_wrapper.py
+++ python/pb_unit_tests/descriptor_pool_test_wrapper.py
@@ -24,11 +24,10 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import unittest
+from google.protobuf.internal import api_implementation
 from google.protobuf.internal.descriptor_pool_test import *
 
-# This is testing that certain methods unconditionally throw TypeError.
-# In the new extension we simply don't define them at all.
-AddDescriptorTest.testAddTypeError.__unittest_expecting_failure__ = True
+AddDescriptorTest.testAddTypeError.__unittest_skip__ = True
 
 SecondaryDescriptorFromDescriptorDB.testErrorCollector.__unittest_expecting_failure__ = True
 
