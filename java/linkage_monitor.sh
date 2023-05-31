16cdc329714cadc2380d704fc34e51ce49bc1404
Tomo Suzuki <suztomo@google.com>
2023-05-31 00:09:05 -0400

    Tell Maven the protoc location

--- java/linkage_monitor.sh
+++ java/linkage_monitor.sh
@@ -16,7 +16,7 @@ echo "Maven command: $(which mvn)"
 
 echo "protoc command:$(which protoc); $(protoc --version)"
 
-mvn --projects "bom,core,util" -e -B -Dhttps.protocols=TLSv1.2 install -Dmaven.test.skip=true -Dprotoc=protoc
+mvn --projects "bom,core,util" -e -B -Dhttps.protocols=TLSv1.2 install -Dmaven.test.skip=true -Dprotoc="${RUNFILES_DIR}/com_google_protobuf/protoc"
 
 curl -v -O "https://storage.googleapis.com/cloud-opensource-java-linkage-monitor/linkage-monitor-latest-all-deps.jar"
 
