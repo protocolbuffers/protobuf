0d9e7e448aaaa534664990cb5f8df03f7afb8e0f
Tomo Suzuki <suztomo@google.com>
2023-06-02 00:27:11 -0400

    protoc location validation

--- java/linkage_monitor.sh
+++ java/linkage_monitor.sh
@@ -14,13 +14,17 @@ ls -alt core/
 
 echo "Maven command: $(which mvn)"
 
-echo "protoc command:$(which protoc); $(protoc --version)"
+protoc_location="${RUNFILES_DIR}/com_google_protobuf/protoc"
+if [ ! -x "${protoc_location}" ]; then
+  echo "${protoc_location} is not found or not executable"
+  exit 1
+fi
 
 mvn --projects "bom,core,util" -e -B -Dhttps.protocols=TLSv1.2 install \
     -Dmaven.test.skip=true \
     -Dprotobuf.basedir="../" \
-    -Dprotoc="${RUNFILES_DIR}/com_google_protobuf/protoc" \
+    -Dprotoc="${protoc_location}"
 
 curl -v -O "https://storage.googleapis.com/cloud-opensource-java-linkage-monitor/linkage-monitor-latest-all-deps.jar"
 
-java -Xmx2048m -jar linkage-monitor-latest-all-deps.jar com.google.cloud:google-cloud-shared-dependencies
+java -Xmx2048m -jar linkage-monitor-latest-all-deps.jar com.google.cloud:first-party-dependencies
