3b5b751a338a2777c9398374a3753770a1aaba7d
Matt Kulukundis <matt.fowles@gmail.com>
2023-07-07 21:04:36 -0400

    integers may not have finalizers so store strings instead

--- ruby/tests/object_cache_test.rb
+++ ruby/tests/object_cache_test.rb
@@ -13,8 +13,8 @@ class ObjectCacheTest < Test::Unit::TestCase
   def test_try_add_returns_existing_value
     cache = Google::Protobuf::OBJECT_CACHE.class.new
     key = 0xdeadbeefdeadbeef
-    first_value = 42
-    second_value = 43
+    first_value = "42"
+    second_value = "43"
     assert_same first_value, cache.try_add(key, first_value)
     assert_same first_value, cache.get(key)
     assert_same first_value, cache.try_add(key, second_value)
@@ -28,7 +28,7 @@ class ObjectCacheTest < Test::Unit::TestCase
 
     100.times do |i|
       threads[i] = Thread.new {
-        Thread.current["result"] = cache.try_add(key, i)
+        Thread.current["result"] = cache.try_add(key, i.to_s)
       }
     end
 
