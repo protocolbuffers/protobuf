19f6d4bb1f1d2dd3fbb9912c4f56356a0d1105e6
Matt Kulukundis <matt.fowles@gmail.com>
2023-07-07 23:00:46 -0400

    Improve coverage by testing Legacy on modern Ruby

--- ruby/tests/object_cache_test.rb
+++ ruby/tests/object_cache_test.rb
@@ -1,21 +1,23 @@
 require 'google/protobuf'
 require 'test/unit'
 
-if RUBY_PLATFORM == "java"
-  return
-end
-
-class ObjectCacheTest < Test::Unit::TestCase
+class PlatformTest < Test::Unit::TestCase
   def test_correct_implementation_for_platform
+    if RUBY_PLATFORM == "java"
+      return
+    end
+
     if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.7.0') and Google::Protobuf::SIZEOF_LONG >= Google::Protobuf::SIZEOF_VALUE
       assert_instance_of Google::Protobuf::ObjectCache, Google::Protobuf::OBJECT_CACHE
     else
       assert_instance_of Google::Protobuf::LegacyObjectCache, Google::Protobuf::OBJECT_CACHE
     end
   end
+end
 
+module ObjectCacheTestModule
   def test_try_add_returns_existing_value
-    cache = Google::Protobuf::OBJECT_CACHE.class.new
+    cache = self.create
 
     keys = ["k1", "k2"]
     vals = ["v1", "v2"]
@@ -28,7 +30,7 @@ class ObjectCacheTest < Test::Unit::TestCase
   end
 
   def test_multithreaded_access
-    cache = Google::Protobuf::OBJECT_CACHE.class.new
+    cache = self.create
 
     keys = ["k0", "k1", "k2", "k3", "k4", "k5", "k6", "k7"]
 
@@ -45,3 +47,20 @@ class ObjectCacheTest < Test::Unit::TestCase
     assert_equal 8, results.values.uniq.size
   end
 end
+
+class ObjectCacheTest < Test::Unit::TestCase
+  def create
+    Google::Protobuf::ObjectCache.new
+  end
+
+  include ObjectCacheTestModule
+end
+
+class LegacyObjectCacheTest < Test::Unit::TestCase
+  def create
+    Google::Protobuf::LegacyObjectCache.new
+  end
+
+  include ObjectCacheTestModule
+end
+
