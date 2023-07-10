fc034d57fa15c36e878560304f94b1b45a4d661a
Hong Shin <hongshin@google.com>
2023-07-10 14:26:26 -0700

    Add V0 support for signed ints
    PiperOrigin-RevId: 546983452

--- rust/test/shared/accessors_test.rs
+++ rust/test/shared/accessors_test.rs
@@ -33,11 +33,11 @@ use unittest_proto::proto2_unittest::TestAllTypes;
 
 #[test]
 fn test_default_accessors() {
-    // defaults are from here:
-    // http://google3/third_party/protobuf/unittest.proto section default_*
     let msg = TestAllTypes::new();
     assert_eq!(msg.default_int32(), 41);
     assert_eq!(msg.default_int64(), 42);
+    assert_eq!(msg.default_sint32(), -45);
+    assert_eq!(msg.default_sint64(), 46);
     assert_eq!(msg.default_uint32(), 43);
     assert_eq!(msg.default_uint64(), 44);
     assert_eq!(msg.default_bool(), true);
@@ -72,6 +72,36 @@ fn test_optional_int64_accessors() {
     msg.optional_int64_set(None);
     assert_eq!(msg.optional_int64_opt(), None);
     assert_eq!(msg.optional_int64(), 0);
+}
+
+#[test]
+fn test_optional_sint32_accessors() {
+    let mut msg = TestAllTypes::new();
+    assert_eq!(msg.optional_sint32_opt(), None);
+    assert_eq!(msg.optional_sint32(), 0);
+
+    msg.optional_sint32_set(Some(-22));
+    assert_eq!(msg.optional_sint32_opt(), Some(-22));
+    assert_eq!(msg.optional_sint32(), -22);
+
+    msg.optional_sint32_set(None);
+    assert_eq!(msg.optional_sint32_opt(), None);
+    assert_eq!(msg.optional_sint32(), 0);
+}
+
+#[test]
+fn test_optional_sint64_accessors() {
+    let mut msg = TestAllTypes::new();
+    assert_eq!(msg.optional_sint64_opt(), None);
+    assert_eq!(msg.optional_sint64(), 0);
+
+    msg.optional_sint64_set(Some(7000));
+    assert_eq!(msg.optional_sint64_opt(), Some(7000));
+    assert_eq!(msg.optional_sint64(), 7000);
+
+    msg.optional_sint64_set(None);
+    assert_eq!(msg.optional_sint64_opt(), None);
+    assert_eq!(msg.optional_sint64(), 0);
 }
 
 #[test]
