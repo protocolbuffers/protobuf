f9a5c32d785248cef81525ffa564cdf0a562503f
Mike Kruskal <mkruskal@google.com>
2023-07-26 11:38:20 -0700

    Introduce C++ feature for UTF8 validation.
    This will replace the frontend string_field_validation field, which we've discovered can't accurately model all of the unique behaviors in each language.
    
    PiperOrigin-RevId: 551269189

--- src/google/protobuf/cpp_features.pb.cc
+++ src/google/protobuf/cpp_features.pb.cc
@@ -27,6 +27,7 @@ PROTOBUF_CONSTEXPR CppFeatures::CppFeatures(::_pbi::ConstantInitialized)
       /*decltype(_impl_._has_bits_)*/ {},
       /*decltype(_impl_._cached_size_)*/ {},
       /*decltype(_impl_.legacy_closed_enum_)*/ false,
+      /*decltype(_impl_.utf8_validation_)*/ 0,
     } {}
 struct CppFeaturesDefaultTypeInternal {
   PROTOBUF_CONSTEXPR CppFeaturesDefaultTypeInternal() : _instance(::_pbi::ConstantInitialized{}) {}
@@ -40,8 +41,7 @@ PROTOBUF_ATTRIBUTE_NO_DESTROY PROTOBUF_CONSTINIT PROTOBUF_EXPORT
     PROTOBUF_ATTRIBUTE_INIT_PRIORITY1 CppFeaturesDefaultTypeInternal _CppFeatures_default_instance_;
 }  // namespace pb
 static ::_pb::Metadata file_level_metadata_google_2fprotobuf_2fcpp_5ffeatures_2eproto[1];
-static constexpr const ::_pb::EnumDescriptor**
-    file_level_enum_descriptors_google_2fprotobuf_2fcpp_5ffeatures_2eproto = nullptr;
+static const ::_pb::EnumDescriptor* file_level_enum_descriptors_google_2fprotobuf_2fcpp_5ffeatures_2eproto[1];
 static constexpr const ::_pb::ServiceDescriptor**
     file_level_service_descriptors_google_2fprotobuf_2fcpp_5ffeatures_2eproto = nullptr;
 const ::uint32_t TableStruct_google_2fprotobuf_2fcpp_5ffeatures_2eproto::offsets[] PROTOBUF_SECTION_VARIABLE(
@@ -55,12 +55,14 @@ const ::uint32_t TableStruct_google_2fprotobuf_2fcpp_5ffeatures_2eproto::offsets
     ~0u,  // no _split_
     ~0u,  // no sizeof(Split)
     PROTOBUF_FIELD_OFFSET(::pb::CppFeatures, _impl_.legacy_closed_enum_),
+    PROTOBUF_FIELD_OFFSET(::pb::CppFeatures, _impl_.utf8_validation_),
     0,
+    1,
 };
 
 static const ::_pbi::MigrationSchema
     schemas[] PROTOBUF_SECTION_VARIABLE(protodesc_cold) = {
-        {0, 9, -1, sizeof(::pb::CppFeatures)},
+        {0, 10, -1, sizeof(::pb::CppFeatures)},
 };
 
 static const ::_pb::Message* const file_default_instances[] = {
@@ -68,11 +70,15 @@ static const ::_pb::Message* const file_default_instances[] = {
 };
 const char descriptor_table_protodef_google_2fprotobuf_2fcpp_5ffeatures_2eproto[] PROTOBUF_SECTION_VARIABLE(protodesc_cold) = {
     "\n\"google/protobuf/cpp_features.proto\022\002pb"
-    "\032 google/protobuf/descriptor.proto\"D\n\013Cp"
-    "pFeatures\0225\n\022legacy_closed_enum\030\001 \001(\010B\031\210"
-    "\001\001\230\001\004\230\001\001\242\001\r\n\0042023\022\005false::\n\003cpp\022\033.google"
-    ".protobuf.FeatureSet\030\350\007 \001(\0132\017.pb.CppFeat"
-    "ures"
+    "\032 google/protobuf/descriptor.proto\"\373\001\n\013C"
+    "ppFeatures\0225\n\022legacy_closed_enum\030\001 \001(\010B\031"
+    "\210\001\001\230\001\004\230\001\001\242\001\r\n\0042023\022\005false\022Y\n\017utf8_valida"
+    "tion\030\002 \001(\0162\036.pb.CppFeatures.Utf8Validati"
+    "onB \210\001\001\230\001\004\230\001\001\242\001\024\n\0042023\022\014VERIFY_PARSE\"Z\n\016"
+    "Utf8Validation\022\033\n\027UTF8_VALIDATION_UNKNOW"
+    "N\020\000\022\020\n\014VERIFY_PARSE\020\001\022\017\n\013VERIFY_DLOG\020\002\022\010"
+    "\n\004NONE\020\003::\n\003cpp\022\033.google.protobuf.Featur"
+    "eSet\030\350\007 \001(\0132\017.pb.CppFeatures"
 };
 static const ::_pbi::DescriptorTable* const descriptor_table_google_2fprotobuf_2fcpp_5ffeatures_2eproto_deps[1] =
     {
@@ -82,7 +88,7 @@ static ::absl::once_flag descriptor_table_google_2fprotobuf_2fcpp_5ffeatures_2ep
 const ::_pbi::DescriptorTable descriptor_table_google_2fprotobuf_2fcpp_5ffeatures_2eproto = {
     false,
     false,
-    204,
+    388,
     descriptor_table_protodef_google_2fprotobuf_2fcpp_5ffeatures_2eproto,
     "google/protobuf/cpp_features.proto",
     &descriptor_table_google_2fprotobuf_2fcpp_5ffeatures_2eproto_once,
@@ -112,6 +118,34 @@ PROTOBUF_ATTRIBUTE_WEAK const ::_pbi::DescriptorTable* descriptor_table_google_2
   return &descriptor_table_google_2fprotobuf_2fcpp_5ffeatures_2eproto;
 }
 namespace pb {
+const ::google::protobuf::EnumDescriptor* CppFeatures_Utf8Validation_descriptor() {
+  ::google::protobuf::internal::AssignDescriptors(&descriptor_table_google_2fprotobuf_2fcpp_5ffeatures_2eproto);
+  return file_level_enum_descriptors_google_2fprotobuf_2fcpp_5ffeatures_2eproto[0];
+}
+bool CppFeatures_Utf8Validation_IsValid(int value) {
+  switch (value) {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+      return true;
+    default:
+      return false;
+  }
+}
+#if (__cplusplus < 201703) && \
+  (!defined(_MSC_VER) || (_MSC_VER >= 1900 && _MSC_VER < 1912))
+
+constexpr CppFeatures_Utf8Validation CppFeatures::UTF8_VALIDATION_UNKNOWN;
+constexpr CppFeatures_Utf8Validation CppFeatures::VERIFY_PARSE;
+constexpr CppFeatures_Utf8Validation CppFeatures::VERIFY_DLOG;
+constexpr CppFeatures_Utf8Validation CppFeatures::NONE;
+constexpr CppFeatures_Utf8Validation CppFeatures::Utf8Validation_MIN;
+constexpr CppFeatures_Utf8Validation CppFeatures::Utf8Validation_MAX;
+constexpr int CppFeatures::Utf8Validation_ARRAYSIZE;
+
+#endif  // (__cplusplus < 201703) &&
+        // (!defined(_MSC_VER) || (_MSC_VER >= 1900 && _MSC_VER < 1912))
 // ===================================================================
 
 class CppFeatures::_Internal {
@@ -121,6 +155,9 @@ class CppFeatures::_Internal {
     8 * PROTOBUF_FIELD_OFFSET(CppFeatures, _impl_._has_bits_);
   static void set_has_legacy_closed_enum(HasBits* has_bits) {
     (*has_bits)[0] |= 1u;
+  }
+  static void set_has_utf8_validation(HasBits* has_bits) {
+    (*has_bits)[0] |= 2u;
   }
 };
 
@@ -141,6 +178,7 @@ inline void CppFeatures::SharedCtor(::_pb::Arena* arena) {
       decltype(_impl_._has_bits_){},
       /*decltype(_impl_._cached_size_)*/ {},
       decltype(_impl_.legacy_closed_enum_){false},
+      decltype(_impl_.utf8_validation_){0},
   };
 }
 CppFeatures::~CppFeatures() {
@@ -161,7 +199,12 @@ PROTOBUF_NOINLINE void CppFeatures::Clear() {
   // Prevent compiler warnings about cached_has_bits being unused
   (void) cached_has_bits;
 
-  _impl_.legacy_closed_enum_ = false;
+  cached_has_bits = _impl_._has_bits_[0];
+  if (cached_has_bits & 0x00000003u) {
+    ::memset(&_impl_.legacy_closed_enum_, 0, static_cast<::size_t>(
+        reinterpret_cast<char*>(&_impl_.utf8_validation_) -
+        reinterpret_cast<char*>(&_impl_.legacy_closed_enum_)) + sizeof(_impl_.utf8_validation_));
+  }
   _impl_._has_bits_.Clear();
   _internal_metadata_.Clear<::google::protobuf::UnknownFieldSet>();
 }
@@ -174,20 +217,23 @@ const char* CppFeatures::_InternalParse(
 
 
 PROTOBUF_CONSTINIT PROTOBUF_ATTRIBUTE_INIT_PRIORITY1
-const ::_pbi::TcParseTable<0, 1, 0, 0, 2> CppFeatures::_table_ = {
+const ::_pbi::TcParseTable<1, 2, 1, 0, 2> CppFeatures::_table_ = {
   {
     PROTOBUF_FIELD_OFFSET(CppFeatures, _impl_._has_bits_),
     0, // no _extensions_
-    1, 0,  // max_field_number, fast_idx_mask
+    2, 8,  // max_field_number, fast_idx_mask
     offsetof(decltype(_table_), field_lookup_table),
-    4294967294,  // skipmap
+    4294967292,  // skipmap
     offsetof(decltype(_table_), field_entries),
-    1,  // num_field_entries
-    0,  // num_aux_entries
-    offsetof(decltype(_table_), field_names),  // no aux_entries
+    2,  // num_field_entries
+    1,  // num_aux_entries
+    offsetof(decltype(_table_), aux_entries),
     &_CppFeatures_default_instance_._instance,
     ::_pbi::TcParser::GenericFallback,  // fallback
   }, {{
+    // optional .pb.CppFeatures.Utf8Validation utf8_validation = 2 [retention = RETENTION_RUNTIME, targets = TARGET_TYPE_FIELD, targets = TARGET_TYPE_FILE, edition_defaults = {
+    {::_pbi::TcParser::FastEr0S1,
+     {16, 1, 3, PROTOBUF_FIELD_OFFSET(CppFeatures, _impl_.utf8_validation_)}},
     // optional bool legacy_closed_enum = 1 [retention = RETENTION_RUNTIME, targets = TARGET_TYPE_FIELD, targets = TARGET_TYPE_FILE, edition_defaults = {
     {::_pbi::TcParser::SingularVarintNoZag1<bool, offsetof(CppFeatures, _impl_.legacy_closed_enum_), 0>(),
      {8, 0, 0, PROTOBUF_FIELD_OFFSET(CppFeatures, _impl_.legacy_closed_enum_)}},
@@ -197,9 +243,12 @@ const ::_pbi::TcParseTable<0, 1, 0, 0, 2> CppFeatures::_table_ = {
     // optional bool legacy_closed_enum = 1 [retention = RETENTION_RUNTIME, targets = TARGET_TYPE_FIELD, targets = TARGET_TYPE_FILE, edition_defaults = {
     {PROTOBUF_FIELD_OFFSET(CppFeatures, _impl_.legacy_closed_enum_), _Internal::kHasBitsOffset + 0, 0,
     (0 | ::_fl::kFcOptional | ::_fl::kBool)},
-  }},
-  // no aux_entries
-  {{
+    // optional .pb.CppFeatures.Utf8Validation utf8_validation = 2 [retention = RETENTION_RUNTIME, targets = TARGET_TYPE_FIELD, targets = TARGET_TYPE_FILE, edition_defaults = {
+    {PROTOBUF_FIELD_OFFSET(CppFeatures, _impl_.utf8_validation_), _Internal::kHasBitsOffset + 1, 0,
+    (0 | ::_fl::kFcOptional | ::_fl::kEnumRange)},
+  }}, {{
+    {0, 4},
+  }}, {{
   }},
 };
 
@@ -216,6 +265,13 @@ const ::_pbi::TcParseTable<0, 1, 0, 0, 2> CppFeatures::_table_ = {
     target = stream->EnsureSpace(target);
     target = ::_pbi::WireFormatLite::WriteBoolToArray(
         1, this->_internal_legacy_closed_enum(), target);
+  }
+
+  // optional .pb.CppFeatures.Utf8Validation utf8_validation = 2 [retention = RETENTION_RUNTIME, targets = TARGET_TYPE_FIELD, targets = TARGET_TYPE_FILE, edition_defaults = {
+  if (cached_has_bits & 0x00000002u) {
+    target = stream->EnsureSpace(target);
+    target = ::_pbi::WireFormatLite::WriteEnumToArray(
+        2, this->_internal_utf8_validation(), target);
   }
 
   if (PROTOBUF_PREDICT_FALSE(_internal_metadata_.have_unknown_fields())) {
@@ -235,12 +291,20 @@ const ::_pbi::TcParseTable<0, 1, 0, 0, 2> CppFeatures::_table_ = {
   // Prevent compiler warnings about cached_has_bits being unused
   (void) cached_has_bits;
 
-  // optional bool legacy_closed_enum = 1 [retention = RETENTION_RUNTIME, targets = TARGET_TYPE_FIELD, targets = TARGET_TYPE_FILE, edition_defaults = {
   cached_has_bits = _impl_._has_bits_[0];
-  if (cached_has_bits & 0x00000001u) {
-    total_size += 2;
-  }
+  if (cached_has_bits & 0x00000003u) {
+    // optional bool legacy_closed_enum = 1 [retention = RETENTION_RUNTIME, targets = TARGET_TYPE_FIELD, targets = TARGET_TYPE_FILE, edition_defaults = {
+    if (cached_has_bits & 0x00000001u) {
+      total_size += 2;
+    }
 
+    // optional .pb.CppFeatures.Utf8Validation utf8_validation = 2 [retention = RETENTION_RUNTIME, targets = TARGET_TYPE_FIELD, targets = TARGET_TYPE_FILE, edition_defaults = {
+    if (cached_has_bits & 0x00000002u) {
+      total_size += 1 +
+                    ::_pbi::WireFormatLite::EnumSize(this->_internal_utf8_validation());
+    }
+
+  }
   return MaybeComputeUnknownFieldsSize(total_size, &_impl_._cached_size_);
 }
 
@@ -259,8 +323,15 @@ void CppFeatures::MergeImpl(::google::protobuf::Message& to_msg, const ::google:
   ::uint32_t cached_has_bits = 0;
   (void) cached_has_bits;
 
-  if ((from._impl_._has_bits_[0] & 0x00000001u) != 0) {
-    _this->_internal_set_legacy_closed_enum(from._internal_legacy_closed_enum());
+  cached_has_bits = from._impl_._has_bits_[0];
+  if (cached_has_bits & 0x00000003u) {
+    if (cached_has_bits & 0x00000001u) {
+      _this->_impl_.legacy_closed_enum_ = from._impl_.legacy_closed_enum_;
+    }
+    if (cached_has_bits & 0x00000002u) {
+      _this->_impl_.utf8_validation_ = from._impl_.utf8_validation_;
+    }
+    _this->_impl_._has_bits_[0] |= cached_has_bits;
   }
   _this->_internal_metadata_.MergeFrom<::google::protobuf::UnknownFieldSet>(from._internal_metadata_);
 }
@@ -280,7 +351,12 @@ void CppFeatures::InternalSwap(CppFeatures* other) {
   using std::swap;
   _internal_metadata_.InternalSwap(&other->_internal_metadata_);
   swap(_impl_._has_bits_[0], other->_impl_._has_bits_[0]);
-        swap(_impl_.legacy_closed_enum_, other->_impl_.legacy_closed_enum_);
+  ::google::protobuf::internal::memswap<
+      PROTOBUF_FIELD_OFFSET(CppFeatures, _impl_.utf8_validation_)
+      + sizeof(CppFeatures::_impl_.utf8_validation_)
+      - PROTOBUF_FIELD_OFFSET(CppFeatures, _impl_.legacy_closed_enum_)>(
+          reinterpret_cast<char*>(&_impl_.legacy_closed_enum_),
+          reinterpret_cast<char*>(&other->_impl_.legacy_closed_enum_));
 }
 
 ::google::protobuf::Metadata CppFeatures::GetMetadata() const {
