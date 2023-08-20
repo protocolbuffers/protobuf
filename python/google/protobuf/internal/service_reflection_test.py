791b69f552d7ea0fc19a770902b14e766458a5d4
Rin Arakaki <rnarkkx@gmail.com>
2023-08-20 20:38:19 +0900

    Update service_reflection_test.py

--- python/google/protobuf/internal/service_reflection_test.py
+++ python/google/protobuf/internal/service_reflection_test.py
@@ -33,107 +33,98 @@
 __author__ = 'petar@google.com (Petar Petrov)'
 
 
-import unittest
-
 from google.protobuf import service_reflection
 from google.protobuf import service
 from google.protobuf import unittest_pb2
 
 
-class FooUnitTest(unittest.TestCase):
+class TestFooUnit:
+    def test_service(self):
+        class MockRpcChannel(service.RpcChannel):
+            def CallMethod(self, method, controller, request, response, callback):
+                self.method = method
+                self.controller = controller
+                self.request = request
+                callback(response)
 
-  def testService(self):
-    class MockRpcChannel(service.RpcChannel):
-      def CallMethod(self, method, controller, request, response, callback):
-        self.method = method
-        self.controller = controller
-        self.request = request
-        callback(response)
+        class MockRpcController(service.RpcController):
+            def SetFailed(self, msg):
+                self.failure_message = msg
 
-    class MockRpcController(service.RpcController):
-      def SetFailed(self, msg):
-        self.failure_message = msg
+        self.callback_response = None
 
-    self.callback_response = None
+        class MyService(unittest_pb2.TestService):
+            pass
 
-    class MyService(unittest_pb2.TestService):
-      pass
+        self.callback_response = None
 
-    self.callback_response = None
+        def MyCallback(response):
+            self.callback_response = response
 
-    def MyCallback(response):
-      self.callback_response = response
+        rpc_controller = MockRpcController()
+        channel = MockRpcChannel()
+        srvc = MyService()
+        srvc.Foo(rpc_controller, unittest_pb2.FooRequest(), MyCallback)
+        assert 'Method Foo not implemented.' == rpc_controller.failure_message
+        assert self.callback_response is None
 
-    rpc_controller = MockRpcController()
-    channel = MockRpcChannel()
-    srvc = MyService()
-    srvc.Foo(rpc_controller, unittest_pb2.FooRequest(), MyCallback)
-    self.assertEqual('Method Foo not implemented.',
-                     rpc_controller.failure_message)
-    self.assertEqual(None, self.callback_response)
+        rpc_controller.failure_message = None
 
-    rpc_controller.failure_message = None
+        service_descriptor = unittest_pb2.TestService.GetDescriptor()
+        srvc.CallMethod(service_descriptor.methods[1], rpc_controller,
+                        unittest_pb2.BarRequest(), MyCallback)
+        assert (srvc.GetRequestClass(service_descriptor.methods[1])
+                is unittest_pb2.BarRequest)
+        assert (srvc.GetResponseClass(service_descriptor.methods[1])
+                is unittest_pb2.BarResponse)
+        assert 'Method Bar not implemented.' == rpc_controller.failure_message
+        assert self.callback_response is None
 
-    service_descriptor = unittest_pb2.TestService.GetDescriptor()
-    srvc.CallMethod(service_descriptor.methods[1], rpc_controller,
-                    unittest_pb2.BarRequest(), MyCallback)
-    self.assertTrue(srvc.GetRequestClass(service_descriptor.methods[1]) is
-                    unittest_pb2.BarRequest)
-    self.assertTrue(srvc.GetResponseClass(service_descriptor.methods[1]) is
-                    unittest_pb2.BarResponse)
-    self.assertEqual('Method Bar not implemented.',
-                     rpc_controller.failure_message)
-    self.assertEqual(None, self.callback_response)
+        class MyServiceImpl(unittest_pb2.TestService):
+            def Foo(self, rpc_controller, request, done):
+                self.foo_called = True
+            def Bar(self, rpc_controller, request, done):
+                self.bar_called = True
 
-    class MyServiceImpl(unittest_pb2.TestService):
-      def Foo(self, rpc_controller, request, done):
-        self.foo_called = True
-      def Bar(self, rpc_controller, request, done):
-        self.bar_called = True
+        srvc = MyServiceImpl()
+        rpc_controller.failure_message = None
+        srvc.Foo(rpc_controller, unittest_pb2.FooRequest(), MyCallback)
+        assert rpc_controller.failure_message is None
+        assert srvc.foo_called is True
 
-    srvc = MyServiceImpl()
-    rpc_controller.failure_message = None
-    srvc.Foo(rpc_controller, unittest_pb2.FooRequest(), MyCallback)
-    self.assertEqual(None, rpc_controller.failure_message)
-    self.assertEqual(True, srvc.foo_called)
+        rpc_controller.failure_message = None
+        srvc.CallMethod(service_descriptor.methods[1], rpc_controller,
+                        unittest_pb2.BarRequest(), MyCallback)
+        assert rpc_controller.failure_message is None
+        assert srvc.bar_called is True
 
-    rpc_controller.failure_message = None
-    srvc.CallMethod(service_descriptor.methods[1], rpc_controller,
-                    unittest_pb2.BarRequest(), MyCallback)
-    self.assertEqual(None, rpc_controller.failure_message)
-    self.assertEqual(True, srvc.bar_called)
+    def test_service_stub(self):
+        class MockRpcChannel(service.RpcChannel):
+            def CallMethod(self, method, controller, request,
+                          response_class, callback):
+                self.method = method
+                self.controller = controller
+                self.request = request
+                callback(response_class())
 
-  def testServiceStub(self):
-    class MockRpcChannel(service.RpcChannel):
-      def CallMethod(self, method, controller, request,
-                     response_class, callback):
-        self.method = method
-        self.controller = controller
-        self.request = request
-        callback(response_class())
+        self.callback_response = None
 
-    self.callback_response = None
+        def MyCallback(response):
+            self.callback_response = response
 
-    def MyCallback(response):
-      self.callback_response = response
+        channel = MockRpcChannel()
+        stub = unittest_pb2.TestService_Stub(channel)
+        rpc_controller = 'controller'
+        request = 'request'
 
-    channel = MockRpcChannel()
-    stub = unittest_pb2.TestService_Stub(channel)
-    rpc_controller = 'controller'
-    request = 'request'
+        # GetDescriptor now static, still works as instance method for compatibility
+        assert(unittest_pb2.TestService_Stub.GetDescriptor()
+              == stub.GetDescriptor())
 
-    # GetDescriptor now static, still works as instance method for compatibility
-    self.assertEqual(unittest_pb2.TestService_Stub.GetDescriptor(),
-                     stub.GetDescriptor())
+        # Invoke method.
+        stub.Foo(rpc_controller, request, MyCallback)
 
-    # Invoke method.
-    stub.Foo(rpc_controller, request, MyCallback)
-
-    self.assertIsInstance(self.callback_response, unittest_pb2.FooResponse)
-    self.assertEqual(request, channel.request)
-    self.assertEqual(rpc_controller, channel.controller)
-    self.assertEqual(stub.GetDescriptor().methods[0], channel.method)
-
-
-if __name__ == '__main__':
-  unittest.main()
+        assert isinstance(self.callback_response, unittest_pb2.FooResponse)
+        assert request == channel.request
+        assert rpc_controller == channel.controller
+        assert stub.GetDescriptor().methods[0] == channel.method
