c1c4a1fcdb77ed969ed9f04722c2572f15f1e88d
Mike Kruskal <mkruskal@google.com>
2023-09-07 10:44:54 -0700

    Stop setting edition strings in FeatureSets.
    PiperOrigin-RevId: 563479246

--- upb/upb/reflection/file_def.h
+++ upb/upb/reflection/file_def.h
@@ -48,7 +48,7 @@ bool upb_FileDef_HasOptions(const upb_FileDef* f);
 UPB_API const char* upb_FileDef_Name(const upb_FileDef* f);
 const UPB_DESC(FileOptions) * upb_FileDef_Options(const upb_FileDef* f);
 const char* upb_FileDef_Package(const upb_FileDef* f);
-const char* upb_FileDef_Edition(const upb_FileDef* f);
+UPB_DESC(Edition) upb_FileDef_Edition(const upb_FileDef* f);
 UPB_API const upb_DefPool* upb_FileDef_Pool(const upb_FileDef* f);
 
 const upb_FileDef* upb_FileDef_PublicDependency(const upb_FileDef* f, int i);
