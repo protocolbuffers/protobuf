bc0a87423e65f646c0bd3c016114e098159b69b0
Jie Luo <jieluo@google.com>
2023-09-14 10:31:40 -0700

    Use PyImport_ImportModuleLevel() instead of PyImport_Import() for descriptor_pb2. PyImport_ImportModuleLevel() custom globals and locals pointing to some namespace we control, not normal-looking __builtins__
    Change PYUPB_DESCRIPTOR_MODULE to "google.protobuf.descriptor_pb2" because protobuf python is using google.protobuf.descriptor_pb2 everywhere
    
    Also check import result of PYUPB_DESCRIPTOR_MODULE
    
    PiperOrigin-RevId: 565409795

--- upb/python/protobuf.h
+++ upb/python/protobuf.h
@@ -41,7 +41,6 @@
 #define PYUPB_PROTOBUF_PUBLIC_PACKAGE "google.protobuf"
 #define PYUPB_PROTOBUF_INTERNAL_PACKAGE "google.protobuf.internal"
 #define PYUPB_DESCRIPTOR_PROTO_PACKAGE "google.protobuf"
-#define PYUPB_DESCRIPTOR_MODULE "google.protobuf.descriptor_pb2"
 #define PYUPB_MODULE_NAME "google._upb._message"
 // end:github_only
 
@@ -49,10 +48,10 @@
 // #define PYUPB_PROTOBUF_PUBLIC_PACKAGE "google3.net.proto2.python.public"
 // #define PYUPB_PROTOBUF_INTERNAL_PACKAGE "google3.net.proto2.python.internal"
 // #define PYUPB_DESCRIPTOR_PROTO_PACKAGE "proto2"
-// #define PYUPB_DESCRIPTOR_MODULE "google3.net.proto2.proto.descriptor_pb2"
 // #define PYUPB_MODULE_NAME "google3.third_party.upb.python._message"
 // end:google_only
 
+#define PYUPB_DESCRIPTOR_MODULE "google.protobuf.descriptor_pb2"
 #define PYUPB_RETURN_OOM return PyErr_SetNone(PyExc_MemoryError), NULL
 
 struct PyUpb_WeakMap;
