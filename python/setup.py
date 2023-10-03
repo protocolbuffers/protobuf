545f01c5e52e023919509130e96816b37c317435
Adam Cozzette <acozzette@google.com>
2023-10-03 14:22:40 -0700

    Add support for Python 3.12
    PiperOrigin-RevId: 570496193

--- python/setup.py
+++ python/setup.py
@@ -432,6 +432,7 @@ if __name__ == '__main__':
           'Programming Language :: Python :: 3.9',
           'Programming Language :: Python :: 3.10',
           'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
       ],
       namespace_packages=['google'],
       packages=find_packages(
