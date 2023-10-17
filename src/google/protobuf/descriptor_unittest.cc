8e6fc5f8aa8581b4924bee657c13c2c7c019dd19
Mike Kruskal <mkruskal@google.com>
2023-10-17 16:07:01 -0700

    Ensure that the range of valid editions is always dense.
    PiperOrigin-RevId: 574292659

--- src/google/protobuf/descriptor_unittest.cc
+++ src/google/protobuf/descriptor_unittest.cc
@@ -7217,6 +7217,13 @@ TEST_F(ValidationErrorTest, UnusedImportWithOtherError) {
       "foo.proto: Foo.foo: EXTENDEE: \"Baz\" is not defined.\n");
 }
 
+TEST(EditionsTest, DenseRange) {
+  for (int i = static_cast<int>(PROTOBUF_MINIMUM_EDITION);
+       i <= static_cast<int>(PROTOBUF_MAXIMUM_EDITION); ++i) {
+    EXPECT_TRUE(Edition_IsValid(i));
+  }
+}
+
 using FeaturesBaseTest = ValidationErrorTest;
 
 class FeaturesTest : public FeaturesBaseTest {
