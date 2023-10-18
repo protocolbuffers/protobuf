8ff15497f301dec312d4f714078674b91e5f18f3
Mike Kruskal <mkruskal@google.com>
2023-10-18 13:32:07 -0700

    Change the behavior of --experimental_editions.
    Previously, this gated all editions access and then further checks were applied.  This removes those checks so that we can incrementally roll out editions support to individual languages.  Anyone using the --experimental_editions flag should be careful not to use it on generators that don't support editions.  As we launch editions support for individual generators, this flag won't be required to use them.
    
    PiperOrigin-RevId: 574577631

--- src/google/protobuf/compiler/code_generator.cc
+++ src/google/protobuf/compiler/code_generator.cc
@@ -23,6 +23,9 @@
 #include "google/protobuf/descriptor.h"
 #include "google/protobuf/feature_resolver.h"
 
+// Must be included last.
+#include "google/protobuf/port_def.inc"
+
 namespace google {
 namespace protobuf {
 namespace compiler {
@@ -57,6 +60,14 @@ bool CodeGenerator::GenerateAll(const std::vector<const FileDescriptor*>& files,
 
 absl::StatusOr<FeatureSetDefaults> CodeGenerator::BuildFeatureSetDefaults()
     const {
+  if ((GetSupportedFeatures() & FEATURE_SUPPORTS_EDITIONS) == 0) {
+    // For generators that don't fully support editions yet, provide an
+    // optimistic set of defaults.  Protoc will check this condition later
+    // anyway.
+    return FeatureResolver::CompileDefaults(
+        FeatureSet::descriptor(), GetFeatureExtensions(),
+        PROTOBUF_MINIMUM_EDITION, PROTOBUF_MAXIMUM_EDITION);
+  }
   return FeatureResolver::CompileDefaults(
       FeatureSet::descriptor(), GetFeatureExtensions(), GetMinimumEdition(),
       GetMaximumEdition());
@@ -131,3 +142,5 @@ bool IsKnownFeatureProto(absl::string_view filename) {
 }  // namespace compiler
 }  // namespace protobuf
 }  // namespace google
+
+#include "google/protobuf/port_undef.inc"
