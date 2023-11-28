ffbb5fa3e578b031e03addf1dc5bf8499b9f0657
David Chamberlin <david.chamberlin@gs.com>
2023-11-28 16:15:30 +0000

    [Build] Address review comments

--- upb_generator/protoc-gen-upb.cc
+++ upb_generator/protoc-gen-upb.cc
@@ -890,16 +890,16 @@ void WriteHeader(const DefPoolPair& pools, upb::FileDefPtr file,
 
   if (options.bootstrap) {
     for (auto message : this_file_messages) {
-      output("extern const $1upb_MiniTable* $0();\n", MessageInitName(message),
-             PadSuffix(options.dllexport_decl));
+      output("extern const$1 upb_MiniTable* $0();\n", MessageInitName(message),
+             PadPrefix(options.dllexport_decl));
     }
     for (auto message : forward_messages) {
-      output("extern const $1upb_MiniTable* $0();\n", MessageInitName(message),
-             PadSuffix(options.dllexport_decl));
+      output("extern const$1 upb_MiniTable* $0();\n", MessageInitName(message),
+             PadPrefix(options.dllexport_decl));
     }
     for (auto enumdesc : this_file_enums) {
-      output("extern const $1upb_MiniTableEnum* $0();\n", EnumInit(enumdesc),
-             PadSuffix(options.dllexport_decl));
+      output("extern const$1 upb_MiniTableEnum* $0();\n", EnumInit(enumdesc),
+             PadPrefix(options.dllexport_decl));
     }
     output("\n");
   }
