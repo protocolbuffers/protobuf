1054eb3a372212644bd1898c7a3619f029830a34
swurl <swurl@swurl.xyz>
2024-01-20 10:35:58 -0500

    [build] cmake: change lite, headers, export tgt names
    To work with CANDLE/WIX/MSI
    
    Signed-off-by: swurl <swurl@swurl.xyz>

--- cmake/libprotobuf-lite.cmake
+++ cmake/libprotobuf-lite.cmake
@@ -3,43 +3,43 @@
 include(${protobuf_SOURCE_DIR}/src/file_lists.cmake)
 include(${protobuf_SOURCE_DIR}/cmake/protobuf-configure-target.cmake)
 
-add_library(libprotobuf-lite ${protobuf_SHARED_OR_STATIC}
+add_library(libprotobuf_lite ${protobuf_SHARED_OR_STATIC}
   ${libprotobuf_lite_srcs}
   ${libprotobuf_lite_hdrs}
   ${protobuf_version_rc_file})
 if(protobuf_HAVE_LD_VERSION_SCRIPT)
   if(${CMAKE_VERSION} VERSION_GREATER 3.13 OR ${CMAKE_VERSION} VERSION_EQUAL 3.13)
-    target_link_options(libprotobuf-lite PRIVATE -Wl,--version-script=${protobuf_SOURCE_DIR}/src/libprotobuf-lite.map)
+    target_link_options(libprotobuf_lite PRIVATE -Wl,--version-script=${protobuf_SOURCE_DIR}/src/libprotobuf-lite.map)
   elseif(protobuf_BUILD_SHARED_LIBS)
-    target_link_libraries(libprotobuf-lite PRIVATE -Wl,--version-script=${protobuf_SOURCE_DIR}/src/libprotobuf-lite.map)
+    target_link_libraries(libprotobuf_lite PRIVATE -Wl,--version-script=${protobuf_SOURCE_DIR}/src/libprotobuf-lite.map)
   endif()
-  set_target_properties(libprotobuf-lite PROPERTIES
+  set_target_properties(libprotobuf_lite PROPERTIES
     LINK_DEPENDS ${protobuf_SOURCE_DIR}/src/libprotobuf-lite.map)
 endif()
-target_link_libraries(libprotobuf-lite PRIVATE ${CMAKE_THREAD_LIBS_INIT})
+target_link_libraries(libprotobuf_lite PRIVATE ${CMAKE_THREAD_LIBS_INIT})
 if(protobuf_LINK_LIBATOMIC)
-  target_link_libraries(libprotobuf-lite PRIVATE atomic)
+  target_link_libraries(libprotobuf_lite PRIVATE atomic)
 endif()
 if(${CMAKE_SYSTEM_NAME} STREQUAL "Android")
-  target_link_libraries(libprotobuf-lite PRIVATE log)
+  target_link_libraries(libprotobuf_lite PRIVATE log)
 endif()
-target_include_directories(libprotobuf-lite PUBLIC ${protobuf_SOURCE_DIR}/src)
-target_link_libraries(libprotobuf-lite PUBLIC ${protobuf_ABSL_USED_TARGETS})
-protobuf_configure_target(libprotobuf-lite)
+target_include_directories(libprotobuf_lite PUBLIC ${protobuf_SOURCE_DIR}/src)
+target_link_libraries(libprotobuf_lite PUBLIC ${protobuf_ABSL_USED_TARGETS})
+protobuf_configure_target(libprotobuf_lite)
 if(protobuf_BUILD_SHARED_LIBS)
-  target_compile_definitions(libprotobuf-lite
+  target_compile_definitions(libprotobuf_lite
     PUBLIC  PROTOBUF_USE_DLLS
     PRIVATE LIBPROTOBUF_EXPORTS)
 endif()
-set_target_properties(libprotobuf-lite PROPERTIES
+set_target_properties(libprotobuf_lite PROPERTIES
     VERSION ${protobuf_VERSION}
-    OUTPUT_NAME ${LIB_PREFIX}protobuf-lite
+    OUTPUT_NAME ${LIB_PREFIX}protobuf_lite
     DEBUG_POSTFIX "${protobuf_DEBUG_POSTFIX}"
     # For -fvisibility=hidden and -fvisibility-inlines-hidden
     C_VISIBILITY_PRESET hidden
     CXX_VISIBILITY_PRESET hidden
     VISIBILITY_INLINES_HIDDEN ON
 )
-add_library(protobuf::libprotobuf-lite ALIAS libprotobuf-lite)
+add_library(protobuf::libprotobuf-lite ALIAS libprotobuf_lite)
 
-target_link_libraries(libprotobuf-lite PRIVATE utf8_validity)
+target_link_libraries(libprotobuf_lite PRIVATE utf8_validity)
