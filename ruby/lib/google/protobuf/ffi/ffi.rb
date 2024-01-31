096644977d798d7886921c81b0fa3a69646cede3
Joshua Haberman <haberman@google.com>
2024-01-31 13:03:10 -0800

    Increased the size of the error message buffer by 4x, to 511 bytes.
    The previous limit of 127 was causing error messages to get truncated when long identifiers are in use.
    
    This will increase the stack usage of any code that is allocating a `upb_Status` on the stack. If we want to support smaller stacks in the future, we could change `upb_Status` to store its capacity inline, so different users can allocate error buffers with different capacity.
    
    Fixes: https://github.com/protocolbuffers/protobuf/issues/14376
    PiperOrigin-RevId: 603136385

--- ruby/lib/google/protobuf/ffi/ffi.rb
+++ ruby/lib/google/protobuf/ffi/ffi.rb
@@ -21,7 +21,7 @@ module Google
       Upb_Map_Begin = -1
 
       ## Encoding Status
-      Upb_Status_MaxMessage = 127
+      Upb_Status_MaxMessage = 511
       Upb_Encode_Deterministic = 1
       Upb_Encode_SkipUnknown = 2
 
