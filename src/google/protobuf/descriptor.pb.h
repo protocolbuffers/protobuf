4295b956053a15dfd0e508df2e8e4c3386631b91
Mike Kruskal <mkruskal@google.com>
2024-04-02 11:11:37 -0700

    Internal change
    PiperOrigin-RevId: 621236613

--- src/google/protobuf/descriptor.pb.h
+++ src/google/protobuf/descriptor.pb.h
@@ -665,7 +665,6 @@ inline bool GeneratedCodeInfo_Annotation_Semantic_Parse(absl::string_view name, 
 }
 enum Edition : int {
   EDITION_UNKNOWN = 0,
-  EDITION_LEGACY = 900,
   EDITION_PROTO2 = 998,
   EDITION_PROTO3 = 999,
   EDITION_2023 = 1000,
