a1f534c6e5fe38f871231106d2d741321f809046
Jie Luo <jieluo@google.com>
2024-05-16 22:42:05 -0700

    Nextgen Proto Pythonic API:  “Add-on” proto for length prefixed serialize/parse
    Added the following methods:
    serialize_length_prefixed(message: Message, output) -> None
    parse_length_prefixed(message_class: Type[Message], input_bytes: bytes) -> Message
    
    The output of serialize_length_prefixed should be ByteIO or custom buffered IO that data should be written to. The output stream must be buffered, e.g. using
      https://docs.python.org/3/library/io.html#buffered-streams.
    
    PiperOrigin-RevId: 634637782

--- python/google/protobuf/internal/proto_test.py
+++ python/google/protobuf/internal/proto_test.py
@@ -8,12 +8,14 @@
 
 """Tests Nextgen Pythonic protobuf APIs."""
 
+import io
 import unittest
 
 from google.protobuf import proto
-
+from google.protobuf.internal import encoder
 from google.protobuf.internal import test_util
 from google.protobuf.internal import testing_refleaks
+
 from google.protobuf.internal import _parameterized
 from google.protobuf import unittest_pb2
 from google.protobuf import unittest_proto3_arena_pb2
@@ -23,12 +25,86 @@ from google.protobuf import unittest_proto3_arena_pb2
 @testing_refleaks.TestCase
 class ProtoTest(unittest.TestCase):
 
-  def testSerializeParse(self, message_module):
+  def test_serialize_parse(self, message_module):
     msg = message_module.TestAllTypes()
     test_util.SetAllFields(msg)
     serialized_data = proto.serialize(msg)
     parsed_msg = proto.parse(message_module.TestAllTypes, serialized_data)
     self.assertEqual(msg, parsed_msg)
+
+  def test_serialize_parse_length_prefixed(self, message_module):
+    number_of_messages = 10
+
+    out = io.BytesIO()
+    for index in range(0, number_of_messages):
+      msg = message_module.TestAllTypes(
+          optional_int32=index, optional_string='hello'
+      )
+      proto.serialize_length_prefixed(msg, out)
+
+    input_bytes = io.BytesIO(out.getvalue())
+    index = 0
+    while True:
+      msg = proto.parse_length_prefixed(
+          message_module.TestAllTypes, input_bytes
+      )
+      if msg is None:
+        break
+      self.assertEqual(msg.optional_int32, index)  # pytype: disable=attribute-error
+      self.assertEqual(msg.optional_string, 'hello')  # pytype: disable=attribute-error
+      index += 1
+
+    self.assertEqual(index, number_of_messages)
+
+  def test_serialize_parse_length_prefixed_empty(self, message_module):
+    number_of_messages = 10
+
+    empty_alltypes = message_module.TestAllTypes()
+    out = io.BytesIO()
+    for index in range(0, number_of_messages):
+      proto.serialize_length_prefixed(empty_alltypes, out)
+
+    input_bytes = io.BytesIO(out.getvalue())
+    index = 0
+    while True:
+      msg = proto.parse_length_prefixed(
+          message_module.TestAllTypes, input_bytes
+      )
+      if msg is None:
+        break
+      self.assertEqual(msg, empty_alltypes)
+      index += 1
+
+    self.assertEqual(index, number_of_messages)
+
+  def test_serialize_parse_length_prefixed_bytes(self, message_module):
+    out = io.BytesIO()
+    msg = message_module.TestAllTypes(
+        optional_int32=123, optional_string='hello'
+    )
+    proto.serialize_length_prefixed(msg, out)
+    input_bytes = out.getvalue()
+    parsed_msg = proto.parse_length_prefixed(
+        message_module.TestAllTypes, input_bytes
+    )
+    self.assertEqual(msg, parsed_msg)
+
+  def test_parse_length_prefixed_truncated(self, message_module):
+    out = io.BytesIO()
+    encoder._VarintEncoder()(out.write, 9999)
+    msg = message_module.TestAllTypes(optional_int32=1)
+    out.write(proto.serialize(msg))
+
+    input_bytes = io.BytesIO(out.getvalue())
+    with self.assertRaises(ValueError) as context:
+      proto.parse_length_prefixed(message_module.TestAllTypes, input_bytes)
+      self.assertEqual(
+          str(context.exception),
+          'Truncated message or non-buffered input_bytes: '
+          'Expected 9999 bytes but only 2 bytes parsed for '
+          'TestAllTypes.',
+      )
+
 
 if __name__ == '__main__':
   unittest.main()
--- python/google/protobuf/proto.py
+++ python/google/protobuf/proto.py
@@ -7,8 +7,11 @@
 
 """Contains the Nextgen Pythonic protobuf APIs."""
 
-import typing
+import io
+from typing import Type, Union
 
+from google.protobuf.internal import decoder
+from google.protobuf.internal import encoder
 from google.protobuf.message import Message
 
 def serialize(message: Message, deterministic: bool=None) -> bytes:
@@ -24,7 +27,8 @@ def serialize(message: Message, deterministic: bool=None) -> bytes:
   """
   return message.SerializeToString(deterministic=deterministic)
 
-def parse(message_class: typing.Type[Message], payload: bytes) -> Message:
+
+def parse(message_class: Type[Message], payload: bytes) -> Message:
   """Given a serialized data in binary form, deserialize it into a Message.
 
   Args:
@@ -37,3 +41,96 @@ def parse(message_class: typing.Type[Message], payload: bytes) -> Message:
   new_message = message_class()
   new_message.ParseFromString(payload)
   return new_message
+
+
+def serialize_length_prefixed(message: Message, output: io.BytesIO) -> None:
+  """Writes the size of the message as a varint and the serialized message.
+
+  Writes the size of the message as a varint and then the serialized message.
+  This allows more data to be written to the output after the message. Use
+  parse_length_prefixed to parse messages written by this method.
+
+  The output stream must be buffered, e.g. using
+  https://docs.python.org/3/library/io.html#buffered-streams.
+
+  Args:
+    message: The protocol buffer message that should be serialized.
+    output: BytesIO or custom buffered IO that data should be written to.
+  """
+  size = message.ByteSize()
+  encoder._VarintEncoder()(output.write, size)
+  out_size = output.write(serialize(message))
+  if out_size is None:
+    raise TypeError('Output stream does not extend io.RawIOBase')
+  if out_size != size:
+    raise TypeError(
+        'Failed to write complete message (wrote: %d, expected: %d)'
+        '. Ensure output is using buffered IO.' % (out_size, size)
+    )
+
+
+def parse_length_prefixed(
+    message_class: Type[Message], input_bytes: Union[io.BytesIO, bytes]
+) -> Message:
+  """Parse a message from input_bytes.
+
+  Args:
+    message_class: The protocol buffer message class that parser should parse.
+    input_bytes: A buffered input.
+
+  Returns:
+    A parsed message if successful. None if input_bytes is at EOF.
+  """
+
+  def _read_varint(input_bytes):
+    """Read a varint from input_bytes.
+
+    Args:
+      input_bytes: A buffered input.
+
+    Returns:
+      The varint value or None if we read EOF
+    """
+    size = 0
+    shift = 0
+    mask = (1 << 64) - 1
+
+    while 1:
+      try:
+        b = input_bytes.read(1)[0]
+      except IndexError as e:
+        if shift == 0:
+          return None
+        else:
+          raise ValueError('Fail to read message size %s' % str(e))
+      size |= (b & 0x7F) << shift
+      if not b & 0x80:
+        size &= mask
+        break
+      shift += 7
+      if shift >= 64:
+        raise ValueError(
+            'Fail to read message size: Too many bytes when decoding varint.'
+        )
+    return size
+
+  buffer = (
+      io.BytesIO(input_bytes) if isinstance(input_bytes, bytes) else input_bytes
+  )
+  size = _read_varint(buffer)
+  if size is None:
+    return None
+
+  if size == 0:
+    message = message_class()
+    return message
+
+  message = parse(message_class, buffer.read(size))
+  parsed_size = message.ByteSize()
+  if parsed_size != size:
+    raise ValueError(
+        'Truncated message or non-buffered input_bytes: '
+        'Expected {0} bytes but only {1} bytes parsed for '
+        '{2}.'.format(size, parsed_size, message.DESCRIPTOR.name)
+    )
+  return message
