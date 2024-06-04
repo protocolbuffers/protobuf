b57c37414631f467b353926f1b54bd6ead16052f
Hong Shin <hongshin@google.com>
2024-06-04 10:24:56 -0700

    protos: Collapse ClearMessage template
    We introduce Ptr removal so that our helper functions can be agnostically passed in a Ptr<T> or a T*.
    
    We utilize SFINAE to determine if PtrOrRaw. If the target is const, or not in {T, T*, Ptr<T>}, the template will not match and be discarded (but still compile successfully).
    
    PiperOrigin-RevId: 640198851

--- protos/protos.h
+++ protos/protos.h
@@ -242,6 +242,26 @@ absl::Status SetExtension(upb_Message* message, upb_Arena* message_arena,
                           const upb_MiniTableExtension* ext,
                           const upb_Message* extension);
 
+template <typename T>
+struct RemovePtr;
+
+template <typename T>
+struct RemovePtr<Ptr<T>> {
+  using type = T;
+};
+
+template <typename T>
+struct RemovePtr<T*> {
+  using type = T;
+};
+
+template <typename T>
+using RemovePtrT = typename RemovePtr<T>::type;
+
+template <typename T, typename U = RemovePtrT<T>,
+          typename = std::enable_if_t<!std::is_const_v<U>>>
+using PtrOrRaw = T;
+
 }  // namespace internal
 
 template <typename T>
@@ -280,14 +300,10 @@ void DeepCopy(const T* source_message, T* target_message) {
 }
 
 template <typename T>
-void ClearMessage(Ptr<T> message) {
-  static_assert(!std::is_const_v<T>, "");
-  upb_Message_Clear(internal::GetInternalMsg(message), T::minitable());
-}
-
-template <typename T>
-void ClearMessage(T* message) {
-  ClearMessage(protos::Ptr(message));
+void ClearMessage(internal::PtrOrRaw<T> message) {
+  auto ptr = Ptr(message);
+  auto minitable = internal::GetMiniTable(ptr);
+  upb_Message_Clear(internal::GetInternalMsg(ptr), minitable);
 }
 
 class ExtensionRegistry {
