eb1fdd39b44817f63d14c3e3321128e04350d220
esorot <theodorerose@google.com>
2024-07-18 16:18:03 +0000

    fix targets

--- regenerate_stale_files.sh
+++ regenerate_stale_files.sh
@@ -13,16 +13,12 @@ cd $(dirname -- "$0")
 readonly BazelBin="${BAZEL:-bazel} ${BAZEL_STARTUP_FLAGS}"
 
 STALENESS_TESTS=(
-  "java/core:generated_java_defaults_staleness_test"
-  "upb/reflection:bootstrap_upb_defaults_staleness_test"
   "src:cmake_lists_staleness_test"
   "src/google/protobuf:well_known_types_staleness_test"
   "objectivec:well_known_types_staleness_test"
   "php:test_amalgamation_staleness"
   "ruby/ext/google/protobuf_c:test_amalgamation_staleness"
   "upb/cmake:test_generated_files"
-  "upb/reflection:descriptor_upb_proto_staleness_test"
-  "upb_generator:plugin_upb_proto_staleness_test"
 )
 
 # Run and fix all staleness tests.
