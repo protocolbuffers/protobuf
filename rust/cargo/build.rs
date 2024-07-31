9d628f8024b0431dd9731282a20dabe2bae68ead
Derek Benson <derekbenson@google.com>
2024-07-31 10:44:00 -0700

    Create an initial version of protobuf-codegen. This exposes an initial working version of codegen for use with cargo.
    PiperOrigin-RevId: 658062486

--- rust/cargo/build.rs
+++ rust/cargo/build.rs
@@ -5,6 +5,8 @@
 // license that can be found in the LICENSE file or at
 // https://developers.google.com/open-source/licenses/bsd
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 fn main() {
     cc::Build::new()
         .flag("-std=c99")
@@ -12,10 +14,12 @@ fn main() {
         // .flag("-flto=thin")
         .warnings(false)
         .include("libupb")
+        .include("libupb/third_party/utf8_range")
         .file("libupb/upb/upb.c")
         .file("libupb/third_party/utf8_range/utf8_range.c")
         .define("UPB_BUILD_API", Some("1"))
         .compile("libupb");
     let path = std::path::Path::new("libupb");
-    println!("cargo:include={}", path.canonicalize().unwrap().display())
+    println!("cargo:include={}", path.canonicalize().unwrap().display());
+    println!("cargo:version={}", VERSION);
 }
