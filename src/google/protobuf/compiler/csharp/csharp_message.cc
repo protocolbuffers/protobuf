16713ce189ba8682730a66ad9919b6c44545682b
Hao Dong <imaxct@users.noreply.github.com>
2024-08-03 00:16:28 +0000

    Update framework methods

--- src/google/protobuf/compiler/csharp/csharp_message.cc
+++ src/google/protobuf/compiler/csharp/csharp_message.cc
@@ -124,6 +124,16 @@ void MessageGenerator::Generate(io::Printer* printer) {
 
   printer->Print("private pb::UnknownFieldSet _unknownFields;\n");
 
+  WriteGeneratedCodeAttributes(printer);
+  printer->Print(
+      vars,
+      "private scg::IEqualityComparer<$class_name$> Comparer\n"
+      "{\n"
+      "  get;\n"
+      "  private set;\n"
+      "}"
+  );
+
   if (has_extension_ranges_) {
     if (IsDescriptorProto(descriptor_->file())) {
       printer->Print(vars,
@@ -431,34 +441,8 @@ void MessageGenerator::GenerateFrameworkMethods(io::Printer* printer) {
   WriteGeneratedCodeAttributes(printer);
   printer->Print(vars,
                  "public bool Equals($class_name$ other) {\n"
-                 "  if (ReferenceEquals(other, null)) {\n"
-                 "    return false;\n"
-                 "  }\n"
-                 "  if (ReferenceEquals(other, this)) {\n"
-                 "    return true;\n"
-                 "  }\n");
-  printer->Indent();
-  for (int i = 0; i < descriptor_->field_count(); i++) {
-    std::unique_ptr<FieldGeneratorBase> generator(
-        CreateFieldGeneratorInternal(descriptor_->field(i)));
-    generator->WriteEquals(printer);
-  }
-  for (int i = 0; i < descriptor_->real_oneof_decl_count(); i++) {
-    printer->Print(
-        "if ($property_name$Case != other.$property_name$Case) return false;\n",
-        "property_name",
-        UnderscoresToCamelCase(descriptor_->oneof_decl(i)->name(), true));
-  }
-  if (has_extension_ranges_) {
-    printer->Print(
-        "if (!Equals(_extensions, other._extensions)) {\n"
-        "  return false;\n"
-        "}\n");
-  }
-  printer->Outdent();
-  printer->Print(
-      "  return Equals(_unknownFields, other._unknownFields);\n"
-      "}\n\n");
+                 "  return this.Comparer.Equals(this, other);"
+                 "}\n\n");
 
   // GetHashCode
   // Start with a non-zero value to easily distinguish between null and "empty"
@@ -466,31 +450,8 @@ void MessageGenerator::GenerateFrameworkMethods(io::Printer* printer) {
   WriteGeneratedCodeAttributes(printer);
   printer->Print(
       "public override int GetHashCode() {\n"
-      "  int hash = 1;\n");
-  printer->Indent();
-  for (int i = 0; i < descriptor_->field_count(); i++) {
-    std::unique_ptr<FieldGeneratorBase> generator(
-        CreateFieldGeneratorInternal(descriptor_->field(i)));
-    generator->WriteHash(printer);
-  }
-  for (int i = 0; i < descriptor_->real_oneof_decl_count(); i++) {
-    printer->Print(
-        "hash ^= (int) $name$Case_;\n", "name",
-        UnderscoresToCamelCase(descriptor_->oneof_decl(i)->name(), false));
-  }
-  if (has_extension_ranges_) {
-    printer->Print(
-        "if (_extensions != null) {\n"
-        "  hash ^= _extensions.GetHashCode();\n"
-        "}\n");
-  }
-  printer->Print(
-      "if (_unknownFields != null) {\n"
-      "  hash ^= _unknownFields.GetHashCode();\n"
-      "}\n"
-      "return hash;\n");
-  printer->Outdent();
-  printer->Print("}\n\n");
+      "  return this.Comparer.GetHashCode(this);"
+      "}\n\n");
 
   WriteGeneratedCodeAttributes(printer);
   printer->Print(
