91ed370b38269f17c9801d5da49337d4d691c904
Eron Wright <eron@pulumi.com>
2024-08-20 12:12:50 -0700

    [csharp] increase recursion limit

--- csharp/src/Google.Protobuf/CodedInputStream.cs
+++ csharp/src/Google.Protobuf/CodedInputStream.cs
@@ -54,7 +54,7 @@ namespace Google.Protobuf
         /// </summary>
         private ParserInternalState state;
 
-        internal const int DefaultRecursionLimit = 100;
+        internal const int DefaultRecursionLimit = 500;
         internal const int DefaultSizeLimit = Int32.MaxValue;
         internal const int BufferSize = 4096;
 
--- csharp/src/Google.Protobuf/ParseContext.cs
+++ csharp/src/Google.Protobuf/ParseContext.cs
@@ -23,7 +23,7 @@ namespace Google.Protobuf
     [SecuritySafeCritical]
     public ref struct ParseContext
     {
-        internal const int DefaultRecursionLimit = 100;
+        internal const int DefaultRecursionLimit = 500;
         internal const int DefaultSizeLimit = int.MaxValue;
 
         internal ReadOnlySpan<byte> buffer;
