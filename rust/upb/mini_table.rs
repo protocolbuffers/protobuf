90c35a23f254511ba650dd0f6b9d952a94ce5441
Hong Shin <hongshin@google.com>
2024-08-26 15:51:26 -0700

    Migrate protos::Serialize to hpb::Serialize
    PiperOrigin-RevId: 667745782

--- rust/upb/mini_table.rs
+++ rust/upb/mini_table.rs
@@ -15,13 +15,34 @@ opaque_pointee!(upb_MiniTableField);
 pub type RawMiniTableField = NonNull<upb_MiniTableField>;
 
 extern "C" {
+    /// Finds the field with the provided number, will return NULL if no such
+    /// field is found.
+    ///
+    /// # Safety
+    /// - `m` must be legal to deref
     pub fn upb_MiniTable_FindFieldByNumber(
         m: *const upb_MiniTable,
         number: u32,
     ) -> *const upb_MiniTableField;
 
+    /// Gets the field with the corresponding upb field index. This will never
+    /// return null: the provided number must be within bounds or else this is
+    /// UB.
+    ///
+    /// # Safety
+    /// - `m` must be legal to deref
+    /// - `number` must be a valid field index in the `m` table
     pub fn upb_MiniTable_GetFieldByIndex(
         m: *const upb_MiniTable,
         number: u32,
     ) -> *const upb_MiniTableField;
+
+    /// Gets the sub-MiniTable associated with `f`.
+    /// # Safety
+    /// - `m` and `f` must be valid to deref
+    /// - `f` must be a mesage or map typed field associated with `m`
+    pub fn upb_MiniTable_SubMessage(
+        m: *const upb_MiniTable,
+        f: *const upb_MiniTableField,
+    ) -> *const upb_MiniTable;
 }
