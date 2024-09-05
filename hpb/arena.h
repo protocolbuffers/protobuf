5233cdd7f60be23ff12e598150ce9551c657cb27
Hong Shin <hongshin@google.com>
2024-09-05 13:27:51 -0700

    Introduce hpb::Arena stub
    PiperOrigin-RevId: 671489452

---
+++ hpb/arena.h
@@ -0,0 +1,10 @@
+#ifndef GOOGLE_PROTOBUF_HPB_ARENA_H__
+#define GOOGLE_PROTOBUF_HPB_ARENA_H__
+
+#include "upb/mem/arena.hpp"
+
+namespace hpb {
+using Arena = upb::Arena;
+}
+
+#endif  // GOOGLE_PROTOBUF_HPB_ARENA_H__
