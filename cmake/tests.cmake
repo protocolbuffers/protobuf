2c749ad7d4dc3cb98aadff6f0a0e858c88a0e854
Matteo Paoluzzi <paoluzzi.matteo@protonmail.com>
2024-09-05 19:42:46 +0200

    fix alignment

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -241,7 +241,7 @@ foreach(_hdr ${_exclude_hdrs})
 endforeach()
 
 foreach(_hdr ${_local_hdrs})
-string(REPLACE "${protobuf_SOURCE_DIR}/src" "" _file ${_hdr})
+  string(REPLACE "${protobuf_SOURCE_DIR}/src" "" _file ${_hdr})
   set(_tmp_file "${CMAKE_BINARY_DIR}/tmp-install-test/${_file}")
   add_custom_command(TARGET remove-installed-headers PRE_BUILD
                      COMMAND ${CMAKE_COMMAND} -E remove -f "${_hdr}")
