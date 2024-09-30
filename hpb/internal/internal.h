5c9551457f433d1d01a89beba2b260d12c517cf0
Hong Shin <hongshin@google.com>
2024-09-30 14:23:26 -0700

    Internal Change
    PiperOrigin-RevId: 680718209

--- hpb/internal/internal.h
+++ hpb/internal/internal.h
@@ -1,3 +1,10 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2024 Google LLC.  All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
 #ifndef GOOGLE_PROTOBUF_HPB_INTERNAL_INTERNAL_H__
 #define GOOGLE_PROTOBUF_HPB_INTERNAL_INTERNAL_H__
 
