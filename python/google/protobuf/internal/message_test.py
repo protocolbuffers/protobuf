0720536eca20ca2f801127869d7f1211bceb3865
Jie Luo <jieluo@google.com>
2024-11-12 11:51:53 -0800

    Breaking change: Python nested message class __qualname__ now contains the outer message name. (Previous __qualname__ has the same result with __name__ for nested message that outer message name was not included)
    example:
    
    message Foo {
      message Bar {
        bool bool_field = 1;
      }
    }
    nested = test_pb2.Foo.Bar()
    self.assertEqual('Bar', nested.__class__.__name__)
    self.assertEqual('Foo.Bar', nested.__class__.__qualname__) # It was 'Bar' before
    PiperOrigin-RevId: 695817876

--- python/google/protobuf/internal/message_test.py
+++ python/google/protobuf/internal/message_test.py
@@ -1337,6 +1337,18 @@ class MessageTest(unittest.TestCase):
     self.assertIn('oneof_bytes', m)
     self.assertNotIn('oneof_string', m)
 
+  def testMessageClassName(self, message_module):
+    m = message_module.TestAllTypes()
+    self.assertEqual('TestAllTypes', type(m).__name__)
+    self.assertEqual('TestAllTypes', m.__class__.__qualname__)
+
+    nested = message_module.TestAllTypes.NestedMessage()
+    self.assertEqual('NestedMessage', type(nested).__name__)
+    self.assertEqual('NestedMessage', nested.__class__.__name__)
+    self.assertEqual(
+        'TestAllTypes.NestedMessage', nested.__class__.__qualname__
+    )
+
 
 # Class to test proto2-only features (required, extensions, etc.)
 @testing_refleaks.TestCase
