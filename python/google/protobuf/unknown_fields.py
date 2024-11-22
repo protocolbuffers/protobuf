3a53403ad1b8bc05e7c0da4f90879beb97a12880
Mike Kruskal <mkruskal@google.com>
2024-11-21 17:04:46 -0800

    Remove decoder.SkipField.
    This is only used in some narrow edge cases, and is a less-safe version of our unknown field decoders.  Switching to those reduces some duplication and improves error handling.
    
    PiperOrigin-RevId: 698966570

--- python/google/protobuf/unknown_fields.py
+++ python/google/protobuf/unknown_fields.py
@@ -72,9 +72,7 @@ else:
           InternalAdd(field_number, wire_format.WIRETYPE_LENGTH_DELIMITED, data)
       else:
         for tag_bytes, buffer in unknown_fields:
-          # pylint: disable=protected-access
-          (tag, _) = decoder._DecodeVarint(tag_bytes, 0)
-          field_number, wire_type = wire_format.UnpackTag(tag)
+          field_number, wire_type = decoder.DecodeTag(tag_bytes)
           if field_number == 0:
             raise RuntimeError('Field number 0 is illegal.')
           (data, _) = decoder._DecodeUnknownField(
