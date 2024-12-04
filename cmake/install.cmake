3b389187ceefb3bfe02b0277a9df0726a93ad08c
Andre Sailer <andre.philippe.sailer@cern.ch>
2024-12-03 19:07:04 -0800

    PkgConfig: account for possible debug postfix in library name (#19444)
    PkgConfig: account for possible debug postfix in library name, this fixes #19325
    
    Closes #19444
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/protocolbuffers/protobuf/pull/19444 from andresailer:pkgConfigD 8f353cb6ca839ec9d48684fc1e9b45b7a61fe224
    PiperOrigin-RevId: 702547617

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -20,6 +20,10 @@ if (protobuf_BUILD_SHARED_LIBS)
   set(_protobuf_PC_CFLAGS -DPROTOBUF_USE_DLLS)
 endif ()
 
+if(CMAKE_BUILD_TYPE STREQUAL Debug)
+  # attach debug postfix only in debug mode
+  set(protobuf_LIBRARY_POSTFIX ${protobuf_DEBUG_POSTFIX})
+endif()
 configure_file(${CMAKE_CURRENT_SOURCE_DIR}/cmake/protobuf.pc.cmake
                ${CMAKE_CURRENT_BINARY_DIR}/protobuf.pc @ONLY)
 configure_file(${CMAKE_CURRENT_SOURCE_DIR}/cmake/protobuf-lite.pc.cmake
