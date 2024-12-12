0765dd476afc53e3bad345c6a65ac3c2688ac49e
Mike Kruskal <mkruskal@google.com>
2024-12-11 20:21:56 -0800

    Automated Code Change
    PiperOrigin-RevId: 705341930

--- src/google/protobuf/descriptor_unittest.cc
+++ src/google/protobuf/descriptor_unittest.cc
@@ -2997,8 +2997,7 @@ TEST_F(MiscTest, FieldOptions) {
 
   // "bar" had options set.
   EXPECT_NE(&FieldOptions::default_instance(), options);
-  EXPECT_TRUE(bar->options().has_ctype());
-  EXPECT_EQ(FieldOptions::CORD, bar->options().ctype());
+  EXPECT_EQ(bar->cpp_string_type(), FieldDescriptor::CppStringType::kCord);
 }
 
 // ===================================================================
@@ -3638,7 +3637,7 @@ TEST(CustomOptions, OptionLocations) {
 
   // See that the regular options went through unscathed.
   EXPECT_TRUE(message->options().has_message_set_wire_format());
-  EXPECT_EQ(FieldOptions::CORD, field->options().ctype());
+  EXPECT_EQ(field->cpp_string_type(), FieldDescriptor::CppStringType::kString);
 }
 
 TEST(CustomOptions, OptionTypes) {
