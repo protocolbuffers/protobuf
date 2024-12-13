25591760618d7e36a93cc3637c78272d00d70aee
Adam Cozzette <acozzette@google.com>
2024-12-13 11:22:58 -0800

    Rust: update protobuf_codegen crate to rely on separately installed protoc
    I also added a check to ensure that `protoc --version` matches the protobuf
    runtime version. The minitable generator plugin version should also match, but
    unfortunately we can't easily check this today because it does not provide a
    way to check its version.
    
    PiperOrigin-RevId: 705944904

--- rust/release_crates/protobuf_codegen/src/lib.rs
+++ rust/release_crates/protobuf_codegen/src/lib.rs
@@ -4,20 +4,49 @@ use std::path::{Path, PathBuf};
 pub struct CodeGen {
     inputs: Vec<PathBuf>,
     output_dir: PathBuf,
-    protoc_path: Option<PathBuf>,
-    protoc_gen_upb_minitable_path: Option<PathBuf>,
     includes: Vec<PathBuf>,
 }
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+// Given the output of "protoc --version", returns a shortened version string
+// suitable for comparing against the protobuf crate version.
+//
+// The output of protoc --version looks something like "libprotoc XX.Y",
+// optionally followed by "-dev" or "-rcN". We want to strip the "-dev" suffix
+// if present and return something like "30.0" or "30.0-rc1".
+fn protoc_version(protoc_output: &str) -> String {
+    let mut s = protoc_output.strip_prefix("libprotoc ").unwrap().to_string();
+    let first_dash = s.find("-dev");
+    if let Some(i) = first_dash {
+        s.truncate(i);
+    }
+    s
+}
+
+// Given a crate version string, returns just the part of it suitable for
+// comparing against the protoc version. The crate version is of the form
+// "X.Y.Z" with an optional suffix starting with a dash. We want to drop the
+// major version ("X.") and only keep the suffix if it starts with "-rc".
+fn expected_protoc_version(cargo_version: &str) -> String {
+    let mut s = cargo_version.to_string();
+    let is_release_candidate = s.find("-rc") != None;
+    if !is_release_candidate {
+        if let Some(i) = s.find('-') {
+            s.truncate(i);
+        }
+    }
+    let mut v: Vec<&str> = s.split('.').collect();
+    assert_eq!(v.len(), 3);
+    v.remove(0);
+    v.join(".")
+}
 
 impl CodeGen {
     pub fn new() -> Self {
         Self {
             inputs: Vec::new(),
             output_dir: PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("protobuf_generated"),
-            protoc_path: None,
-            protoc_gen_upb_minitable_path: None,
             includes: Vec::new(),
         }
     }
@@ -34,20 +63,6 @@ impl CodeGen {
 
     pub fn output_dir(&mut self, output_dir: impl AsRef<Path>) -> &mut Self {
         self.output_dir = output_dir.as_ref().to_owned();
-        self
-    }
-
-    pub fn protoc_path(&mut self, protoc_path: impl AsRef<Path>) -> &mut Self {
-        self.protoc_path = Some(protoc_path.as_ref().to_owned());
-        self
-    }
-
-    pub fn protoc_gen_upb_minitable_path(
-        &mut self,
-        protoc_gen_upb_minitable_path: impl AsRef<Path>,
-    ) -> &mut Self {
-        self.protoc_gen_upb_minitable_path =
-            Some(protoc_gen_upb_minitable_path.as_ref().to_owned());
         self
     }
 
@@ -92,12 +107,22 @@ impl CodeGen {
             );
         }
 
-        let protoc_path = if let Some(path) = &self.protoc_path {
-            path.clone()
-        } else {
-            protoc_path().expect("To be a supported platform")
-        };
-        let mut cmd = std::process::Command::new(protoc_path);
+        let mut version_cmd = std::process::Command::new("protoc");
+        let output = version_cmd
+            .arg("--version")
+            .output()
+            .map_err(|e| format!("failed to run protoc --version: {}", e))?;
+
+        let protoc_version = protoc_version(&String::from_utf8(output.stdout).unwrap());
+        let expected_protoc_version = expected_protoc_version(VERSION);
+        if protoc_version != expected_protoc_version {
+            panic!(
+                "Expected protoc version {} but found {}",
+                expected_protoc_version, protoc_version
+            );
+        }
+
+        let mut cmd = std::process::Command::new("protoc");
         for input in &self.inputs {
             cmd.arg(input);
         }
@@ -105,12 +130,6 @@ impl CodeGen {
             // Attempt to make the directory if it doesn't exist
             let _ = std::fs::create_dir(&self.output_dir);
         }
-        let protoc_gen_upb_minitable_path = if let Some(path) = &self.protoc_gen_upb_minitable_path
-        {
-            path.clone()
-        } else {
-            protoc_gen_upb_minitable_path().expect("To be a supported platform")
-        };
 
         for include in &self.includes {
             println!("cargo:rerun-if-changed={}", include.display());
@@ -118,10 +137,6 @@ impl CodeGen {
 
         cmd.arg(format!("--rust_out={}", self.output_dir.display()))
             .arg("--rust_opt=experimental-codegen=enabled,kernel=upb")
-            .arg(format!(
-                "--plugin=protoc-gen-upb_minitable={}",
-                protoc_gen_upb_minitable_path.display()
-            ))
             .arg(format!("--upb_minitable_out={}", self.output_dir.display()));
         for include in &self.includes {
             cmd.arg(format!("--proto_path={}", include.display()));
@@ -162,32 +177,24 @@ impl CodeGen {
     }
 }
 
-fn get_path_for_arch() -> Option<PathBuf> {
-    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    path.push("bin");
-    match (std::env::consts::OS, std::env::consts::ARCH) {
-        ("macos", "x86_64") => path.push("osx-x86_64"),
-        ("macos", "aarch64") => path.push("osx-aarch_64"),
-        ("linux", "aarch64") => path.push("linux-aarch_64"),
-        ("linux", "powerpc64") => path.push("linux-ppcle_64"),
-        ("linux", "s390x") => path.push("linux-s390_64"),
-        ("linux", "x86") => path.push("linux-x86_32"),
-        ("linux", "x86_64") => path.push("linux-x86_64"),
-        ("windows", "x86") => path.push("win32"),
-        ("windows", "x86_64") => path.push("win64"),
-        _ => return None,
-    };
-    Some(path)
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use googletest::prelude::*;
 
-pub fn protoc_path() -> Option<PathBuf> {
-    let mut path = get_path_for_arch()?;
-    path.push("protoc");
-    Some(path)
-}
+    #[googletest::test]
+    fn test_protoc_version() {
+        assert_eq!(protoc_version("libprotoc 30.0"), "30.0");
+        assert_eq!(protoc_version("libprotoc 30.0-dev"), "30.0");
+        assert_eq!(protoc_version("libprotoc 30.0-rc1"), "30.0-rc1");
+    }
 
-pub fn protoc_gen_upb_minitable_path() -> Option<PathBuf> {
-    let mut path = get_path_for_arch()?;
-    path.push("protoc-gen-upb_minitable");
-    Some(path)
+    #[googletest::test]
+    fn test_expected_protoc_version() {
+        assert_eq!(expected_protoc_version("4.30.0"), "30.0");
+        assert_eq!(expected_protoc_version("4.30.0-alpha"), "30.0");
+        assert_eq!(expected_protoc_version("4.30.0-beta"), "30.0");
+        assert_eq!(expected_protoc_version("4.30.0-pre"), "30.0");
+        assert_eq!(expected_protoc_version("4.30.0-rc1"), "30.0-rc1");
+    }
 }
