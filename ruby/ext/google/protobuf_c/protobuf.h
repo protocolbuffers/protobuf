4b1dd06357ec7f1d908093083246677253711734
Yuji Yaginuma <yuuji.yaginuma@gmail.com>
2024-12-13 16:18:16 +0900

    Remove redundant version check
    This was added by 3f98af287b8b1919e1efb65d5f5851ff1e7628ce.
    I'm not sure why it was needed. But, currently, the supported version
    is specified in the gemspec(`required_ruby_version `) correctly.
    So no need to check it inside code.

--- ruby/ext/google/protobuf_c/protobuf.h
+++ ruby/ext/google/protobuf_c/protobuf.h
@@ -16,12 +16,6 @@
 #undef NDEBUG
 #endif
 
-#include <ruby/version.h>
-
-#if RUBY_API_VERSION_CODE < 20700
-#error Protobuf requires Ruby >= 2.7
-#endif
-
 #include <assert.h>  // Must be included after the NDEBUG logic above.
 #include <ruby/encoding.h>
 #include <ruby/vm.h>
