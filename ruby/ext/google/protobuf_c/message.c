1ee3841d9793d7596289aa5fff208ea9b99ff9f2
なつき <i@ntk.me>
2024-12-18 10:22:48 -0800

    Fix ruby `has_...?` method

--- ruby/ext/google/protobuf_c/message.c
+++ ruby/ext/google/protobuf_c/message.c
@@ -362,7 +362,8 @@ static VALUE Message_field_accessor(VALUE _self, const upb_FieldDef* f,
       if (!upb_FieldDef_HasPresence(f)) {
         rb_raise(rb_eRuntimeError, "Field does not have presence.");
       }
-      return upb_Message_HasFieldByDef(Message_Get(_self, NULL), f);
+      return upb_Message_HasFieldByDef(Message_Get(_self, NULL), f) ? Qtrue
+                                                                    : Qfalse;
     case METHOD_WRAPPER_GETTER: {
       Message* self = ruby_to_Message(_self);
       if (upb_Message_HasFieldByDef(self->msg, f)) {
