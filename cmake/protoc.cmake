71896307cd777814b6ff0f08603c70e050e13d16
Mike Kruskal <mkruskal@google.com>
2024-12-23 14:19:35 -0800

    Copybara test

--- cmake/protoc.cmake
+++ cmake/protoc.cmake
@@ -2,6 +2,7 @@ set(protoc_files
   ${protobuf_SOURCE_DIR}/src/google/protobuf/compiler/main.cc
 )
 
+
 add_executable(protoc ${protoc_files} ${protobuf_version_rc_file})
 target_link_libraries(protoc
   libprotoc
