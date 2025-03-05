874b8031cf4f72a0eeeab3be26207c38df52aa74
Jason Lunn <jason.lunn@gmail.com>
2025-03-05 16:01:12 -0500

    Handle differences in behavior for env between macOS and linux

--- conformance/conformance_ruby.rb
+++ conformance/conformance_ruby.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby -I. -Iruby -Iruby/lib
+#!/usr/bin/env -S ruby -I. -Iruby -Iruby/lib
 #
 # Protocol Buffers - Google's data interchange format
 # Copyright 2008 Google Inc.  All rights reserved.
