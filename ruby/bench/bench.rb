7c58754fc8e08d0fdeda35d296c021b5f08c0bd8
Kevin Menard <kevin@nirvdrum.com>
2025-03-18 15:51:40 -0400

    Make Protobuf field names 12 characters long to match the mean field name length at Shopify.
    Co-authored-by: Dave Benvenuti <dave.benvenuti@shopify.com>

--- ruby/bench/bench.rb
+++ ruby/bench/bench.rb
@@ -3,10 +3,10 @@ require "google/protobuf"
 require_relative "bench_messages_pb"
 
 message = BenchMessages::Message.new(
-  field_1: "howdy",
-  field_2: 123,
-  field_3: { "a" => "1", "b" => "2", "c" => "3" },
-  field_4: BenchMessages::SubMessage.new(field_1: 456),
+  bigger_fld_1: "howdy",
+  bigger_fld_2: 123,
+  bigger_fld_3: { "a" => "1", "b" => "2", "c" => "3" },
+  bigger_fld_4: BenchMessages::SubMessage.new(bigger_fld_1: 456),
 )
 
 def assert_equal(val1, val2)
@@ -15,24 +15,24 @@ end
 
 Benchmark.ips do |x|
   x.report("Message#[]") do
-    assert_equal("howdy", message["field_1"])
-    assert_equal(123, message["field_2"])
-    assert_equal({ "a" => "1", "b" => "2", "c" => "3" }, message["field_3"].to_h)
-    assert_equal(456, message["field_4"]["field_1"])
+    assert_equal("howdy", message["bigger_fld_1"])
+    assert_equal(123, message["bigger_fld_2"])
+    assert_equal({ "a" => "1", "b" => "2", "c" => "3" }, message["bigger_fld_3"].to_h)
+    assert_equal(456, message["bigger_fld_4"]["bigger_fld_1"])
   end
 
   x.report("Message#get (symbols)") do
-    assert_equal("howdy", message.get(:field_1))
-    assert_equal(123, message.get(:field_2))
-    assert_equal({ "a" => "1", "b" => "2", "c" => "3" }, message.get(:field_3).to_h)
-    assert_equal(456, message.get(:field_4).get(:field_1))
+    assert_equal("howdy", message.get(:bigger_fld_1))
+    assert_equal(123, message.get(:bigger_fld_2))
+    assert_equal({ "a" => "1", "b" => "2", "c" => "3" }, message.get(:bigger_fld_3).to_h)
+    assert_equal(456, message.get(:bigger_fld_4).get(:bigger_fld_1))
   end
 
   x.report("Message#get (strings)") do
-    assert_equal("howdy", message.get("field_1"))
-    assert_equal(123, message.get("field_2"))
-    assert_equal({ "a" => "1", "b" => "2", "c" => "3" }, message.get("field_3").to_h)
-    assert_equal(456, message.get("field_4").get("field_1"))
+    assert_equal("howdy", message.get("bigger_fld_1"))
+    assert_equal(123, message.get("bigger_fld_2"))
+    assert_equal({ "a" => "1", "b" => "2", "c" => "3" }, message.get("bigger_fld_3").to_h)
+    assert_equal(456, message.get("bigger_fld_4").get("bigger_fld_1"))
   end
 
   x.compare!
