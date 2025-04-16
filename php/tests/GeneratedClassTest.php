cb4b6df1268b29e9e1db8099cc07d75d2320cc13
FabioBatSilva <fabio.bat.silva@gmail.com>
2025-04-15 20:06:38 -0400

    [php][ext] - Remove duplicate assertion

--- php/tests/GeneratedClassTest.php
+++ php/tests/GeneratedClassTest.php
@@ -51,7 +51,6 @@ class GeneratedClassTest extends TestBase
 
         $this->assertTrue(property_exists(TestMessage::class, 'optional_string'));
         $this->assertTrue(property_exists($message, 'optional_string'));
-        $this->assertFalse(isset($message->optional_string));
 
         $message->setOptionalString('bar');
 
@@ -63,6 +62,7 @@ class GeneratedClassTest extends TestBase
 
         // isset() should return false for attributes that are not public.
         $this->assertFalse(isset($message->optional_string));
+        // Should be false for attributes that are not part of the message.
         $this->assertFalse(isset($message->foo));
     }
 
