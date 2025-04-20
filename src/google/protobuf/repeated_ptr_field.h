7d19b7d6a74f8641c7af0bf1b695d8fca246bf3c
Pavel Solodovnikov <pavel.al.solodovnikov@gmail.com>
2025-04-20 21:15:49 +0300

    Fix link errors when building `libprotoc` using latest llvm-mingw toolchain
    Looks like Clang in MinGW mode doesn't add explicit
    instantiations of a template function (defined in a separate
    translation unit) to the overall export set of the library,
    despite the enclosing class having `PROTOBUF_EXPORT` attribute.
    
    This happens when building `libprotoc` with LLVM-MinGW toolchain,
    e.g., while trying to build/install protobuf via vcpkg for one
    of the `*-mingw-dynamic` triples.
    
    So, add another `PROTOBUF_EXPORT` to the explicit instantiation
    declaration for `RepeatedPtrFieldBase::MergeFrom<std::string>`.
    This won't hurt in either case, but resolves the link issue for
    llvm-mingw builds of the `libprotoc` library.
    
    Signed-off-by: Pavel Solodovnikov <pavel.al.solodovnikov@gmail.com>

--- src/google/protobuf/repeated_ptr_field.h
+++ src/google/protobuf/repeated_ptr_field.h
@@ -804,7 +804,7 @@ inline void RepeatedPtrFieldBase::MergeFrom<Message>(
 }
 
 // Appends all `std::string` values from `from` to this instance.
-template <>
+template <> PROTOBUF_EXPORT
 void RepeatedPtrFieldBase::MergeFrom<std::string>(
     const RepeatedPtrFieldBase& from);
 
