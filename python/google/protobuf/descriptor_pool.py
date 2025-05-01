a60d1737d32c8358468de4350505955329eca9fb
Mike Kruskal <mkruskal@google.com>
2025-05-01 12:05:59 -0700

    Internal changes
    PiperOrigin-RevId: 753677301

--- python/google/protobuf/descriptor_pool.py
+++ python/google/protobuf/descriptor_pool.py
@@ -85,9 +85,9 @@ class DescriptorPool(object):
 
   if _USE_C_DESCRIPTORS:
 
-   def __new__(cls, descriptor_db=None):
-     # pylint: disable=protected-access
-     return descriptor._message.DescriptorPool(descriptor_db)
+     def __new__(cls, descriptor_db=None):
+       # pylint: disable=protected-access
+       return descriptor._message.DescriptorPool(descriptor_db)
 
   def __init__(
       self, descriptor_db=None, use_deprecated_legacy_json_field_conflicts=False
