f6fa63aabb68dd2750dabdc53bf96bcac3d43452
esrauchg <140440793+esrauchg@users.noreply.github.com>
2025-05-15 18:43:34 +0000

    Add missing hashset import.

--- java/core/src/main/java/com/google/protobuf/GeneratedMessage.java
+++ java/core/src/main/java/com/google/protobuf/GeneratedMessage.java
@@ -21,6 +21,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
