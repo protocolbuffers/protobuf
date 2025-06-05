dc32d987d95ff32cab0ab6fe320a37b195e7fec7
Adam Cozzette <acozzette@google.com>
2025-06-05 07:26:06 -0700

    Rust: implement ProxiedInRepeated with a blanket implementation for upb
    Most of the method implementations ended up being identical to the gencode
    implementations. The only ones that had to be significantly different were
    `repeated_get_unchecked` and `repeated_get_mut_unchecked`. I had to make them
    rely on the `UpbTypeConversions` trait instead of using methods on the
    generated view and mut proxies.
    
    The motivation for this is to try to implement as much as possible in the
    runtime instead of in generated code. That should help mitigate the downsides
    of needing to accommodate stale gencode, because it will allow us to add
    features or modify behavior in a way that takes effect even if the gencode is
    not updated.
    
    PiperOrigin-RevId: 767596134

--- rust/upb.rs
+++ rust/upb.rs
@@ -465,6 +465,116 @@ macro_rules! impl_repeated_bytes {
     }
 }
 
+unsafe impl<T> ProxiedInRepeated for T
+where
+    T: Message + UpbTypeConversions + AssociatedMiniTable,
+{
+    fn repeated_new(_private: Private) -> Repeated<Self> {
+        let arena = Arena::new();
+        Repeated::from_inner(Private, unsafe {
+            InnerRepeated::from_raw_parts(upb_Array_New(arena.raw(), CType::Message), arena)
+        })
+    }
+
+    unsafe fn repeated_free(_private: Private, _repeated: &mut Repeated<Self>) {
+        // No-op: the memory will be dropped by the arena.
+    }
+
+    fn repeated_len(repeated: View<Repeated<Self>>) -> usize {
+        // SAFETY: `repeated.as_raw()` is a valid `upb_Array*`.
+        unsafe { upb_Array_Size(repeated.as_raw(Private)) }
+    }
+
+    fn repeated_push(mut repeated: Mut<Repeated<Self>>, val: impl IntoProxied<Self>) {
+        // SAFETY:
+        // - `repeated.as_raw()` is a valid `upb_Array*`.
+        // - `msg_ptr` is a valid `upb_Message*`.
+        unsafe {
+            upb_Array_Append(
+                repeated.as_raw(Private),
+                <Self as UpbTypeConversions>::into_message_value_fuse_if_required(
+                    repeated.raw_arena(Private),
+                    val.into_proxied(Private),
+                ),
+                repeated.raw_arena(Private),
+            );
+        };
+    }
+
+    fn repeated_clear(mut repeated: Mut<Repeated<Self>>) {
+        // SAFETY:
+        // - `repeated.as_raw()` is a valid `upb_Array*`.
+        unsafe { upb_Array_Resize(repeated.as_raw(Private), 0, repeated.raw_arena(Private)) };
+    }
+
+    unsafe fn repeated_get_unchecked<'a>(
+        repeated: View<'a, Repeated<Self>>,
+        index: usize,
+    ) -> View<'a, Self> {
+        // SAFETY:
+        // - `repeated.as_raw()` is a valid `const upb_Array*`.
+        // - `index < len(repeated)` is promised by the caller.
+        let val = unsafe { upb_Array_Get(repeated.as_raw(Private), index) };
+        // SAFETY:
+        // - `val` has the correct variant for Self.
+        // - `val` is valid for `'a` lifetime.
+        unsafe { Self::from_message_value(val) }
+    }
+
+    unsafe fn repeated_get_mut_unchecked<'a>(
+        mut repeated: Mut<'a, Repeated<Self>>,
+        index: usize,
+    ) -> Mut<'a, Self> {
+        // SAFETY:
+        // - `repeated.as_raw()` is a valid `upb_Array*`.
+        // - `repeated` is a an array of message-valued elements.
+        // - `index < len(repeated)` is promised by the caller.
+        let val = unsafe { upb_Array_GetMutable(repeated.as_raw(Private), index) };
+        // SAFETY:
+        // - `val` is the correct variant for `Self`.
+        // - `val` is valid for `'a` lifetime.
+        unsafe { Self::from_message_mut(val, repeated.arena(Private)) }
+    }
+
+    unsafe fn repeated_set_unchecked(
+        mut repeated: Mut<Repeated<Self>>,
+        index: usize,
+        val: impl IntoProxied<Self>,
+    ) {
+        unsafe {
+            upb_Array_Set(
+                repeated.as_raw(Private),
+                index,
+                <Self as UpbTypeConversions>::into_message_value_fuse_if_required(
+                    repeated.raw_arena(Private),
+                    val.into_proxied(Private),
+                ),
+            )
+        }
+    }
+
+    fn repeated_copy_from(src: View<Repeated<Self>>, dest: Mut<Repeated<Self>>) {
+        // SAFETY:
+        // - Elements of `src` and `dest` have message minitable `Self::mini_table()`.
+        unsafe {
+            repeated_message_copy_from(src, dest, <Self as AssociatedMiniTable>::mini_table());
+        }
+    }
+
+    fn repeated_reserve(mut repeated: Mut<Repeated<Self>>, additional: usize) {
+        // SAFETY:
+        // - `repeated.as_raw()` is a valid `upb_Array*`.
+        unsafe {
+            let size = upb_Array_Size(repeated.as_raw(Private));
+            upb_Array_Reserve(
+                repeated.as_raw(Private),
+                size + additional,
+                repeated.raw_arena(Private),
+            );
+        }
+    }
+}
+
 impl<'msg, T> RepeatedMut<'msg, T> {
     // Returns a `RawArena` which is live for at least `'msg`
     #[doc(hidden)]
