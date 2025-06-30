89a9d23f629e61e22cd288d7e910457217f527b4
Hong Shin <hongshin@google.com>
2025-06-30 07:48:15 -0700

    hpb: push down types to backend/upb/types and backend/cpp/types
    PiperOrigin-RevId: 777568126

---
+++ hpb/backend/cpp/types.h
@@ -0,0 +1,23 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2025 Google LLC.  All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+#ifndef GOOGLE_PROTOBUF_HPB_BACKEND_CPP_TYPES_H__
+#define GOOGLE_PROTOBUF_HPB_BACKEND_CPP_TYPES_H__
+
+#include "google/protobuf/arena.h"
+
+namespace hpb {
+namespace internal {
+namespace backend {
+namespace cpp {
+using Arena = google::protobuf::Arena;
+}  // namespace cpp
+}  // namespace backend
+}  // namespace internal
+}  // namespace hpb
+
+#endif  // GOOGLE_PROTOBUF_HPB_BACKEND_CPP_TYPES_H__
