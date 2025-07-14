c59595d02f7fe4f0a9d7422c0e1110f5e141bbd9
Clayton Knittel <cknittel@google.com>
2025-07-13 19:53:00 -0700

    Move gltf protos to ::imp::gltf::imp_proto namespace.
    PiperOrigin-RevId: 782716630

--- src/google/protobuf/compiler/java/java_features.pb.cc
+++ src/google/protobuf/compiler/java/java_features.pb.cc
@@ -332,6 +332,9 @@ JavaFeatures::~JavaFeatures() {
 }
 inline void JavaFeatures::SharedDtor(MessageLite& self) {
   JavaFeatures& this_ = static_cast<JavaFeatures&>(self);
+  if constexpr (::_pbi::DebugHardenVerifyHasBitConsistency()) {
+    this_.VerifyHasBitConsistency();
+  }
   this_._internal_metadata_.Delete<::google::protobuf::UnknownFieldSet>();
   ABSL_DCHECK(this_.GetArena() == nullptr);
   this_._impl_.~Impl_();
@@ -466,6 +469,9 @@ PROTOBUF_NOINLINE void JavaFeatures::Clear() {
     ::google::protobuf::io::EpsCopyOutputStream* PROTOBUF_NONNULL stream) const {
   const JavaFeatures& this_ = *this;
 #endif  // PROTOBUF_CUSTOM_VTABLE
+  if constexpr (::_pbi::DebugHardenVerifyHasBitConsistency()) {
+    this_.VerifyHasBitConsistency();
+  }
   // @@protoc_insertion_point(serialize_to_array_start:pb.JavaFeatures)
   ::uint32_t cached_has_bits = 0;
   (void)cached_has_bits;
@@ -551,6 +557,9 @@ PROTOBUF_NOINLINE void JavaFeatures::Clear() {
 void JavaFeatures::MergeImpl(::google::protobuf::MessageLite& to_msg, const ::google::protobuf::MessageLite& from_msg) {
   auto* const _this = static_cast<JavaFeatures*>(&to_msg);
   auto& from = static_cast<const JavaFeatures&>(from_msg);
+  if constexpr (::_pbi::DebugHardenVerifyHasBitConsistency()) {
+    from.VerifyHasBitConsistency();
+  }
   // @@protoc_insertion_point(class_specific_merge_from_start:pb.JavaFeatures)
   ABSL_DCHECK_NE(&from, _this);
   ::uint32_t cached_has_bits = 0;
