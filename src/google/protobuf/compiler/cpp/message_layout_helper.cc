f83d451ea3ff67b97f8dca67a675f710c803b437
Clayton Knittel <cknittel@google.com>
2025-08-20 11:21:00 -0700

    Add a return statement to FieldHotnessIndex satisfy the compiler.
    PiperOrigin-RevId: 797389570

--- src/google/protobuf/compiler/cpp/message_layout_helper.cc
+++ src/google/protobuf/compiler/cpp/message_layout_helper.cc
@@ -101,6 +101,9 @@ constexpr size_t MessageLayoutHelper::FieldHotnessIndex(FieldHotness hotness) {
       case FieldHotness::kMaxHotness:
         internal::Unreachable();
     }
+
+    // Make the compiler happy.
+    return 0;
   } else {
     return static_cast<size_t>(hotness);
   }
