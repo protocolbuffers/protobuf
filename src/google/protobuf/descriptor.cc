877d633412d91f0f5014e04d7fd88079411a1778
Mike Kruskal <mkruskal@google.com>
2025-09-04 20:50:24 -0700

    Add Java (mutable) language feature handling to 2024 transform.
    This handles all of the global java option changes, as well as the mutable/proto1 features.  These can't be reasonably separated because they're all so interdependent.
    
    PiperOrigin-RevId: 803293866

--- src/google/protobuf/descriptor.cc
+++ src/google/protobuf/descriptor.cc
@@ -6731,7 +6731,7 @@ FileDescriptor* DescriptorBuilder::BuildFileImpl(
           }
         });
   }
-  if (!had_errors_) {
+  if (!had_errors_ && pool_->enforce_symbol_visibility_) {
     // Check Symbol Visibility Rules.
     CheckVisibilityRules(result, proto);
   }
@@ -7929,7 +7929,8 @@ void DescriptorBuilder::CrossLinkField(FieldDescriptor* field,
                                      "\" is not a message type.");
                });
       return;
-    } else if (!extendee.IsVisibleFrom(file_)) {
+    } else if (!extendee.IsVisibleFrom(file_) &&
+               pool_->enforce_symbol_visibility_) {
       AddError(field->full_name(), proto,
                DescriptorPool::ErrorCollector::EXTENDEE, [&] {
                  return extendee.GetVisibilityError(file_, "target of extend");
@@ -8041,7 +8042,7 @@ void DescriptorBuilder::CrossLinkField(FieldDescriptor* field,
       field->is_map_ = sub_message->options().map_entry();
     }
 
-    if (!type.IsVisibleFrom(file_)) {
+    if (!type.IsVisibleFrom(file_) && pool_->enforce_symbol_visibility_) {
       AddError(field->full_name(), proto, DescriptorPool::ErrorCollector::TYPE,
                [&] { return type.GetVisibilityError(file_); });
       return;
