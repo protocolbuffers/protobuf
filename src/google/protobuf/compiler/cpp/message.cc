1116a562234713ee72609d03c564a8bd0ae72c91
Clayton Knittel <cknittel@google.com>
2025-10-15 14:22:27 -0700

    Internal change.
    PiperOrigin-RevId: 819912321

--- src/google/protobuf/compiler/cpp/message.cc
+++ src/google/protobuf/compiler/cpp/message.cc
@@ -126,10 +126,11 @@ void DebugAssertUniformLikelyPresence(
 // uses arena offsets, it no longer needs arena seeding, and it is not
 // zero-initializable.
 bool MessageHasFieldUsingArenaOffset(const Descriptor* descriptor) {
-  return absl::c_any_of(FieldRange(descriptor),
-                        [](const FieldDescriptor* field) {
-                          return field->is_repeated() || field->is_map();
-                        }) ||
+  return absl::c_any_of(
+             FieldRange(descriptor),
+             [](const FieldDescriptor* field) {
+               return field->is_repeated() || field->is_map();
+             }) ||
          descriptor->extension_range_count() > 0;
 }
 
