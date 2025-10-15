143e3eadeef8928750cd9fcb62c6d4cac79e8435
Clayton Knittel <cknittel@google.com>
2025-10-14 22:57:48 -0700

    Use arena offsets instead of holding an arena pointer in `RepeatedField`.
    This does not reduce the size of `RepeatedField`, but does greatly simplify the SOO logic, and makes `size()` branchless. Additionally, `RepeatedField`s now need `memcpy` in `MessageCreator::PlacementNew`, but don't require arena seeding.
    
    All behavior changes introduced by this cl are flag-gated with `PROTOBUF_INTERNAL_REMOVE_ARENA_PTRS_REPEATED_FIELD`, which is currently off.
    
    PiperOrigin-RevId: 819581961

--- src/google/protobuf/compiler/cpp/message.cc
+++ src/google/protobuf/compiler/cpp/message.cc
@@ -128,7 +128,7 @@ void DebugAssertUniformLikelyPresence(
 bool MessageHasFieldUsingArenaOffset(const Descriptor* descriptor) {
   return absl::c_any_of(FieldRange(descriptor),
                         [](const FieldDescriptor* field) {
-                          return IsRepeatedPtrField(field) || field->is_map();
+                          return field->is_repeated() || field->is_map();
                         }) ||
          descriptor->extension_range_count() > 0;
 }
@@ -4065,7 +4065,7 @@ MessageGenerator::NewOpRequirements MessageGenerator::GetNewOp(
         print_arena_offset();
       }
     } else if (field->is_repeated()) {
-      if (use_arena_offset && IsRepeatedPtrField(field)) {
+      if (use_arena_offset) {
         op.needs_memcpy = true;
       } else {
         op.needs_arena_seeding = true;
