f483c40ede9a21e2dfa8cfd9d90a7524dec5b551
Mike Kruskal <mkruskal@google.com>
2025-10-21 18:20:55 -0700

    Attempt at fixing determinism in our nuget release
    Fixes #24013
    
    PiperOrigin-RevId: 822355722

--- csharp/build_release.sh
+++ csharp/build_release.sh
@@ -6,6 +6,12 @@ cd $(dirname $(readlink $BASH_SOURCE))
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
 export DOTNET_CLI_TELEMETRY_OPTOUT=true
 
+# Make sure that SourceLink uses the GitHub repo, even if that's not where
+# our origin remote points at.
+git remote add github https://github.com/googleapis/google-api-dotnet-client
+export GitRepositoryRemoteName=github
+export DeterministicSourcePaths=true
+
 # Builds Google.Protobuf NuGet packages
 dotnet restore -s /lib/csharp/ src/Google.Protobuf/Google.Protobuf.csproj
 dotnet pack --no-restore -c Release src/Google.Protobuf.sln -p:ContinuousIntegrationBuild=true
