547ae49491e0eb5a88657184a27dd508b53c72db
Hong Shin <hongshin@google.com>
2025-10-23 15:10:37 -0700

    internal change
    PiperOrigin-RevId: 823205194

--- python/google/protobuf/use_pure_python.cc
+++ python/google/protobuf/use_pure_python.cc
@@ -1,4 +1,10 @@
-// The opposite side of go/fastpythonproto.
+// Protocol Buffers - Google's data interchange format
+// Copyright 2021 Google LLC.  All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
 #include <Python.h>
 
 namespace google {
