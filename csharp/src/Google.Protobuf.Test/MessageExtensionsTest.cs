4f927a1d35a15431de37a8f16e19523e76574e64
g.verdier <g.verdier@criteo.com>
2025-10-31 11:29:23 +0100

    Add test against byte[]

--- csharp/src/Google.Protobuf.Test/MessageExtensionsTest.cs
+++ csharp/src/Google.Protobuf.Test/MessageExtensionsTest.cs
@@ -8,12 +8,33 @@ namespace Google.Protobuf;
 public class MessageExtensionsTest
 {
     [Test]
-    public void TestWriteDelimitedToRoundTripWithBufferWriter()
+    public void TestWriteLengthPrefixedToWithBufferWriter()
+    {
+        TestWellKnownTypes message = new() { StringField = "abcd" };
+
+        TestArrayBufferWriter<byte> buffer = new();
+        message.WriteLengthPrefixedTo(buffer);
+
+        Assert.That(
+            buffer.WrittenSpan.ToArray(),
+            Is.EqualTo(new byte[]
+            {
+                9, // length prefix
+                138, 1, // tag
+                6, // message length
+                10, // tag
+                4, // string length
+                97, 98, 99, 100, // abcd
+            }));
+    }
+
+    [Test]
+    public void TestWriteLengthPrefixedToRoundTripWithBufferWriter()
     {
         TestWellKnownTypes message = new() { Int32Field = 25 };
 
         TestArrayBufferWriter<byte> buffer = new();
-        message.WriteDelimitedTo(buffer);
+        message.WriteLengthPrefixedTo(buffer);
 
         Assert.That(
             TestWellKnownTypes.Parser.ParseDelimitedFrom(new MemoryStream(buffer.WrittenSpan.ToArray())),
