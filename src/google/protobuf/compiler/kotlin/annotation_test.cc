bbb29d2da7892afcd01cd08a33fefee762b15727
Mikita Belahlazau <nbeloglazov@google.com>
2025-12-01 10:10:00 -0800

    Add Kythe annotations to generated Kotlin proto DSLs.
    Annotates generated builder fields, getters, setters, hasFoo and clearFoo methods.
    
    PiperOrigin-RevId: 838826642

--- src/google/protobuf/compiler/kotlin/annotation_test.cc
+++ src/google/protobuf/compiler/kotlin/annotation_test.cc
@@ -89,8 +89,20 @@ TEST_F(KotlinMetadataTest, CapturesFooOrNull) {
   FileDescriptorProto file;
   atu::ExpectedOutput output("com/google/protos/bar/MessageKt.kt");
   EXPECT_TRUE(CaptureMetadata("test.proto", &file, {&output}));
-  CheckAnnotation(output, {kMessageTypeFieldNumber, 0, kFieldFieldNumber, 0},
-                  "fooOrNull", Annotation::NONE);
+  std::vector<int> foo_path = {kMessageTypeFieldNumber, 0, kFieldFieldNumber,
+                               0};
+  CheckAnnotation(output, foo_path, "fooOrNull",
+                  /* expected_semantic= */ Annotation::NONE);
+  CheckAnnotation(output, foo_path, "foo",
+                  /* expected_semantic= */ Annotation::NONE);
+  CheckAnnotation(output, foo_path, "get",
+                  /* expected_semantic= */ Annotation::NONE);
+  CheckAnnotation(output, foo_path, "set",
+                  /* expected_semantic= */ Annotation::SET);
+  CheckAnnotation(output, foo_path, "hasFoo",
+                  /* expected_semantic= */ Annotation::NONE);
+  CheckAnnotation(output, foo_path, "clearFoo",
+                  /* expected_semantic= */ Annotation::SET);
 }
 
 }  // namespace
