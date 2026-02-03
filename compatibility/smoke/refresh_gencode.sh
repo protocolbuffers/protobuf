544af560ccc6860368898dc3af19f212a499f4e3
ericsalo <eric.salo@gmail.com>
2026-02-03 21:03:39 +0000

    add refresh_gencode.sh script to Java smoke tests

--- compatibility/smoke/refresh_gencode.sh
+++ compatibility/smoke/refresh_gencode.sh
@@ -6,13 +6,11 @@ set -e -x
 
 build="BUILD.bazel"
 
-# Copy the original build file.
+# Copy and eventually restore the original build file.
 cp "${build}" "${build}".original
+trap "mv ${build}.original ${build}" EXIT
 
 # Update the subdirs.
 for file in v*; do
 	add_gencode.sh "${file:1}"
 done
-
-# Restore the original build file.
-trap "mv ${build}.original ${build}" EXIT
