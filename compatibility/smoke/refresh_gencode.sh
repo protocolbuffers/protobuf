c2b119a87b22c90eb4772b723529cc4388cabd9f
ericsalo <eric.salo@gmail.com>
2026-02-03 20:53:13 +0000

    add refresh_gencode.sh script to Java smoke tests

--- compatibility/smoke/refresh_gencode.sh
+++ compatibility/smoke/refresh_gencode.sh
@@ -2,16 +2,17 @@
 # Run this script to update the gencode for all version subdirs.
 # Answer yes when prompted about removing write-protected files.
 
+set -e -x
+
 build="BUILD.bazel"
 
-# Remember the original length of the build file.
-lines=`wc "${build}" | cut -f2 -d' '`
+# Copy the original build file.
+cp "${build}" "${build}".original
 
 # Update the subdirs.
 for file in v*; do
 	add_gencode.sh "${file:1}"
 done
 
-# Restore the build file to its original size.
-orig=`head --lines="${lines}" "${build}"`
-echo "${orig}" > "${build}"
+# Restore the original build file.
+trap "mv ${build}.original ${build}" EXIT
