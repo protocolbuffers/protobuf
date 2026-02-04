504d7f033d5c6641929c99dc872763dc6bf1efdd
Tony Liao <tonyliaoss@google.com>
2026-02-04 15:47:59 -0500

    Remove the wheels directory.

--- wheels/protobuf-5.30.0/build/lib.linux-x86_64-cpython-313/google/protobuf/internal/python_edition_defaults.py
+++
@@ -1,5 +0,0 @@
-"""
-This file contains the serialized FeatureSetDefaults object corresponding to
-the Pure Python runtime.  This is used for feature resolution under Editions.
-"""
-_PROTOBUF_INTERNAL_PYTHON_EDITION_DEFAULTS = b"\n\023\030\204\007\"\000*\014\010\001\020\002\030\002 \003(\0010\002\n\023\030\347\007\"\000*\014\010\002\020\001\030\001 \002(\0010\001\n\023\030\350\007\"\014\010\001\020\001\030\001 \002(\0010\001*\000 \346\007(\350\007"
