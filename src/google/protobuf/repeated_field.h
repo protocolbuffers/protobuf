ea7cb358d42954c5059c303f66f07a32ec224dba
Clayton Knittel <cknittel@google.com>
2026-03-12 14:17:01 -0700

    Pass a const reference to the user-provided lambda in `RepeatedField`'s `proto2::sort` and `proto2::stable_sort`.
    It is an anti-pattern to mutate elements in a repeated field as part of this callback, which is meant to only determine the sort order of the repeated field.
    
    PiperOrigin-RevId: 882774121

--- src/google/protobuf/repeated_field.h
+++ src/google/protobuf/repeated_field.h
@@ -1402,7 +1402,8 @@ size_t erase(RepeatedField<T>& cont, const U& value) {
 template <int&..., typename T, typename Compare>
 void sort(internal::RepeatedIterator<T> begin,
           internal::RepeatedIterator<T> end, Compare cmp) {
-  std::sort(begin, end, cmp);
+  std::sort(begin, end,
+            [&cmp](const auto& lhs, const auto& rhs) { return cmp(lhs, rhs); });
 }
 template <int&..., typename T>
 void sort(internal::RepeatedIterator<T> begin,
@@ -1412,7 +1413,9 @@ void sort(internal::RepeatedIterator<T> begin,
 template <int&..., typename T, typename Compare>
 void stable_sort(internal::RepeatedIterator<T> begin,
                  internal::RepeatedIterator<T> end, Compare cmp) {
-  std::stable_sort(begin, end, cmp);
+  std::stable_sort(begin, end, [&cmp](const auto& lhs, const auto& rhs) {
+    return cmp(lhs, rhs);
+  });
 }
 template <int&..., typename T>
 void stable_sort(internal::RepeatedIterator<T> begin,
