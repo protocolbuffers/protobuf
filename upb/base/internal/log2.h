b22d59765c93a8aab2938a8c2680937f346f1728
Joshua Haberman <haberman@google.com>
2026-04-08 11:33:59 -0700

    Detect overflow in upb_Array_Realloc().
    PiperOrigin-RevId: 896612294

--- upb/base/internal/log2.h
+++ upb/base/internal/log2.h
@@ -29,6 +29,7 @@ UPB_INLINE int upb_Log2Ceiling(size_t x) {
 #elif SIZE_MAX == UINT_MAX && UPB_HAS_BUILTIN(__builtin_clz)
   return (sizeof(size_t) * CHAR_BIT) - __builtin_clz(x - 1);
 #else
+  if (x > SIZE_MAX / 2) return sizeof(size_t) * CHAR_BIT;
   int lg2 = 0;
   while ((1 << lg2) < x) lg2++;
   return lg2;
@@ -44,6 +45,14 @@ UPB_INLINE size_t upb_RoundUpToPowerOfTwo(size_t x) {
     return SIZE_MAX;
   }
   return ((size_t)1) << lg2;
+}
+
+UPB_INLINE bool upb_ShlOverflow(size_t* a, unsigned int b) {
+  if (*a > (SIZE_MAX >> b)) {
+    return true;
+  }
+  *a <<= b;
+  return false;
 }
 
 #ifdef __cplusplus
