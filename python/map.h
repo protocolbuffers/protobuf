c6e5e040afc8542b2f84964e1e0e3b250e43c6f3
Runze Wang <runze@google.com>
2026-04-21 20:44:47 -0700

    [py/upb] Make GetOptions() return immutable options for scalar type. UPB will raise a TypeError when options returned GetOptions() by is mutated.
    PiperOrigin-RevId: 903578729

--- python/map.h
+++ python/map.h
@@ -32,8 +32,10 @@ PyObject* PyUpb_MapContainer_GetOrCreateWrapper(upb_Map* map,
 upb_Map* PyUpb_MapContainer_Reify(PyObject* self, upb_Map* map,
                                   PyUpb_WeakMap* subobj_map, intptr_t iter);
 
-// Reifies this map object if it is not already reified.
-upb_Map* PyUpb_MapContainer_EnsureReified(PyObject* self);
+// Reifies this map object if it is not already reified, and ensures it is
+// mutable. If the parent message (for stubs) or the map itself (for reified
+// maps) is frozen, this function will set a Python TypeError and return NULL.
+upb_Map* PyUpb_MapContainer_AssureWritable(PyObject* self);
 
 // Invalidates any existing iterators for the map `obj`.
 void PyUpb_MapContainer_Invalidate(PyObject* obj);
