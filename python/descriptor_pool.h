7805d0ce1ee8db02f1b952637dda59e040ebb55f
Joshua Haberman <haberman@google.com>
2026-05-08 13:32:40 -0700

    Made Python/upb thread-compatible (removed thread-hostile code)
    This CL fixes all known bugs in Python/upb that prevented the library from being thread-compatible under free threading (GIL disabled).
    
    Key components:
    1. Thread-safety primitives: Added `PyUpb_WeakMap`, `PyUpb_LazyPtr`, and `PyUpb_Mutex` under `third_party/upb/python/free_threading`.
    2. Message WeakMap: Unset subobject weak maps in `PyUpb_Message` are initialized and updated thread-safely.
    3. Descriptor WeakMap: descriptors are cached in a WeakMap.
    3. ModuleState WeakMap: Upgraded global descriptor_map in `PyUpb_ModuleState` to PyUpb_WeakMap so descriptor caching across threads is thread-safe.
    4. Arena & Container Thread Safety: Made arena caching, refcounting, map container versioning, and repeated container reification thread-safe under free-threading.
    
    Our extensive use of WeakMap could lead to object creation thrashing if objects are frequently access and then immediately discarded in a loop.  If this is an issue, we could move to strong references, which would avoid thrashing but would create reference cycles that can only be collected when GC runs or the entire object tree is discarded.
    
    PiperOrigin-RevId: 912670831

--- python/descriptor_pool.h
+++ python/descriptor_pool.h
@@ -15,15 +15,18 @@
 
 #include "protobuf.h"
 
-// Returns a Python wrapper object for the given symtab. The symtab must have
-// been created from a Python DescriptorPool originally.
-PyObject* PyUpb_DescriptorPool_Get(const upb_DefPool* symtab);
 
 // Given a Python DescriptorPool, returns the underlying symtab.
 upb_DefPool* PyUpb_DescriptorPool_GetSymtab(PyObject* pool);
 
 // Returns the default DescriptorPool (a global singleton).
 PyObject* PyUpb_DescriptorPool_GetDefaultPool(void);
+
+bool PyUpb_DescriptorPool_CacheAdd(PyObject* pool, const void* key,
+                                   PyObject** py_obj);
+PyObject* PyUpb_DescriptorPool_CacheGet(PyObject* pool, const void* key);
+bool PyUpb_DescriptorPool_CacheEraseIfEqual(PyObject* pool, const void* key,
+                                            PyObject* obj);
 
 // Module-level init.
 bool PyUpb_InitDescriptorPool(PyObject* m);
