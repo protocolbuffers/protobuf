ad698c54d4a5292966a5de22271c0e13cabfd262
Anthony Hurtado <amhurtado@pm.me>
2026-05-26 11:40:38 -0500

    Add test for [weak = true] on non-message field validation
    Adds a BuildFileWithErrors test verifying that applying [weak = true]
    to a non-message field (TYPE_INT32) is rejected with the expected error
    during descriptor validation.

--- src/google/protobuf/descriptor_unittest.cc
+++ src/google/protobuf/descriptor_unittest.cc
@@ -7542,6 +7542,19 @@ TEST_F(ValidationErrorTest, OutputTypeNotAMessage) {
 }
 
 
+TEST_F(ValidationErrorTest, WeakOnNonMessageField) {
+  BuildFileWithErrors(
+      "name: \"foo.proto\" "
+      "message_type { "
+      "  name: \"Foo\" "
+      "  field { name:\"bar\" number:1 label:LABEL_OPTIONAL "
+      "          type:TYPE_INT32 "
+      "          options { weak: true } } "
+      "} ",
+      "foo.proto: Foo.bar: TYPE: [weak = true] can only be specified for "
+      "submessage fields.\n");
+}
+
 TEST_F(ValidationErrorTest, IllegalPackedField) {
   BuildFileWithErrors(
       R"pb(
