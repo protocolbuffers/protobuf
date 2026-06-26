a85fcbc0b51475218a6c50840abeb0b1552c726a
Tony Liao <tonyliaoss@google.com>
2026-06-26 07:57:11 -0700

    Internal change.
    PiperOrigin-RevId: 938591389

--- src/google/protobuf/json/json_enumval_custom_string_test.cc
+++ src/google/protobuf/json/json_enumval_custom_string_test.cc
@@ -5,13 +5,19 @@
 // license that can be found in the LICENSE file or at
 // https://developers.google.com/open-source/licenses/bsd
 
+#include <cstddef>
+#include <iterator>
 #include <string>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "absl/log/absl_check.h"
 #include "absl/status/status.h"
+#include "absl/strings/string_view.h"
+#include "google/protobuf/arena.h"
 #include "google/protobuf/json/json.h"
 #include "google/protobuf/json/json_enumval_custom_string.pb.h"
+
 
 #ifndef EXPECT_OK
 #define EXPECT_OK(x) EXPECT_TRUE(x.ok())
@@ -234,6 +240,7 @@ TEST(JsonEnumvalCustomStringTest, GreatHelmIntOverride) {
   EXPECT_OK(status);
   EXPECT_EQ(json_res, R"json({"armor":1})json");
 }
+
 }  // namespace
 }  // namespace protobuf
 }  // namespace google
