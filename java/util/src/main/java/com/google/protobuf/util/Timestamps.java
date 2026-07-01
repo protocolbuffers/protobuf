899fd239b32781fe93bc5753d8f788310eb60f5b
Rachel Goldfinger <rgoldfinger@google.com>
2026-07-01 12:24:06 -0700

    Make Timestamps.parse() attempt non-lenient parsing first and warning if this fails.
    PiperOrigin-RevId: 941233651

--- java/util/src/main/java/com/google/protobuf/util/Timestamps.java
+++ java/util/src/main/java/com/google/protobuf/util/Timestamps.java
@@ -24,6 +24,7 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
 import java.util.TimeZone;
+import java.util.logging.Logger;
 import javax.annotation.Nullable;
 
 /**
@@ -31,6 +32,8 @@ import javax.annotation.Nullable;
  * {@link IllegalArgumentException} if the input(s) are not {@linkplain #isValid(Timestamp) valid}.
  */
 public final class Timestamps {
+
+  private static final Logger logger = Logger.getLogger(Timestamps.class.getName());
 
   // Timestamp for "0001-01-01T00:00:00Z"
   static final long TIMESTAMP_SECONDS_MIN = -62135596800L;
@@ -245,7 +248,22 @@ public final class Timestamps {
       secondValue = timeValue.substring(0, pointPosition);
       nanoValue = timeValue.substring(pointPosition + 1);
     }
-    Date date = timestampFormat.get().parse(secondValue);
+    SimpleDateFormat sdf = timestampFormat.get();
+    boolean wasLenient = sdf.isLenient();
+    Date date;
+    try {
+      sdf.setLenient(false);
+      date = sdf.parse(secondValue);
+    } catch (ParseException e) {
+      sdf.setLenient(wasLenient);
+      date = sdf.parse(secondValue);
+      logger.warning(
+          "Invalid timestamp detected: "
+              + value
+              + " (b/515049363). This will become an error in 2027.");
+    } finally {
+      sdf.setLenient(wasLenient);
+    }
     long seconds = date.getTime() / MILLIS_PER_SECOND;
     int nanos = nanoValue.isEmpty() ? 0 : parseNanos(nanoValue);
     // Parse timezone offsets.
