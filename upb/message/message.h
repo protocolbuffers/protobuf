33bca9296f8e94e29ca98225b9c7f7cb813e3e10
Ada Zhang <adazhang@google.com>
2026-07-29 09:28:58 -0700

    Use `upb_TaggedAuxPtr_IsNull` helper in upb/message/message.h.
    PiperOrigin-RevId: 955926792

--- upb/message/message.h
+++ upb/message/message.h
@@ -62,7 +62,7 @@ UPB_INLINE bool upb_Message_HasUnknown(const upb_Message* msg) {
   if (!in) return false;
   for (size_t i = 0; i < in->size; i++) {
     upb_TaggedAuxPtr tagged_ptr = in->aux_data[i];
-    if (tagged_ptr.ptr != 0 &&
+    if (!upb_TaggedAuxPtr_IsNull(tagged_ptr) &&
         !upb_TaggedAuxPtr_IsSemanticallyKnown(tagged_ptr)) {
       return true;
     }
