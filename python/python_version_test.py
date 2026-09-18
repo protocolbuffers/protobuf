85ff72d8e06e84505a0c2ffbd356389f481f4849
Richard Sartor <rsartor@google.com>
2026-09-18 16:49:40 +0000

    Print diagnostic info.

--- python/python_version_test.py
+++ python/python_version_test.py
@@ -9,6 +9,7 @@
 import os
 import sys
 import unittest
+import platform
 
 
 class PythonVersionTest(unittest.TestCase):
@@ -17,6 +18,24 @@ class PythonVersionTest(unittest.TestCase):
     """Test that we can import nested import public messages."""
 
     exp = os.getenv('KOKORO_PYTHON_VERSION', '')
+
+    print(f':::: platform.uname: {platform.uname()}')
+    print(f':::: platform.platform: {platform.platform()}')
+    print(f':::: platform.python_branch: {platform.python_branch()}')
+    print(f':::: platform.python_build: {platform.python_build()}')
+    print(f':::: platform.python_compiler: {platform.python_compiler()}')
+    print(f':::: platform.python_implementation: {platform.python_implementation()}')
+    print(f':::: platform.python_revision: {platform.python_revision()}')
+    print(f':::: platform.python_version: {platform.python_version()}')
+    #print(f':::: sys.byte_order: {sys.byte_order}')
+    print(f':::: sys.executable: {sys.executable}')
+    print(f':::: sys.path: {sys.path}')
+    print(f':::: sys.path_hooks: {sys.path_hooks}')
+    print(f':::: sys.path_importer_cache: {sys.path_importer_cache}')
+    print(f':::: sys.platform: {sys.platform}')
+    print(f':::: sys.version: {sys.version}')
+    print(f':::: sys.version_info: {sys.version_info}')
+
     if not exp:
       print('No system python version found, skipping check', file=sys.stderr)
       return
