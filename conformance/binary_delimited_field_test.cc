CodeLinaro-mirror/yocto-mirrors_protobuf

Delete the legacy text-format conformance suite.
Every text-format conformance test now lives in the gtest-based text suite (:text_conformance_tests / cpp_text_test), so TextFormatConformanceTestSuite has nothing left to run.  This deletes text_format_conformance_suite.{h,cc}, its cc_library and Makefile.am sources, and its include and instance in conformance_test_main.cc; the merged runner now runs the gtest suites and the legacy binary/JSON suite only.

--text_format_failure_list stays accepted: ParseConformanceRunnerArgs() used to derive the accepted failure list flags from the legacy suites, so it gains an explicit alias table (kGtestOnlyFailureListFlags) of flags no legacy suite loads any more.  The files are recorded under their own flag name, so the legacy binary/JSON suite's list is unchanged and the gtest phase can tell which list an entry came from.  Every test target and script that passes the flag keeps working unchanged.  TODO: b/563657875 - drop the alias once the sh_test/JSPB invocations move to the conformance_test() macro.

Since no legacy suite loads that list any more, the gtest phase now reports its unmatched entries itself, with the legacy suite's message and unmatched.txt, and fails the run like the legacy text-format suite did.  Its report comes in one block per failure list flag (FailureListReport in conformance_test_main.cc), each with the update_failure_list hints naming that flag's last file (or a `<the --flag file>` placeholder when the flag was never given): a result matched by a failure list entry goes to the flag whose file lists the entry, a test in no list to --text_format_failure_list if it is a text-format test (IsTextFormatTest()) and to --failure_list otherwise; only the unmatched-entry check is left to the legacy suite for the flag it loads.  Deleting the legacy binary/JSON suite later therefore won't change the report's shape.  The lists are grouped, validated and indexed in one pass (LoadFailureListReports(), replacing Validat

conformance/binary_delimited_field_test.cc
@@ -1,0 +1,136 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2025 Google LLC.  All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+// Binary conformance tests for TestAllTypesEdition2023: length-prefixed and
+// delimited (group-encoded) message fields, maps, and delimited extensions
+// under editions.  This holds the binary-output leg of the legacy suite-level
+// BinaryAndJsonConformanceSuite::RunDelimitedFieldTests(); the legacy suite
+// still sends the binary->JSON leg of the same DelimitedFieldTest cases.  The
+// test names and the requests sent to the testee are identical to the legacy
+// ones.
+//
+// TODO: b/410122158 - The binary->JSON legs of these tests join the JSON suite
+// once JSON matching is migrated.
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "conformance/binary_wireformat.h"
+#include "conformance/matchers.h"
+#include "conformance/message_type_fixtures.h"
+#include "conformance/test_environment.h"
+
+namespace google {
+namespace protobuf {
+namespace conformance {
+namespace {
+
+// Every test here parses TestAllTypesEdition2023 (message()), so the fixtures
+// skip themselves when --maximum_edition doesn't cover editions.
+using DelimitedFieldTest = Edition2023ConformanceTest;
+
+// Field 1 is optional_int32.
+TEST_F(DelimitedFieldTest, ValidNonMessage) {
+  EXPECT_THAT(
+      RequiredTest("ValidNonMessage")
+          .ParseBinary(message(), VarintField(1, 99))
+          .SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(R"pb(optional_int32: 99)pb"))));
+}
+
+// Field 18 is optional_nested_message, whose field 1 is a.
+TEST_F(DelimitedFieldTest, ValidLengthPrefixedField) {
+  EXPECT_THAT(
+      RequiredTest("ValidLengthPrefixedField")
+          .ParseBinary(message(), LengthPrefixedField(18, VarintField(1, 99)))
+          .SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(R"pb(optional_nested_message {
+                                                  a: 99
+                                                })pb"))));
+}
+
+// Field 56 is map_int32_int32; map entries have key = 1 and value = 2.
+TEST_F(DelimitedFieldTest, ValidMapInteger) {
+  EXPECT_THAT(
+      RequiredTest("ValidMap.Integer")
+          .ParseBinary(message(),
+                       LengthPrefixedField(
+                           56, Wire(VarintField(1, 99), VarintField(2, 87))))
+          .SerializeBinary(),
+      Yields(ParsedPayload(
+          EqualsTextProto(R"pb(map_int32_int32 { key: 99 value: 87 })pb"))));
+}
+
+// Field 71 is map_string_nested_message.
+TEST_F(DelimitedFieldTest, ValidMapLengthPrefixed) {
+  EXPECT_THAT(
+      RequiredTest("ValidMap.LengthPrefixed")
+          .ParseBinary(message(), LengthPrefixedField(
+                                      71, Wire(LengthPrefixedField(1, "a"),
+                                               LengthPrefixedField(
+                                                   2, VarintField(1, 87)))))
+          .SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(R"pb(map_string_nested_message {
+                                                  key: "a"
+                                                  value: { a: 87 }
+                                                })pb"))));
+}
+
+// Field 201 is groupliketype (whose name matches its type, as a proto2 group
+// would) and field 202 is delimited_field (whose name doesn't).  Both are
+// GroupLikeType, whose field 202 is group_int32.
+TEST_F(DelimitedFieldTest, GroupLike) {
+  EXPECT_THAT(
+      RequiredTest("ValidDelimitedField.GroupLike")
+          .ParseBinary(message(), DelimitedField(201, VarintField(202, 99)))
+          .SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(R"pb(groupliketype {
+                                                  group_int32: 99
+                                                })pb"))));
+}
+
+TEST_F(DelimitedFieldTest, NotGroupLike) {
+  EXPECT_THAT(
+      RequiredTest("ValidDelimitedField.NotGroupLike")
+          .ParseBinary(message(), DelimitedField(202, VarintField(202, 99)))
+          .SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(R"pb(delimited_field {
+                                                  group_int32: 99
+                                                })pb"))));
+}
+
+// Only TestAllTypesEdition2023 declares delimited extensions: field 121 is
+// the groupliketype extension (whose name matches its type, as a proto2 group
+// would) and field 122 is delimited_ext (whose name doesn't).  Both are
+// GroupLikeType, whose field 1 is c.
+using DelimitedExtensionTest = Edition2023ConformanceTest;
+
+TEST_F(DelimitedExtensionTest, GroupLike) {
+  EXPECT_THAT(
+      RequiredTest("ValidDelimitedExtension.GroupLike")
+          .ParseBinary(message(), DelimitedField(121, VarintField(1, 99)))
+          .SerializeBinary(),
+      Yields(ParsedPayload(
+          EqualsTextProto(R"pb([protobuf_test_messages.editions.groupliketype] {
+                                 c: 99
+                               })pb"))));
+}
+
+TEST_F(DelimitedExtensionTest, NotGroupLike) {
+  EXPECT_THAT(
+      RequiredTest("ValidDelimitedExtension.NotGroupLike")
+          .ParseBinary(message(), DelimitedField(122, VarintField(1, 99)))
+          .SerializeBinary(),
+      Yields(ParsedPayload(
+          EqualsTextProto(R"pb([protobuf_test_messages.editions.delimited_ext] {
+                                 c: 99
+                               })pb"))));
+}
+
+}  // namespace
+}  // namespace conformance
+}  // namespace protobuf
+}  // namespace google

conformance/binary_unstable_edition_test.cc
@@ -1,0 +1,71 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2025 Google LLC.  All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+// Binary conformance tests for TestAllTypesEditionUnstable, the test message
+// declared under the unstable edition.  This holds the binary-output leg of
+// the legacy suite-level BinaryAndJsonConformanceSuite::RunUnstableTests();
+// the test names and the requests sent to the testee are identical to the
+// legacy ones.
+//
+// Like the legacy suite, these run whenever --maximum_edition is 2023 or
+// newer (see ConformanceTest::IsSupported()), not only when it names the
+// unstable edition itself.
+//
+// TODO: b/410122158 - The binary->JSON legs of these tests join the JSON suite
+// once JSON matching is migrated.
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "conformance/binary_wireformat.h"
+#include "conformance/matchers.h"
+#include "conformance/test_environment.h"
+#include "conformance/test_protos/test_messages_edition_unstable.pb.h"
+#include "google/protobuf/descriptor.h"
+
+namespace google {
+namespace protobuf {
+namespace conformance {
+namespace {
+
+using ::protobuf_test_messages::edition_unstable::TestAllTypesEditionUnstable;
+
+// Reporting TestAllTypesEditionUnstable through MessageUnderTest() makes the
+// base SetUp() skip these tests when --maximum_edition doesn't cover it.
+class UnstableEditionTest : public ConformanceTest {
+ protected:
+  const Descriptor* MessageUnderTest() const override { return message(); }
+
+  static const Descriptor* message() {
+    return TestAllTypesEditionUnstable::descriptor();
+  }
+};
+
+// Field 13 is optional_bytes.
+TEST_F(UnstableEditionTest, ValidBytes) {
+  EXPECT_THAT(
+      RequiredTest("ValidBytes")
+          .ParseBinary(message(), LengthPrefixedField(13, "foo"))
+          .SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(R"pb(optional_bytes: "foo")pb"))));
+}
+
+// Field 15 is map_string_bytes; its entries have key = 1 and value = 2.
+TEST_F(UnstableEditionTest, ValidMapBytes) {
+  EXPECT_THAT(RequiredTest("ValidMap.Bytes")
+                  .ParseBinary(message(),
+                               LengthPrefixedField(
+                                   15, Wire(LengthPrefixedField(1, "foo"),
+                                            LengthPrefixedField(2, "barbaz"))))
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(
+                  R"pb(map_string_bytes { key: "foo" value: "barbaz" })pb"))));
+}
+
+}  // namespace
+}  // namespace conformance
+}  // namespace protobuf
+}  // namespace google

protocolbuffers/protobuf

conformance/binary_delimited_field_test.cc
@@ -1,0 +1,136 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2025 Google LLC.  All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+// Binary conformance tests for TestAllTypesEdition2023: length-prefixed and
+// delimited (group-encoded) message fields, maps, and delimited extensions
+// under editions.  This holds the binary-output leg of the legacy suite-level
+// BinaryAndJsonConformanceSuite::RunDelimitedFieldTests(); the legacy suite
+// still sends the binary->JSON leg of the same DelimitedFieldTest cases.  The
+// test names and the requests sent to the testee are identical to the legacy
+// ones.
+//
+// TODO: b/410122158 - The binary->JSON legs of these tests join the JSON suite
+// once JSON matching is migrated.
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "conformance/binary_wireformat.h"
+#include "conformance/matchers.h"
+#include "conformance/message_type_fixtures.h"
+#include "conformance/test_environment.h"
+
+namespace google {
+namespace protobuf {
+namespace conformance {
+namespace {
+
+// Every test here parses TestAllTypesEdition2023 (message()), so the fixtures
+// skip themselves when --maximum_edition doesn't cover editions.
+using DelimitedFieldTest = Edition2023ConformanceTest;
+
+// Field 1 is optional_int32.
+TEST_F(DelimitedFieldTest, ValidNonMessage) {
+  EXPECT_THAT(
+      Testee("ValidNonMessage")
+          .ParseBinary(message(), VarintField(1, 99))
+          .SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(R"pb(optional_int32: 99)pb"))));
+}
+
+// Field 18 is optional_nested_message, whose field 1 is a.
+TEST_F(DelimitedFieldTest, ValidLengthPrefixedField) {
+  EXPECT_THAT(
+      Testee("ValidLengthPrefixedField")
+          .ParseBinary(message(), LengthPrefixedField(18, VarintField(1, 99)))
+          .SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(R"pb(optional_nested_message {
+                                                  a: 99
+                                                })pb"))));
+}
+
+// Field 56 is map_int32_int32; map entries have key = 1 and value = 2.
+TEST_F(DelimitedFieldTest, ValidMapInteger) {
+  EXPECT_THAT(
+      Testee("ValidMap.Integer")
+          .ParseBinary(message(),
+                       LengthPrefixedField(
+                           56, Wire(VarintField(1, 99), VarintField(2, 87))))
+          .SerializeBinary(),
+      Yields(ParsedPayload(
+          EqualsTextProto(R"pb(map_int32_int32 { key: 99 value: 87 })pb"))));
+}
+
+// Field 71 is map_string_nested_message.
+TEST_F(DelimitedFieldTest, ValidMapLengthPrefixed) {
+  EXPECT_THAT(
+      Testee("ValidMap.LengthPrefixed")
+          .ParseBinary(message(), LengthPrefixedField(
+                                      71, Wire(LengthPrefixedField(1, "a"),
+                                               LengthPrefixedField(
+                                                   2, VarintField(1, 87)))))
+          .SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(R"pb(map_string_nested_message {
+                                                  key: "a"
+                                                  value: { a: 87 }
+                                                })pb"))));
+}
+
+// Field 201 is groupliketype (whose name matches its type, as a proto2 group
+// would) and field 202 is delimited_field (whose name doesn't).  Both are
+// GroupLikeType, whose field 202 is group_int32.
+TEST_F(DelimitedFieldTest, GroupLike) {
+  EXPECT_THAT(
+      Testee("ValidDelimitedField.GroupLike")
+          .ParseBinary(message(), DelimitedField(201, VarintField(202, 99)))
+          .SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(R"pb(groupliketype {
+                                                  group_int32: 99
+                                                })pb"))));
+}
+
+TEST_F(DelimitedFieldTest, NotGroupLike) {
+  EXPECT_THAT(
+      Testee("ValidDelimitedField.NotGroupLike")
+          .ParseBinary(message(), DelimitedField(202, VarintField(202, 99)))
+          .SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(R"pb(delimited_field {
+                                                  group_int32: 99
+                                                })pb"))));
+}
+
+// Only TestAllTypesEdition2023 declares delimited extensions: field 121 is
+// the groupliketype extension (whose name matches its type, as a proto2 group
+// would) and field 122 is delimited_ext (whose name doesn't).  Both are
+// GroupLikeType, whose field 1 is c.
+using DelimitedExtensionTest = Edition2023ConformanceTest;
+
+TEST_F(DelimitedExtensionTest, GroupLike) {
+  EXPECT_THAT(
+      Testee("ValidDelimitedExtension.GroupLike")
+          .ParseBinary(message(), DelimitedField(121, VarintField(1, 99)))
+          .SerializeBinary(),
+      Yields(ParsedPayload(
+          EqualsTextProto(R"pb([protobuf_test_messages.editions.groupliketype] {
+                                 c: 99
+                               })pb"))));
+}
+
+TEST_F(DelimitedExtensionTest, NotGroupLike) {
+  EXPECT_THAT(
+      Testee("ValidDelimitedExtension.NotGroupLike")
+          .ParseBinary(message(), DelimitedField(122, VarintField(1, 99)))
+          .SerializeBinary(),
+      Yields(ParsedPayload(
+          EqualsTextProto(R"pb([protobuf_test_messages.editions.delimited_ext] {
+                                 c: 99
+                               })pb"))));
+}
+
+}  // namespace
+}  // namespace conformance
+}  // namespace protobuf
+}  // namespace google

conformance/binary_unstable_edition_test.cc
@@ -1,0 +1,71 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2025 Google LLC.  All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+// Binary conformance tests for TestAllTypesEditionUnstable, the test message
+// declared under the unstable edition.  This holds the binary-output leg of
+// the legacy suite-level BinaryAndJsonConformanceSuite::RunUnstableTests();
+// the test names and the requests sent to the testee are identical to the
+// legacy ones.
+//
+// Like the legacy suite, these run whenever --maximum_edition is 2023 or
+// newer (see ConformanceTest::IsSupported()), not only when it names the
+// unstable edition itself.
+//
+// TODO: b/410122158 - The binary->JSON legs of these tests join the JSON suite
+// once JSON matching is migrated.
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "conformance/binary_wireformat.h"
+#include "conformance/matchers.h"
+#include "conformance/test_environment.h"
+#include "conformance/test_protos/test_messages_edition_unstable.pb.h"
+#include "google/protobuf/descriptor.h"
+
+namespace google {
+namespace protobuf {
+namespace conformance {
+namespace {
+
+using ::protobuf_test_messages::edition_unstable::TestAllTypesEditionUnstable;
+
+// Reporting TestAllTypesEditionUnstable through MessageUnderTest() makes the
+// base SetUp() skip these tests when --maximum_edition doesn't cover it.
+class UnstableEditionTest : public ConformanceTest {
+ protected:
+  const Descriptor* MessageUnderTest() const override { return message(); }
+
+  static const Descriptor* message() {
+    return TestAllTypesEditionUnstable::descriptor();
+  }
+};
+
+// Field 13 is optional_bytes.
+TEST_F(UnstableEditionTest, ValidBytes) {
+  EXPECT_THAT(
+      Testee("ValidBytes")
+          .ParseBinary(message(), LengthPrefixedField(13, "foo"))
+          .SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(R"pb(optional_bytes: "foo")pb"))));
+}
+
+// Field 15 is map_string_bytes; its entries have key = 1 and value = 2.
+TEST_F(UnstableEditionTest, ValidMapBytes) {
+  EXPECT_THAT(Testee("ValidMap.Bytes")
+                  .ParseBinary(message(),
+                               LengthPrefixedField(
+                                   15, Wire(LengthPrefixedField(1, "foo"),
+                                            LengthPrefixedField(2, "barbaz"))))
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(
+                  R"pb(map_string_bytes { key: "foo" value: "barbaz" })pb"))));
+}
+
+}  // namespace
+}  // namespace conformance
+}  // namespace protobuf
+}  // namespace google