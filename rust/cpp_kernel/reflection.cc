511b1c34f0f5fa6406084c8afd6da841dd785183
Runze Wang <runze@google.com>
2026-09-24 09:27:59 -0700

    internal change
    PiperOrigin-RevId: 987557943

--- rust/cpp_kernel/reflection.cc
+++ rust/cpp_kernel/reflection.cc
@@ -19,4 +19,13 @@ google::protobuf::rust::RustStringRawParts proto2_rust_Message_print_to_text_for
   return google::protobuf::rust::RustStringRawParts(text);
 }
 
+// Parses a message from its TextFormat representation.
+bool proto2_rust_Message_parse_from_text_format(google::protobuf::MessageLite* m,
+                                                google::protobuf::rust::PtrAndLen input) {
+  auto* msg = google::protobuf::DynamicCastMessage<google::protobuf::Message>(m);
+  // This should always hold because the Rust API requires `WithReflection`.
+  ABSL_CHECK(msg != nullptr) << "Message does not support reflection.";
+  return google::protobuf::TextFormat::ParseFromString(input.AsStringView(), msg);
+}
+
 }  // extern "C"
--- rust/cpp_kernel/reflection.rs
+++ rust/cpp_kernel/reflection.rs
@@ -24,6 +24,15 @@ unsafe extern "C" {
     /// - `m` is a legally dereferenceable MessageLite* that supports
     ///   reflection.
     fn proto2_rust_Message_print_to_text_format(m: RawMessage) -> RustStringRawParts;
+
+    /// From reflection.cc
+    ///
+    /// # Safety
+    /// - `m` is a legally dereferenceable and mutable MessageLite* that
+    ///   supports reflection.
+    /// - `input` is a valid `PtrAndLen` pointing to readable memory for its
+    ///   length.
+    fn proto2_rust_Message_parse_from_text_format(m: RawMessage, input: PtrAndLen) -> bool;
 }
 
 /// Returns the protobuf TextFormat representation of `msg`.
@@ -33,4 +42,18 @@ pub fn print_to_text_format<T: crate::WithReflection>(msg: &T) -> String {
     // SAFETY: `raw` points to a live message, and `WithReflection` guarantees it
     // supports reflection.
     unsafe { proto2_rust_Message_print_to_text_format(raw) }.into()
+}
+
+/// Parses a message from its protobuf TextFormat representation.
+#[cfg(not(lite_runtime))]
+pub fn parse_from_text_format<T: crate::WithReflection>(input: &str) -> Result<T, ParseError> {
+    let mut msg = T::default();
+    let raw = msg.get_raw_message_mut(Private);
+    // SAFETY:
+    // - `raw` points to a live, mutable message, and `WithReflection`
+    //   guarantees it supports reflection.
+    // - `input` is converted from a valid `&str` slice.
+    unsafe { proto2_rust_Message_parse_from_text_format(raw, input.as_bytes().into()) }
+        .then_some(msg)
+        .ok_or(ParseError)
 }
